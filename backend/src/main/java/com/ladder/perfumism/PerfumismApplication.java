07321fa0d712973f9076a6bfaf4bf638cb77ff24
yeonju501 <loling970124@gmail.com>
2022-03-07 14:06:42 +0900

    [#4] feat: spring 개발 환경설정

---
+++ backend/src/main/java/com/ladder/perfumism/PerfumismApplication.java
@@ -0,0 +1,13 @@
+package com.ladder.perfumism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumismApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ladder/perfumism/PerfumismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladder.perfumism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
