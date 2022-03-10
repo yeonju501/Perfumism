c950792d490bfcd0365ab022df8e79b9ac46a4ab
vtaco <woosj7129@naver.com>
2022-03-10 16:13:35 +0900

    [S06P22D105-22] feat: ArticleService.java articleCreate() 구현

--- backend/src/main/java/com/ladder/perfumism/article/service/ArticleService.java
+++ backend/src/main/java/com/ladder/perfumism/article/service/ArticleService.java
@@ -1,14 +1,29 @@
 package com.ladder.perfumism.article.service;
 
+import com.ladder.perfumism.article.controller.dto.request.ArticleCreateRequest;
+import com.ladder.perfumism.article.domain.Article;
 import com.ladder.perfumism.article.domain.ArticleRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
+
     public ArticleService(ArticleRepository articleRepository){
         this.articleRepository = articleRepository;
+    }
+
+    @Transactional
+    public void articleCreate(ArticleCreateRequest request) {
+        Article article = Article.builder()
+            .title(request.getTitle())
+            .content(request.getTitle())
+            .subject(request.getSubject())
+            .build();
+
+        articleRepository.save(article);
     }
 }
