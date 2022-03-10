f04cfce3da9d3372bb215f308dc7ea608fe43a5a
yeonju501 <loling970124@gmail.com>
2022-03-10 15:07:56 +0900

    [S06P22D105-33] feat: MemberSaveRequest swagger 설정 추가

--- backend/src/main/java/com/ladder/perfumism/member/controller/dto/request/MemberSaveRequest.java
+++ backend/src/main/java/com/ladder/perfumism/member/controller/dto/request/MemberSaveRequest.java
@@ -2,24 +2,28 @@ package com.ladder.perfumism.member.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.member.domain.Member;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class MemberSaveRequest {
 
+    @ApiModelProperty(name = "email", example = "loling3@naver.com")
     @JsonProperty("email")
     private String email;
 
+    @ApiModelProperty(name = "password", example = "dkjghdfkjhdkjh")
     @JsonProperty("password")
     private String password;
 
+    @ApiModelProperty(name = "username", example = "연주")
     @JsonProperty("username")
     private String username;
 
     public MemberSaveRequest() {
     }
 
-    public MemberSaveRequest(String email, String password, String username){
+    public MemberSaveRequest(String email, String password, String username) {
         this.email = email;
         this.password = password;
         this.username = username;
@@ -28,5 +32,4 @@ public class MemberSaveRequest {
     public Member toMember() {
         return Member.createMember(email, password, username);
     }
-
 }
