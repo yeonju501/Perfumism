33fbf434eb95be77fd3d32ebc53c11bea42c2179
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-10 17:39:43 +0900

    [S06P22D105-84] feat: AccordResponse 에 Swagger Example 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/AccordResponse.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/AccordResponse.java
@@ -2,15 +2,19 @@ package com.ladder.perfumism.perfume.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.perfume.domain.PerfumeAccord;
+import io.swagger.annotations.ApiModelProperty;
 
 public class AccordResponse {
 
+    @ApiModelProperty(example = "1")
     @JsonProperty("accord_id")
     private Long accordId;
 
+    @ApiModelProperty(example = "보드카")
     @JsonProperty("kor_name")
     private String korName;
 
+    @ApiModelProperty(example = "vodka")
     @JsonProperty("eng_name")
     private String engName;
 
