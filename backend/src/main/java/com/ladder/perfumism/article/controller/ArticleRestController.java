33389839b38fa6cb123497a9643ecb2d3f77f360
vtaco <woosj7129@naver.com>
2022-03-10 18:16:33 +0900

    [S06P22D105-22] feat: 게시글 작성 기능 Swagger

--- backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
@@ -2,6 +2,8 @@ package com.ladder.perfumism.article.controller;
 
 import com.ladder.perfumism.article.controller.dto.request.ArticleCreateRequest;
 import com.ladder.perfumism.article.service.ArticleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/articles")
+@Api(tags = {"게시글"})
 public class ArticleRestController {
 
     private final ArticleService articleService;
@@ -20,9 +23,12 @@ public class ArticleRestController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Void> createArticle(@RequestBody ArticleCreateRequest request){
+    @ApiOperation(value = "게시글 작성", notes = "게시글 작성하는 api 입니다")
+        public ResponseEntity<Void> createArticle(@RequestBody ArticleCreateRequest request){
+
         articleService.articleCreate(request);
         URI uri = URI.create("api/articles/create");
+
         return ResponseEntity.created(uri).build();
     }
 }
--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/request/ArticleCreateRequest.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/request/ArticleCreateRequest.java
@@ -2,18 +2,22 @@ package com.ladder.perfumism.article.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.article.domain.ArticleSubject;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class ArticleCreateRequest {
 
     @JsonProperty("subject")
+    @ApiModelProperty(required = true, notes = "말머리", example = "TALK")
     private ArticleSubject subject;
 
     @JsonProperty("title")
+    @ApiModelProperty(required = true, notes = "제목", example = "반가워 나는 제목이라고해")
     private String title;
 
     @JsonProperty("content")
+    @ApiModelProperty(required = true, notes = "내용", example = "아 그래? 나는 내용이라고해! 우리 잘 지내보자 ^_^")
     private String content;
 
     protected ArticleCreateRequest(){
