4126e4861e60474a17e924e27eaaa47f4c0a883b
vtaco <woosj7129@naver.com>
2022-03-10 16:15:00 +0900

    [S06P22D105-22] feat: ArticleRestController.java 게시글 생성 요청 처리

--- backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
@@ -1,8 +1,11 @@
 package com.ladder.perfumism.article.controller;
 
+import com.ladder.perfumism.article.controller.dto.request.ArticleCreateRequest;
+import com.ladder.perfumism.article.service.ArticleService;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/articles")
 public class ArticleRestController {
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> createArticle(){
+    private final ArticleService articleService;
 
+    public ArticleRestController(ArticleService articleService){
+        this.articleService = articleService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createArticle(@RequestBody ArticleCreateRequest request){
+        articleService.articleCreate(request);
         URI uri = URI.create("api/articles/create");
         return ResponseEntity.created(uri).build();
     }
