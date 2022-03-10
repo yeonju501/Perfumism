b33710ed8b36a989f803941406c3defde4831890
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-10 17:40:39 +0900

    [S06P22D105-84] feat: PerfumeRestController 에 Swagger 설명 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/controller/PerfumeRestController.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/controller/PerfumeRestController.java
@@ -2,6 +2,8 @@ package com.ladder.perfumism.perfume.controller;
 
 import com.ladder.perfumism.perfume.controller.dto.response.PerfumeDetailResponse;
 import com.ladder.perfumism.perfume.service.PerfumeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = {"향수"})
 public class PerfumeRestController {
 
     private PerfumeService perfumeService;
@@ -18,6 +21,7 @@ public class PerfumeRestController {
         this.perfumeService = perfumeService;
     }
 
+    @ApiOperation(value = "향수 상세 정보", notes = "단일 향수 상세 정보를 조회하는 API 입니다.")
     @GetMapping("/perfumes/{perfume_id}")
     public ResponseEntity<PerfumeDetailResponse> viewDetailPerfume(@PathVariable(value = "perfume_id") Long perfumeId) {
         return ResponseEntity.ok().body(perfumeService.viewDetailPerfume(perfumeId));
