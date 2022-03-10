ec4c2e980f42d208af15d458580cd03c5eca727d
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-10 17:39:22 +0900

    [S06P22D105-84] feat: PerfumeDetailResponse 에 Swagger Example 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/PerfumeDetailResponse.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/PerfumeDetailResponse.java
@@ -4,44 +4,56 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.perfume.domain.Perfume;
 import com.ladder.perfumism.perfume.domain.PerfumeAccord;
 import com.ladder.perfumism.perfume.domain.SimilarPerfume;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PerfumeDetailResponse {
 
+    @ApiModelProperty(example = "1")
     @JsonProperty("perfume_id")
     private Long id;
 
+    @ApiModelProperty(example = "Sweet Tobacco Spirits")
     @JsonProperty("perfume_name")
     private String name;
 
     @JsonProperty("brand")
     private BrandResponse brand;
 
+    @ApiModelProperty(example = "o.37691.jpg")
     @JsonProperty("image")
     private String image;
 
+    @ApiModelProperty(example = "2016")
     @JsonProperty("launch_year")
     private Integer launchYear;
 
+    @ApiModelProperty(example = "3.4")
     @JsonProperty("average_grade")
     private Double averageGrade;
 
+    @ApiModelProperty(example = "Citruses")
     @JsonProperty("top_notes")
     private String topNotes;
 
+    @ApiModelProperty(example = "Saffron")
     @JsonProperty("middle_notes")
     private String middleNotes;
 
+    @ApiModelProperty(example = "Tonka Bean")
     @JsonProperty("base_notes")
     private String baseNotes;
 
+    @ApiModelProperty(example = "126")
     @JsonProperty("total_survey")
     private Long totalSurvey;
 
+    @ApiModelProperty(example = "moderate")
     @JsonProperty("longevity")
     private String longevity;
 
+    @ApiModelProperty(example = "strong")
     @JsonProperty("sillage")
     private String sillage;
 
