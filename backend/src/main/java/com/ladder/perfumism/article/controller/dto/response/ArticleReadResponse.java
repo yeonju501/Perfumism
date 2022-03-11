65f8c84634a94a6016fa60c27dc166043d53c734
vtaco <woosj7129@naver.com>
2022-03-11 15:41:13 +0900

    [S06P22D105-23] feat: ArticleReadResponse 생성자 구현

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadResponse.java
@@ -1,6 +1,7 @@
 package com.ladder.perfumism.article.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.article.domain.Article;
 import com.ladder.perfumism.article.domain.ArticleSubject;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -22,5 +23,28 @@ public class ArticleReadResponse {
 
     @JsonProperty("createAt")
     private LocalDateTime createAt;
+
+    public ArticleReadResponse(){
+
+    }
+
+    public ArticleReadResponse(Long articleId, ArticleSubject subject, String title, String content,
+        LocalDateTime createdAt){
+        this.articleId = articleId;
+        this.subject = subject;
+        this.title = title;
+        this.content = content;
+        this.createAt = createdAt;
+    }
+
+    public static ArticleReadResponse from(Article article){
+        return new ArticleReadResponse(
+            article.getId(),
+            article.getSubject(),
+            article.getTitle(),
+            article.getContent(),
+            article.getCreatedAt()
+        );
+    }
 
 }
