7929229021507e5053705ad5ae6189787a385f03
vtaco <woosj7129@naver.com>
2022-03-11 15:40:13 +0900

    [S06P22D105-23] feat: ArticleReadResponse Json 선언

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadResponse.java
@@ -1,5 +1,26 @@
 package com.ladder.perfumism.article.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.article.domain.ArticleSubject;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
 public class ArticleReadResponse {
+
+    @JsonProperty("article_id")
+    private Long articleId;
+
+    @JsonProperty("subject")
+    private ArticleSubject subject;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("createAt")
+    private LocalDateTime createAt;
 
 }
