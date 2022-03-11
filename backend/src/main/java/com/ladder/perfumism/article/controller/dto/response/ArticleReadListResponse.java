2f41fa43fff941669b1e98deb08440d550eb47e5
vtaco <woosj7129@naver.com>
2022-03-11 15:42:27 +0900

    [S06P22D105-23] feat: ArticleReadListResponse 생성자 구현

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
@@ -1,7 +1,10 @@
 package com.ladder.perfumism.article.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.article.domain.Article;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 
 public class ArticleReadListResponse {
 
@@ -14,6 +17,26 @@ public class ArticleReadListResponse {
 
     @JsonProperty("current_page_count")
     private int currentPageCount;
+
+    public ArticleReadListResponse(){
+
+    }
+
+    public ArticleReadListResponse(List<ArticleReadResponse> articleList, int totalPageCount, int currentPageCount){
+        this.articleList = articleList;
+        this.totalPageCount = totalPageCount;
+        this.currentPageCount = currentPageCount;
+    }
+
+    public static ArticleReadListResponse from(Page<Article> articleList){
+        return new ArticleReadListResponse(
+            articleList.stream()
+                .map(ArticleReadResponse::from)
+                .collect(Collectors.toList()),
+            articleList.getTotalPages(),
+            articleList.getNumber()
+        );
+    }
 
 
 }
