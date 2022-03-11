76b1d465da65079000db0b2700260ee564b1aa33
vtaco <woosj7129@naver.com>
2022-03-11 23:31:57 +0900

    [S06P22D105-87] feat: ArticleReadListResponse ApiModelProperty - Swagger

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
@@ -2,6 +2,7 @@ package com.ladder.perfumism.article.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.article.domain.Article;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
@@ -10,12 +11,15 @@ public class ArticleReadListResponse {
 
 
     @JsonProperty("articleList")
+    @ApiModelProperty(position = 0, notes = "게시글")
     private List<ArticleReadResponse> articleList;
 
     @JsonProperty("total_page_count")
+    @ApiModelProperty(position = 1, notes = "게시글 총 페이지 수")
     private int totalPageCount;
 
     @JsonProperty("current_page_count")
+    @ApiModelProperty(position = 2, notes = "게시글 현재 페이지")
     private int currentPageCount;
 
     public ArticleReadListResponse(){
