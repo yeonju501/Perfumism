68e2d81148d65afe4e10cc75b4d6945ba704eeae
vtaco <woosj7129@naver.com>
2022-03-11 23:29:35 +0900

    [S06P22D105-87] feat: ArticleRestController ApiOperation - Swagger

--- backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/ArticleRestController.java
@@ -6,6 +6,7 @@ import com.ladder.perfumism.article.controller.dto.response.ArticleReadListRespo
 import com.ladder.perfumism.article.service.ArticleService;
 import com.ladder.perfumism.perfume.controller.dto.response.PerfumeDetailResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import java.net.URI;
 import org.springframework.data.domain.Pageable;
@@ -31,7 +32,7 @@ public class ArticleRestController {
     }
 
     @PostMapping
-    @ApiOperation(value = "게시글 작성", notes = "게시글 작성하는 api 입니다")
+    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성 API")
         public ResponseEntity<Void> createArticle(@RequestBody ArticleCreateRequest request){
 
         articleService.articleCreate(request);
@@ -41,12 +42,15 @@ public class ArticleRestController {
     }
 
     @GetMapping
+    @ApiOperation(value = "게시글 목록 조회", notes = "게시글 목록을 받아오는 API")
     public ResponseEntity<ArticleReadListResponse> getArticleList(
         @PageableDefault(sort = "id", direction = Direction.DESC) Pageable pageable){
         return ResponseEntity.ok().body(articleService.showArticleList(pageable));
     }
 
     @GetMapping("/{article_id}")
+    @ApiOperation(value = "게시글 상세 조회", notes = "게시글을 선택했을 때 선택한 게시글을 받아오는 API")
+    @ApiImplicitParam(name = "article_id", value = "게시글 ID", required = true)
     public ResponseEntity<ArticleReadDetailResponse> getArticleDetail(@PathVariable(value = "article_id") Long articleId) {
         return ResponseEntity.ok().body(articleService.showArticleDetail(articleId));
     }
