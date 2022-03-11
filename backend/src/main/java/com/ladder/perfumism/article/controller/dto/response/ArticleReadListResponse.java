684130a6a43486dd24496cfa9c138046f8fadeb5
vtaco <woosj7129@naver.com>
2022-03-11 15:42:10 +0900

    [S06P22D105-23] feat: ArticleReadListResponse Json 선언

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadListResponse.java
@@ -1,5 +1,19 @@
 package com.ladder.perfumism.article.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
 public class ArticleReadListResponse {
+
+
+    @JsonProperty("articleList")
+    private List<ArticleReadResponse> articleList;
+
+    @JsonProperty("total_page_count")
+    private int totalPageCount;
+
+    @JsonProperty("current_page_count")
+    private int currentPageCount;
+
 
 }
