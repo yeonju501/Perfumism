81cb290a32cbf688ad595f02c23e46c58ea570e7
우동진 <wjinh93@gmail.com>
2022-03-11 15:19:54 +0900

    [S06P22D105-95] feat: password 유효성 검사 함수 추가

--- frontend/src/utils/formValidator.ts
+++ frontend/src/utils/formValidator.ts
@@ -2,3 +2,8 @@ export const validateEmailForm = (email: string) => {
 	const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 	return regExp.test(email);
 };
+
+export const validatePassword = (password: string) => {
+	const regExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,20}$/;
+	return regExp.test(password);
+};
