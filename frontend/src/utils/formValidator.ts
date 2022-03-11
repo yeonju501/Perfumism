10e600d7d19c1480b8f98939e1fc4b51723ef04a
우동진 <wjinh93@gmail.com>
2022-03-11 15:15:21 +0900

    [S06P22D105-95] feat: email 유효성 검사 함수 추가

---
+++ frontend/src/utils/formValidator.ts
@@ -0,0 +1,4 @@
+export const validateEmailForm = (email: string) => {
+	const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+	return regExp.test(email);
+};
