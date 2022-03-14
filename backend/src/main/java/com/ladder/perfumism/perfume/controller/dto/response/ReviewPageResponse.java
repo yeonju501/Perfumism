e37b1d7d65492f293089f817e6911a3e83fa8343
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-14 15:19:40 +0900

    [S06P22D105-34] feat: ReviewPageResponse 에 Swagger 작성

--- backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/ReviewPageResponse.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/ReviewPageResponse.java
@@ -2,6 +2,7 @@ package com.ladder.perfumism.perfume.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.perfume.domain.Review;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
@@ -9,12 +10,15 @@ import org.springframework.data.domain.Page;
 public class ReviewPageResponse {
 
     @JsonProperty("reviews")
+    @ApiModelProperty(position = 0, notes = "리뷰 리스트")
     private List<ReviewResponse> reviewResponses;
 
     @JsonProperty("total_page_count")
+    @ApiModelProperty(position = 1, notes = "전체 페이지", example = "2")
     private int totalPageCount;
 
     @JsonProperty("current_page_count")
+    @ApiModelProperty(position = 2, notes = "현재 페이지", example = "0")
     private int currentPageCount;
 
     public ReviewPageResponse() {
