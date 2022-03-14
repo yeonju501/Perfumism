73e07456072e8bf1577f70a36707177209f5124f
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-14 14:07:45 +0900

    [S06P22D105-34] feat: ReviewRepository 에 findByPerfumeId 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/domain/ReviewRepository.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/domain/ReviewRepository.java
@@ -1,7 +1,10 @@
 package com.ladder.perfumism.perfume.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    Page<Review> findByPerfumeId(Perfume perfume, Pageable pageable);
 }
