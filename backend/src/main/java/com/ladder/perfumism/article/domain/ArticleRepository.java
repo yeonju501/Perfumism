69bd493cbc87baff07a3535c8c58e26087249039
vtaco <woosj7129@naver.com>
2022-03-14 16:30:24 +0900

    [S06P22D105-29] feat: ArticleRepository findBySubject() 구현 - 말머리를 사용한 조회

--- backend/src/main/java/com/ladder/perfumism/article/domain/ArticleRepository.java
+++ backend/src/main/java/com/ladder/perfumism/article/domain/ArticleRepository.java
@@ -1,7 +1,12 @@
 package com.ladder.perfumism.article.domain;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 
+    Page<Article> findBySubject(ArticleSubject subjects, Pageable pageable);
 }
