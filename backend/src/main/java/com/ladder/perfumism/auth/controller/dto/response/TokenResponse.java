c33d4bb146a34478a3e10582f6cec720c727c85d
yeonju501 <loling970124@gmail.com>
2022-03-14 18:08:00 +0900

    [S06P22D105-80] feat: TokenResponse swagger 설명 추가

--- backend/src/main/java/com/ladder/perfumism/auth/controller/dto/response/TokenResponse.java
+++ backend/src/main/java/com/ladder/perfumism/auth/controller/dto/response/TokenResponse.java
@@ -1,6 +1,7 @@
 package com.ladder.perfumism.auth.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,9 +9,11 @@ import lombok.Getter;
 public class TokenResponse {
 
     @JsonProperty("access_token")
+    @ApiModelProperty(position = 0, notes = "access token", example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5ZW9uanVAbmF2ZXIuY29tMTEiLCJhdXRoIjoiQTAwIiwiaXNzIjoibGFkZGVyIiwiZXhwIjoxNjQ3MjQ5NTIyLCJpYXQiOjE2NDcyNDc3MjJ9.huloVc_rYaLo83V_aZSAFRvZtRPSFgG5H7fZn_DKwaVSW_aKdVO8cmPPQPqTcLgip7-_htWVJdsUaOPLqxj13Q")
     private String accessToken;
 
     @JsonProperty("refresh_token")
+    @ApiModelProperty(position = 1, notes = "refresh token", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NDc4NTI1MjN9.aMX4FK1a0nsbIXMQLNHfpL_VrFkyU39Suc_ihoXFoeWvlm8CbAo32RhO-Dncn9tHaMx9p7sfqzrbV3YseJgKiw")
     private String refreshToken;
 
     public TokenResponse() {
