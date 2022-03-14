5421cf6862a9f68459ea1d3b5b36b433bb229317
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-14 11:08:37 +0900

    [S06P22D105-104] feat: 추천 메인페이지 UI 구조 생성

--- frontend/src/pages/RecommendPage.tsx
+++ frontend/src/pages/RecommendPage.tsx
@@ -1,5 +1,17 @@
 function RecommendPage() {
-	return <></>;
+	return (
+		<div>
+			<div>
+				<h1>Find your signature scent</h1>
+			</div>
+			<div>
+				<button>누구나 자신만의 향기가 있습니다.</button>
+			</div>
+			<div>
+				<button>자신만의 향기가 있는 당신, 비슷한 향기를 찾아보세요</button>
+			</div>
+		</div>
+	);
 }
 
 export default RecommendPage;
