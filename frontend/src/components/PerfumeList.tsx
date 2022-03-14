edc1192b5ddb14fc3e029b9bc5d98ea925df3e1f
9603100 <imgoing222@gmail.com>
2022-03-14 15:20:58 +0900

    [S06P22D105-109] feat: UI 초안

--- frontend/src/components/PerfumeList.tsx
+++ frontend/src/components/PerfumeList.tsx
@@ -1,5 +1,14 @@
-function PerfumeList() {
-	return;
+function PerfumeList({ perfumeList }) {
+	return (
+		<div>
+			{perfumeList.map((perfume) => (
+				<div>
+					<img src={perfume.image} alt="perfume image" />
+					<p>{perfume.perfume_name}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default PerfumeList;
