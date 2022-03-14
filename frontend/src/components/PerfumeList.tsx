8618bdec238ca4e6efedf227c28d9d1e85ae7594
9603100 <imgoing222@gmail.com>
2022-03-14 15:21:15 +0900

    [S06P22D105-109] feat: type Perfume 생성

--- frontend/src/components/PerfumeList.tsx
+++ frontend/src/components/PerfumeList.tsx
@@ -1,7 +1,14 @@
+type Perfume = {
+	perfume_id: number;
+	perfume_name: string;
+	brand_name: string;
+	image: string;
+};
+
 function PerfumeList({ perfumeList }) {
 	return (
 		<div>
-			{perfumeList.map((perfume) => (
+			{perfumeList.map((perfume: Perfume) => (
 				<div>
 					<img src={perfume.image} alt="perfume image" />
 					<p>{perfume.perfume_name}</p>
