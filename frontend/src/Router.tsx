c39f529400181913bdff10c0b2eeac5a188afef4
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-15 14:41:42 +0900

    [S06P22D105-104] feat: 설문 첫 페이지 라우터 설정

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,5 +1,6 @@
 import PerfumeDetail from "pages/PerfumeDetail";
 import RecommendPage from "pages/RecommendPage";
+import FirstSurveyPage from "pages/FirstSurveyPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Router() {
@@ -9,6 +10,7 @@ function Router() {
 				<Route path="/" />
 				<Route path="/perfume/:perfumeId" element={<PerfumeDetail />} />
 				<Route path="/recommend" element={<RecommendPage />} />
+				<Route path="/recommend/survey" element={<FirstSurveyPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
