fb5b49db0ff3c3b37445a687543e7761f577ab19
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-15 14:43:01 +0900

    [S06P22D105-104] feat: 추천 메인페이지와 설문 첫 페이지 연결

--- frontend/src/pages/RecommendPage.tsx
+++ frontend/src/pages/RecommendPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function RecommendPage() {
 	return (
 		<div>
@@ -5,7 +7,9 @@ function RecommendPage() {
 				<h1>Find your signature scent</h1>
 			</div>
 			<div>
-				<button>누구나 자신만의 향기가 있습니다.</button>
+				<Link to={"/recommend/survey"}>
+					<button>누구나 자신만의 향기가 있습니다.</button>
+				</Link>
 			</div>
 			<div>
 				<button>자신만의 향기가 있는 당신, 비슷한 향기를 찾아보세요</button>
