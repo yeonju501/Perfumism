fed927f9494701b75dc2a75aa97ab25f41760e37
9603100 <imgoing222@gmail.com>
2022-03-16 11:25:00 +0900

    [S06P22D105-109] feat: 향수 상세 정보 렌더링

--- frontend/src/pages/PerfumeDetail.tsx
+++ frontend/src/pages/PerfumeDetail.tsx
@@ -1,45 +1,84 @@
 import axios from "axios";
+import PerfumeList from "components/PerfumeList";
 import { useEffect, useState } from "react";
+
+type perfumeDataType = {
+	perfume_id: number;
+	perfume_name: string;
+	brand: {
+		brand_id: number;
+		brand_name: string;
+	};
+	image: string;
+	launch_year: number;
+	average_grade: number;
+	top_notes: string;
+	middle_notes: string | null;
+	base_notes: string | null;
+	total_survey: number;
+	longevity: string;
+	sillage: string;
+	accords: [];
+	similar_perfume: [];
+};
+
+type accordType = {
+	accord_id: number;
+	kor_name: string;
+	eng_name: string;
+};
 
 function PerfumeDetail() {
 	const URL = process.env.REACT_APP_MAIN_URL;
-	const [perfumeData, setPerfumeData] = useState({});
+	const [perfumeData, setPerfumeData] = useState<perfumeDataType | null>(null);
 
 	useEffect(() => {
 		getPerfumeData();
-	});
+	}, []);
 
 	const getPerfumeData = () => {
 		axios({
 			url: `${URL}/api/perfumes/1`,
 			method: "GET",
-		}).then((res) => setPerfumeData(res));
+		}).then((res) => {
+			console.log(res.data);
+			setPerfumeData(res.data);
+		});
 	};
 
 	return (
-		<div>
+		perfumeData && (
 			<div>
-				<img />
-				<h1>향수 이름</h1>
-				<h3>출시년도</h3>
-				<h3>브랜드명</h3>
-				<h3>평점</h3>
-				<p>main accords</p>
+				<div>
+					<img />
+					<h1>{perfumeData.perfume_name}</h1>
+					<h3>{perfumeData.launch_year}</h3>
+					<h3>{perfumeData.brand.brand_name}</h3>
+					<h3>{perfumeData.average_grade}</h3>
+					<p>main accords</p>
+					<ul>
+						{perfumeData.accords.map((accord: accordType) => (
+							<li key={accord.accord_id}>{accord.eng_name}</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<p>{perfumeData.top_notes}</p>
+					<p>{perfumeData.middle_notes}</p>
+					<p>{perfumeData.base_notes}</p>
+					<p>{perfumeData.longevity}</p>
+					<p>{perfumeData.sillage}</p>
+				</div>
+				<div>
+					<p>000과 비슷한 향수</p>
+					{perfumeData && <PerfumeList perfumes={perfumeData.similar_perfume} />}
+					<p>브랜드의 다른 향수</p>
+				</div>
+				<div>
+					<p>Review 컴포넌트</p>
+				</div>
 			</div>
-			<div>
-				<p>notes</p>
-				<p>지속력</p>
-				<p>잔향</p>
-			</div>
-			<div>
-				{/* 향수 사진+좋아요버튼 목록을 컴포넌트화 할것인가...? => 어디에 쓸수 있는가? */}
-				<p>000과 비슷한 향수</p>
-				<p>브랜드의 다른 향수</p>
-			</div>
-			<div>
-				<p>Review 컴포넌트</p>
-			</div>
-		</div>
+		)
 	);
 }
 
