09cada811db4580d3f37556d79bcadfc576b7df2
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-16 15:40:22 +0900

    [S06P22D105-104] feat: url 쿼리스트링 달기

--- frontend/src/pages/FirstSurveyPage.tsx
+++ frontend/src/pages/FirstSurveyPage.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 function FirstSurveyPage() {
+	const navigate = useNavigate();
+
 	return (
 		<div>
 			<h1>첫 번째 항목</h1>
+			<button
+				onClick={() => {
+					navigate({
+						pathname: "/survey",
+						search: "?a1=1",
+					});
+				}}
+			>
+				다음페이지
+			</button>
 		</div>
 	);
 }
