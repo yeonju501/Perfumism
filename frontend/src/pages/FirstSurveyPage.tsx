6a329784c1a1f09a3d5b3cecaa5a871aac06402c
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-16 16:10:42 +0900

    [S06P22D105-104] feat: 설문 페이지에 따른 조건부 렌더링 추가

--- frontend/src/pages/FirstSurveyPage.tsx
+++ frontend/src/pages/FirstSurveyPage.tsx
@@ -7,29 +7,36 @@ function FirstSurveyPage() {
 
 	return (
 		<div>
-			<h1>첫 번째 항목</h1>
-			<button
-				onClick={() => {
-					navigate({
-						pathname: "/survey",
-						search: "?page=2&a1=1",
-					});
-				}}
-			>
-				다음페이지
-			</button>
-			<h1>두 번째 항목</h1>
-			<button
-				onClick={() => {
-					navigate({
-						pathname: "/survey",
-						search: "?page=3&a1=1&a2=3",
-					});
-				}}
-			>
-				다음페이지
-			</button>
-			<p>페이지 : {page}</p>
+			{page === "1" ? (
+				<div>
+					<h1>첫 번째 항목</h1>
+					<button
+						onClick={() => {
+							navigate({
+								pathname: "/survey",
+								search: "?page=2&a1=1",
+							});
+						}}
+					>
+						다음페이지
+					</button>
+				</div>
+			) : null}
+			{page === "2" ? (
+				<div>
+					<h1>두 번째 항목</h1>
+					<button
+						onClick={() => {
+							navigate({
+								pathname: "/survey",
+								search: "?page=3&a1=1&a2=3",
+							});
+						}}
+					>
+						다음페이지
+					</button>
+				</div>
+			) : null}
 		</div>
 	);
 }
