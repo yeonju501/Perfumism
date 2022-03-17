1b7e9a9af5261b329fed67b14cb30b11972976b9
우동진 <wjinh93@gmail.com>
2022-03-17 15:33:57 +0900

    [S06P22D105-114] feat: 필요없는 함수 제거

--- frontend/src/utils/formValidator.ts
+++ frontend/src/utils/formValidator.ts
@@ -1,25 +1,15 @@
-export const validateEmailForm = (email: string) => {
+export const validateEmailForm = (email: string): boolean => {
 	const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-	return regExp.test(email);
+	if (email) return regExp.test(email);
+	return false;
 };
 
-export const validateEmailEmpty = (email: string) => {
-	return email ? true : false;
+export const validatePassword = (password: string): boolean => {
+	const regExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,20}$/;
+	if (password) return regExp.test(password);
+	return false;
 };
 
-export const validatePassword = (password: string) => {
-	const regExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,20}$/;
-	return regExp.test(password);
-};
-
-export const validatePasswordEmpty = (password: string) => {
-	return password ? true : false;
-};
-
-export const validatenameEmpty = (nickname: string) => {
-	return nickname ? true : false;
-};
-
-export const validateNicknameLength = (nickname: string) => {
+export const validateNickname = (nickname: string): boolean => {
 	return nickname.length > 2 && nickname.length < 10 ? true : false;
 };
