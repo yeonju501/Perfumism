6a65b8b0a9583d60aa41a218a597be45839736ba
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-17 12:11:07 +0900

    [S06P22D105-104] feat: answer 데이터 바인딩 임시

--- frontend/src/components/SurveyItem.tsx
+++ frontend/src/components/SurveyItem.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SurveyItem() {
 	const navigate = useNavigate();
+	const [answer, setAnswer] = useState<string>("");
 
 	const nextPage = () => {
 		navigate({
@@ -11,9 +12,29 @@ function SurveyItem() {
 		});
 	};
 
+	const answerHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setAnswer(e.target.value);
+	};
+
 	return (
 		<div>
 			<h1>질문</h1>
+			<label>
+				<input type="radio" name="answer" value="1" onChange={answerHandleChange} />
+				1번
+			</label>
+			<label>
+				<input type="radio" name="answer" value="2" onChange={answerHandleChange} />
+				2번
+			</label>
+			<label>
+				<input type="radio" name="answer" value="3" onChange={answerHandleChange} />
+				3번
+			</label>
+			<label>
+				<input type="radio" name="answer" value="4" onChange={answerHandleChange} />
+				4번
+			</label>
 			<button onClick={nextPage}>다음페이지</button>
 		</div>
 	);
