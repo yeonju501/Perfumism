15288314ca5b632223bca139c985bd3e766beaa2
우동진 <wjinh93@gmail.com>
2022-03-17 15:51:50 +0900

    [S06P22D105-45] feat: navigate 및 import

---
+++ frontend/src/pages/account/signup/SignUp.tsx
@@ -0,0 +1,10 @@
+import { authApi } from "apis";
+import LoginButton from "components/account/LoginButton";
+import { useNavigate } from "react-router";
+import useForm from "./hooks/useForm";
+
+function SignUp() {
+	const navigate = useNavigate();
+}
+
+export default SignUp;
