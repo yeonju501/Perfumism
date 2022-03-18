47bc643b9c60667ed240ae934d4e92f564d21298
우동진 <wjinh93@gmail.com>
2022-03-18 23:16:55 +0900

    [S06P22D105-129] feat: Link Component로 변경

---
+++ frontend/src/components/account/Link.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const LinkParagraph = styled(Link)`
+	font-size: 1.4rem;
+	font-weight: 700;
+	text-decoration: none;
+	margin-top: 3rem;
+	color: black;
+`;
+
+export default LinkParagraph;
