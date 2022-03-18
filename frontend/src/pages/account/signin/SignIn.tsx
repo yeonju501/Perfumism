064e358c775faeecea4d194b836b829786abaa57
우동진 <wjinh93@gmail.com>
2022-03-18 23:24:18 +0900

    [S06P22D105-50] feat: Container, Header 추가

--- frontend/src/pages/account/signin/SignIn.tsx
+++ frontend/src/pages/account/signin/SignIn.tsx
@@ -1,5 +1,13 @@
+import { useNavigate } from "react-router";
+import { FormContainer, Container } from "components/account/Container";
+import { Button, Input, Label, ErrorText, Header, LinkParagraph } from "components/account/Index";
+
 function SignIn() {
-	return <div>SignIn</div>;
+	return (
+		<Container>
+			<Header>로그인</Header>
+		</Container>
+	);
 }
 
 export default SignIn;
