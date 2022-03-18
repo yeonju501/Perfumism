daba9956ae1df469773a81fa9f9ce8ee42ac389b
우동진 <wjinh93@gmail.com>
2022-03-18 23:32:38 +0900

    [S06P22D105-50] feat: api주소 변경

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -8,13 +8,13 @@ interface UserApiType {
 		username: string;
 	}) => Promise<AxiosResponse>;
 	isExist: (name: string, value: string) => Promise<AxiosResponse<{ result: boolean }>>;
-	login: (userInfo: { email: string; password: string }) => Promise<AxiosResponse>;
+	signin: (userInfo: { email: string; password: string }) => Promise<AxiosResponse>;
 }
 
 const authApi: UserApiType = {
 	signup: (userInfo) => request.post("members/join", userInfo),
 	isExist: (name, value) => request.post(`members/exist-${name}`, value),
-	login: (userInfo) => request.post("/auth/login", userInfo),
+	signin: (userInfo) => request.post("members/login", userInfo),
 };
 
 export default authApi;
