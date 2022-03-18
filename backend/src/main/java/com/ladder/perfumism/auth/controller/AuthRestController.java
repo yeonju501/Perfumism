e1cbfb67f2ff6f1159d1f86f1f352c9452602521
yeonju501 <loling970124@gmail.com>
2022-03-18 23:49:49 +0900

    [S06P22D105-124] feat: AuthRestController 리프레시 토큰 전송 방식 변경

--- backend/src/main/java/com/ladder/perfumism/auth/controller/AuthRestController.java
+++ backend/src/main/java/com/ladder/perfumism/auth/controller/AuthRestController.java
@@ -2,10 +2,11 @@ package com.ladder.perfumism.auth.controller;
 
 import com.ladder.perfumism.auth.controller.dto.request.LoginRequest;
 import com.ladder.perfumism.auth.controller.dto.request.TokenRequest;
-import com.ladder.perfumism.auth.controller.dto.response.TokenResponse;
+import com.ladder.perfumism.auth.controller.dto.response.AccessTokenResponse;
 import com.ladder.perfumism.auth.service.AuthService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,13 +26,14 @@ public class AuthRestController {
 
     @PostMapping("/members/login")
     @ApiOperation(value = "로그인", notes = "로그인 API")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok().body(authService.login(request));
+    public ResponseEntity<AccessTokenResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+
+        return ResponseEntity.ok().body(authService.login(request, response));
     }
 
     @PostMapping("/members/reissue")
     @ApiOperation(value = "토큰 재발급", notes = "토큰 재발급 API")
-    public ResponseEntity<TokenResponse> reissue(@RequestBody TokenRequest request) {
-        return ResponseEntity.ok().body(authService.reissue(request));
+    public ResponseEntity<AccessTokenResponse> reissue(@RequestBody TokenRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok().body(authService.reissue(request, response));
     }
 }
