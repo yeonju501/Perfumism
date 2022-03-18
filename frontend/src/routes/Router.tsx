bdc94f87d86d7d9d1863060bb20c7cf949982918
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-18 18:43:26 +0900

    [S06P22D105-105] feat: 설문 결과 페이지 라우터 설정

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,6 +2,7 @@ import SignUp from "pages/account/signup/SignUp";
 import PerfumeDetail from "pages/PerfumeDetail";
 import Recommend from "pages/recommend/Recommend";
 import Survey from "pages/recommend/Survey";
+import SurveyResult from "pages/recommend/SurveyResult";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
 				<Route path="/perfume/:perfumeId" element={<PerfumeDetail />} />
 				<Route path="/recommend" element={<Recommend />} />
 				<Route path="/survey" element={<Survey />} />
+				<Route path="/survey/result" element={<SurveyResult />} />
 				<Route path="/signup" element={<SignUp />} />
 			</Routes>
 		</BrowserRouter>
