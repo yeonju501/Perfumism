ba1ecd4f9dbd09608948839c340867b5ea3fe409
우동진 <wjinh93@gmail.com>
2022-03-18 23:17:10 +0900

    [S06P22D105-129] feat: index.ts 생성

---
+++ frontend/src/components/account/Index.ts
@@ -0,0 +1,6 @@
+export { default as Input } from "./Input";
+export { default as Header } from "./Header";
+export { default as Label } from "./Label";
+export { default as LinkParagraph } from "./Link";
+export { default as Button } from "./Button";
+export { default as ErrorText } from "./ErrorsText";
