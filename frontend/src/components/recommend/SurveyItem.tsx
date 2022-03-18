2a6a4e165560e28afe4b9a69497dd686ac6758ec
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-18 18:08:19 +0900

    [S06P22D105-105] feat: 설문 데이터 전달

--- frontend/src/components/recommend/SurveyItem.tsx
+++ frontend/src/components/recommend/SurveyItem.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function SurveyItem({ queryString }: any) {
+interface SurveyItemProps {
+	queryString: string;
+	surveyListItem: { 질문: string; 답변: string[] };
+}
+
+function SurveyItem({ queryString, surveyListItem }: SurveyItemProps) {
 	const navigate = useNavigate();
 	const [answer, setAnswer] = useState("");
 
@@ -26,23 +31,13 @@ function SurveyItem({ queryString }: any) {
 
 	return (
 		<div>
-			<h1>질문</h1>
-			<label>
-				<input type="radio" name="answer" value="1" onChange={answerHandleChange} />
-				1번
-			</label>
-			<label>
-				<input type="radio" name="answer" value="2" onChange={answerHandleChange} />
-				2번
-			</label>
-			<label>
-				<input type="radio" name="answer" value="3" onChange={answerHandleChange} />
-				3번
-			</label>
-			<label>
-				<input type="radio" name="answer" value="4" onChange={answerHandleChange} />
-				4번
-			</label>
+			<h1>{surveyListItem["질문"]}</h1>
+			{surveyListItem["답변"].map((answer: string, idx: number) => (
+				<label key={idx}>
+					<input type="radio" name="answer" value={idx} onChange={answerHandleChange} />
+					{answer}
+				</label>
+			))}
 			<button onClick={nextPage}>다음페이지</button>
 		</div>
 	);
--- frontend/src/pages/recommend/Survey.tsx
+++ frontend/src/pages/recommend/Survey.tsx
@@ -6,20 +6,30 @@ function Survey() {
 	const page = searchParams.get("page");
 	const queryString = useLocation().search;
 	const surveyList = [
-		{ "질문 1": ["a1", "a2", "a3", "a4"] },
-		{ "질문 2": ["a1", "a2", "a3", "a4"] },
-		{ "질문 3": ["a1", "a2", "a3", "a4", "a5"] },
-		{ "질문 4": ["a1", "a2", "a3", "a4"] },
-		{ "질문 5": ["a1", "a2", "a3", "a4", "a5"] },
+		{ 질문: "질문1", 답변: ["a1", "a2", "a3", "a4"] },
+		{ 질문: "질문2", 답변: ["a1", "a2", "a3", "a4", "a5"] },
+		{ 질문: "질문3", 답변: ["a1", "a2", "a3", "a4"] },
+		{ 질문: "질문4", 답변: ["a1", "a2", "a3", "a4", "a5"] },
+		{ 질문: "질문5", 답변: ["a1", "a2", "a3", "a4"] },
 	];
 
 	return (
 		<div>
-			{page === "1" ? <SurveyItem queryString={queryString} /> : null}
-			{page === "2" ? <SurveyItem queryString={queryString} /> : null}
-			{page === "3" ? <SurveyItem queryString={queryString} /> : null}
-			{page === "4" ? <SurveyItem queryString={queryString} /> : null}
-			{page === "5" ? <SurveyItem queryString={queryString} /> : null}
+			{page === "1" ? (
+				<SurveyItem queryString={queryString} surveyListItem={surveyList[0]} />
+			) : null}
+			{page === "2" ? (
+				<SurveyItem queryString={queryString} surveyListItem={surveyList[1]} />
+			) : null}
+			{page === "3" ? (
+				<SurveyItem queryString={queryString} surveyListItem={surveyList[2]} />
+			) : null}
+			{page === "4" ? (
+				<SurveyItem queryString={queryString} surveyListItem={surveyList[3]} />
+			) : null}
+			{page === "5" ? (
+				<SurveyItem queryString={queryString} surveyListItem={surveyList[4]} />
+			) : null}
 		</div>
 	);
 }
