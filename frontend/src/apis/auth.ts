92dcf31c28f8db1e48fc54b54c57d5e9efb16194
우동진 <wjinh93@gmail.com>
2022-03-21 20:12:05 +0900

    [S06P22D106-51] feat: findPassword request 추가

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from "axios";
+import { Axios, AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface UserApiType {
@@ -10,6 +10,7 @@ interface UserApiType {
 	isExist: (name: string, value: string) => Promise<AxiosResponse<{ result: boolean }>>;
 	signin: (userInfo: { email: string; password: string }) => Promise<AxiosResponse>;
 	reissue: (refreshToken: string) => Promise<AxiosResponse>;
+	findPassword:(email:string) => Promise<AxiosResponse<{code:string}>
 }
 
 const authApi: UserApiType = {
@@ -17,6 +18,7 @@ const authApi: UserApiType = {
 	isExist: (name, value) => request.post(`members/exist-${name}`, value),
 	signin: (userInfo) => request.post("members/login", userInfo),
 	reissue: (refreshToken) => request.post("members/reissue", { refresh_token: refreshToken }),
+	findPassword: (email) => request.post("/members/find-pw", email),
 };
 
 export default authApi;
