285894be24e57ec99fc5c9e46793d04047441e50
yeonju501 <loling970124@gmail.com>
2022-03-21 16:32:05 +0900

    [S06P22D105-153] feat: FindPasswordRequest 스웨거 설정 추가

--- backend/src/main/java/com/ladder/perfumism/member/controller/dto/request/FindPasswordRequest.java
+++ backend/src/main/java/com/ladder/perfumism/member/controller/dto/request/FindPasswordRequest.java
@@ -1,12 +1,14 @@
 package com.ladder.perfumism.member.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class FindPasswordRequest {
 
     @JsonProperty("email")
+    @ApiModelProperty(name = "email", example = "loling3@naver.com")
     private String email;
 
     public FindPasswordRequest() {
