ce1d52bf67cbc2b9389c8749dd946ee50d9ef26c
9603100 <imgoing222@gmail.com>
2022-03-21 11:16:12 +0900

    [S06P22D105-60] feat: getReviews api 요청 함수 추가

--- frontend/src/apis/review.ts
+++ frontend/src/apis/review.ts
@@ -6,10 +6,12 @@ interface ReviewApiType {
 		review: { grade: number; content: string },
 		perfumeId: number,
 	) => Promise<AxiosResponse>;
+	getReviews: (perfumeId: number) => Promise<AxiosResponse>;
 }
 
 const reviewApi: ReviewApiType = {
 	createReview: (review, perfumeId) => request.post(`auth/reviews/perfumes/${perfumeId}`, review),
+	getReviews: (perfumeId) => request.get(`reviews/perfumes/${perfumeId}`),
 };
 
 export default reviewApi;
