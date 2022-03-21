b5742af2b56c0832967e1c06fe4976682f49ea34
9603100 <imgoing222@gmail.com>
2022-03-21 09:56:59 +0900

    [S06P22D105-137] feat: CreateButton 컴포넌트 생성

---
+++ frontend/src/components/button/CreateButton.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const CreateButton = styled.button`
+	color: black;
+	background-color: #cecece;
+	border: none;
+	cursor: pointer;
+`;
+
+export default CreateButton;
