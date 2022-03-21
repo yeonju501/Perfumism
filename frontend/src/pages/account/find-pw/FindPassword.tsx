757c7a22d00519b5c15aa316bc57e2c08180d7f3
우동진 <wjinh93@gmail.com>
2022-03-21 20:08:25 +0900

    [S06P22D106-51] feat: 비밀번호찾기 페이지 ui추가

--- frontend/src/pages/account/find-pw/FindPassword.tsx
+++ frontend/src/pages/account/find-pw/FindPassword.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Container, FormContainer } from "components/account/Container";
+import { Button, Input, Label, ErrorText, Header, LinkParagraph } from "components/account/Index";
 
 function FindPassword() {
-	return <div>FindPassword</div>;
+	return (
+		<Container>
+			<Header>비밀번호 찾기</Header>
+			<FormContainer>
+				<Label htmlFor="email">이메일</Label>
+				<Input name="email" />
+				<br />
+				<Button backgroundColor="black" color="#fff">
+					비밀번호 찾기
+				</Button>
+			</FormContainer>
+			<LinkParagraph to="/signup">회원가입하러 가기</LinkParagraph>
+			<LinkParagraph to="/signin">로그인하러 가기</LinkParagraph>
+		</Container>
+	);
 }
 
 export default FindPassword;
