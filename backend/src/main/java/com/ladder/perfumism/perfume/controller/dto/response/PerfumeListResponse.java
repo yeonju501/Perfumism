8b07bc4557a689d7e8eca04abf06101b36657141
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-21 15:57:43 +0900

    [S06P22D105-140] feat: PerfumeListResponse 추가

---
+++ backend/src/main/java/com/ladder/perfumism/perfume/controller/dto/response/PerfumeListResponse.java
@@ -0,0 +1,43 @@
+package com.ladder.perfumism.perfume.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.perfume.domain.PerfumeLike;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
+
+public class PerfumeListResponse {
+
+    @JsonProperty("perfumes")
+    @ApiModelProperty(position = 0, notes = "향수 리스트")
+    private List<PerfumeSimpleResponse> perfumeSimpleResponses;
+
+    @JsonProperty("total_page_count")
+    @ApiModelProperty(position = 1, notes = "전체 페이지", example = "2")
+    private int totalPageCount;
+
+    @JsonProperty("current_page_count")
+    @ApiModelProperty(position = 2, notes = "현재 페이지", example = "0")
+    private int currentPageCount;
+
+    public PerfumeListResponse() {
+    }
+
+    public PerfumeListResponse(
+        List<PerfumeSimpleResponse> perfumeSimpleResponses, int totalPageCount, int currentPageCount) {
+        this.perfumeSimpleResponses = perfumeSimpleResponses;
+        this.totalPageCount = totalPageCount;
+        this.currentPageCount = currentPageCount;
+    }
+
+    public static PerfumeListResponse from(Page<PerfumeLike> perfumes){
+        return new PerfumeListResponse(
+            perfumes.stream()
+                .map(PerfumeSimpleResponse::from)
+                .collect(Collectors.toList()),
+            perfumes.getTotalPages(),
+            perfumes.getNumber()
+        );
+    }
+}
