7e8082a22c35776f9953b904470ed31fab38929a
우동진 <wjinh93@gmail.com>
2022-03-21 20:17:17 +0900

    [S06P22D106-51] feat: useForm 적용

--- frontend/src/pages/account/find-pw/FindPassword.tsx
+++ frontend/src/pages/account/find-pw/FindPassword.tsx
@@ -1,7 +1,31 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import { authApi } from "apis";
 import { Container, FormContainer } from "components/account/Container";
 import { Button, Input, Label, ErrorText, Header, LinkParagraph } from "components/account/Index";
+import { formValidator } from "utils";
+import useForm from "../hooks/useForm";
 
 function FindPassword() {
+	const navigate = useNavigate();
+	const { handleChange, handleSubmit, errors } = useForm({
+		initialValues: {
+			email: "",
+		},
+		onSubmit: async ({ email }) => {
+			try {
+				await authApi.findPassword(email).then((res) => {
+					res.status === 200 ? navigate("/") : null;
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		validate: ({ email }) => {
+			const errors: { [key: string]: string } = {};
+			if (!formValidator.validateEmailForm(email)) errors.email = "올바른 이메일을 입력해주세요.";
+			return errors;
+		},
+	});
 	return (
 		<Container>
 			<Header>비밀번호 찾기</Header>
