4d15a8bdfe815c3e2c4d2d650904d939064a59ee
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-21 13:31:48 +0900

    [S06P22D105-71] feat: PerfumeLikeRepository 에 findByPerfumeIdAndMemberId 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/domain/PerfumeLikeRepository.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/domain/PerfumeLikeRepository.java
@@ -1,6 +1,7 @@
 package com.ladder.perfumism.perfume.domain;
 
 import com.ladder.perfumism.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PerfumeLikeRepository extends JpaRepository<PerfumeLike, Long> {
@@ -8,4 +9,6 @@ public interface PerfumeLikeRepository extends JpaRepository<PerfumeLike, Long> 
     Boolean existsByMemberIdAndPerfumeId(Member member, Perfume perfume);
 
     Integer countByPerfumeId(Perfume perfume);
+
+    Optional<PerfumeLike> findByPerfumeIdAndMemberId(Perfume perfume, Member member);
 }
