ab6b2909b04a86bd1da3048ffad52ddd6e372ea7
9603100 <imgoing222@gmail.com>
2022-03-21 13:54:28 +0900

    [S06P22D105-60] feat: 임시 데이터 할당 및 reviewApi parameter에 currentPage 추가

--- frontend/src/components/review/ReviewList.tsx
+++ frontend/src/components/review/ReviewList.tsx
@@ -1,4 +1,5 @@
 import reviewApi from "apis/review";
+import { ShowMoreButton } from "components/button/Button";
 import { useEffect, useState } from "react";
 import { Container } from "./Container";
 
@@ -10,23 +11,51 @@ interface ReviewType {
 	review_id: number;
 	member_id: number;
 	member_name: string;
-	member_iamge: string;
+	member_image: string;
 	grade: number;
 	content: string;
 	likes: number;
 }
 
 function ReviewList({ perfumeId }: ReviewListPropType) {
-	const [reviews, setReviews] = useState([]);
+	const [reviews, setReviews] = useState([
+		{
+			review_id: 1,
+			member_id: 1,
+			member_name: "이승기",
+			member_image: "eocnddlalwlvkdlfdlfma.jpg",
+			grade: 4,
+			content: "이 향수는 마치 꽃이 무수하게 핀 들판의 공기를 그대로 가져온듯 한 향...",
+			likes: 222,
+		},
+		{
+			review_id: 2,
+			member_id: 1,
+			member_name: "이승기",
+			member_image: "eocnddlalwlvkdlfdlfma.jpg",
+			grade: 4,
+			content: "이 향수는 마치 꽃이 무수하게 핀 들판의 공기를 그대로 가져온듯 한 향...",
+			likes: 222,
+		},
+		{
+			review_id: 3,
+			member_id: 1,
+			member_name: "이승기",
+			member_image: "eocnddlalwlvkdlfdlfma.jpg",
+			grade: 4,
+			content: "이 향수는 마치 꽃이 무수하게 핀 들판의 공기를 그대로 가져온듯 한 향...",
+			likes: 222,
+		},
+	]);
 	const [totalPage, setTotalPage] = useState(0);
 	const [currentPage, setCurrentPage] = useState(0);
 
 	useEffect(() => {
-		getReviews();
+		// getReviews();
 	}, []);
 
 	const getReviews = async () => {
-		await reviewApi.getReviews(perfumeId).then((res) => {
+		await reviewApi.getReviews(perfumeId, currentPage).then((res) => {
 			setReviews(res.data.reviews);
 			setTotalPage(res.data.total_page_count);
 			setCurrentPage(res.data.current_page_count);
@@ -34,11 +63,12 @@ function ReviewList({ perfumeId }: ReviewListPropType) {
 	};
 
 	return reviews.length > 0 ? (
-		<Container>
+		<ul>
 			{reviews.map((review: ReviewType) => (
-				<p>{review.content}</p>
+				<li key={review.review_id}>{review.content}</li>
 			))}
-		</Container>
+			<ShowMoreButton>Show More</ShowMoreButton>
+		</ul>
 	) : (
 		<p>작성된 리뷰가 없습니다</p>
 	);
