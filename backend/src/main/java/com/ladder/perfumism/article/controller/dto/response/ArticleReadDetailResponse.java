4cab7f3fe8ca2bd58ee5e7701d9328c34efd736e
vtaco <woosj7129@naver.com>
2022-03-21 16:08:31 +0900

    [S06P22D105-110] feat: ArticleReadDetailResponse member 변수 추가 - 회원 정보(아이디,이름,이미지)

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadDetailResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleReadDetailResponse.java
@@ -10,31 +10,51 @@ import lombok.Getter;
 @Getter
 public class ArticleReadDetailResponse  {
 
+    @JsonProperty("article_id")
+    @ApiModelProperty(position = 0, notes = "게시글 ID",example = "1")
+    private Long articleId;
+
+    @JsonProperty("member_id")
+    @ApiModelProperty(position = 1, notes = "멤버 ID", example = "1")
+    private Long memberId;
+
+    @JsonProperty("member_name")
+    @ApiModelProperty(position = 2, notes = "멤버 이름", example = "우사앙주운")
+    private String memberName;
+
+    @JsonProperty("member_image")
+    @ApiModelProperty(position = 3, notes = "멤버 사진", example = "잘생김.jpg")
+    private String memberImage;
+
     @JsonProperty("subject")
-    @ApiModelProperty(position = 0, notes = "게시글 id", example = "1")
+    @ApiModelProperty(position = 4, notes = "말머리", example = "TALK")
     private ArticleSubject subject;
 
     @JsonProperty("title")
-    @ApiModelProperty(position = 1, notes = "말머리", example = "TALK")
+    @ApiModelProperty(position = 5, notes = "제목", example = "제목입니다")
     private String title;
 
     @JsonProperty("content")
-    @ApiModelProperty(position = 2, notes = "제목", example = "제목입니다")
+    @ApiModelProperty(position = 6, notes = "내용", example = "내용이네요")
     private String content;
 
     @JsonProperty("created_at")
-    @ApiModelProperty(position = 3, notes = "내용", example = "내용입니다")
+    @ApiModelProperty(position = 7, notes = "생성 시간", example = "2022,3,13,14,59,51,0000000")
     private LocalDateTime createdAt;
 
     @JsonProperty("updated_at")
-    @ApiModelProperty(position = 4, notes = "생성 시간", example = "2022,3,13,14,59,51,0000000")
+    @ApiModelProperty(position = 8, notes = "수정 시간", example = "2022,3,13,14,59,51,0000000")
     private LocalDateTime updatedAt;
 
     public ArticleReadDetailResponse(){
 
     }
 
-    public ArticleReadDetailResponse(ArticleSubject subject, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt){
+    public ArticleReadDetailResponse(Long articleId, Long memberId, String memberName, String memberImage,ArticleSubject subject, String title, String content, LocalDateTime createdAt, LocalDateTime updatedAt){
+        this.articleId = articleId;
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.memberImage = memberImage;
         this.subject = subject;
         this.title = title;
         this.content = content;
@@ -44,6 +64,10 @@ public class ArticleReadDetailResponse  {
 
     public static ArticleReadDetailResponse from(Article article){
         return new ArticleReadDetailResponse(
+            article.getId(),
+            article.getMember().getId(),
+            article.getMember().getUsername(),
+            article.getMember().getImage(),
             article.getSubject(),
             article.getTitle(),
             article.getContent(),
