9b0e4c2123ed682a585b1b9f28e0a4966d011a35
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-21 10:54:01 +0900

    [S06P22D105-39] feat: PerfumeService 에 likePerfume 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeService.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeService.java
@@ -2,10 +2,14 @@ package com.ladder.perfumism.perfume.service;
 
 import com.ladder.perfumism.global.exception.BusinessException;
 import com.ladder.perfumism.global.exception.ErrorCode;
+import com.ladder.perfumism.member.domain.Member;
+import com.ladder.perfumism.member.domain.MemberRepository;
 import com.ladder.perfumism.perfume.controller.dto.response.PerfumeDetailResponse;
 import com.ladder.perfumism.perfume.domain.Perfume;
 import com.ladder.perfumism.perfume.domain.PerfumeAccord;
 import com.ladder.perfumism.perfume.domain.PerfumeAccordRepository;
+import com.ladder.perfumism.perfume.domain.PerfumeLike;
+import com.ladder.perfumism.perfume.domain.PerfumeLikeRepository;
 import com.ladder.perfumism.perfume.domain.PerfumeRepository;
 import com.ladder.perfumism.perfume.domain.SimilarPerfume;
 import com.ladder.perfumism.perfume.domain.SimilarPerfumeRepository;
@@ -19,13 +23,18 @@ public class PerfumeService {
     private final PerfumeRepository perfumeRepository;
     private final PerfumeAccordRepository perfumeAccordRepository;
     private final SimilarPerfumeRepository similarPerfumeRepository;
+    private final PerfumeLikeRepository perfumeLikeRepository;
+    private final MemberRepository memberRepository;
 
     public PerfumeService(PerfumeRepository perfumeRepository,
         PerfumeAccordRepository perfumeAccordRepository,
-        SimilarPerfumeRepository similarPerfumeRepository) {
+        SimilarPerfumeRepository similarPerfumeRepository,
+        PerfumeLikeRepository perfumeLikeRepository, MemberRepository memberRepository) {
         this.perfumeRepository = perfumeRepository;
         this.perfumeAccordRepository = perfumeAccordRepository;
         this.similarPerfumeRepository = similarPerfumeRepository;
+        this.perfumeLikeRepository = perfumeLikeRepository;
+        this.memberRepository = memberRepository;
     }
 
     @Transactional(readOnly = true)
@@ -38,5 +47,27 @@ public class PerfumeService {
         List<SimilarPerfume> similarPerfumes = similarPerfumeRepository.findByOriginId(perfume);
 
         return PerfumeDetailResponse.from(perfume, perfumeAccords, similarPerfumes);
+    }
+
+    @Transactional
+    public Long likePerfume(String email, Long perfumeId) {
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
+        Perfume perfume = perfumeRepository.findById(perfumeId)
+            .orElseThrow(() -> new BusinessException(ErrorCode.PERFUME_NOT_FOUND_BY_ID));
+
+        alreadyLikeThisPerfume(member, perfume);
+
+        PerfumeLike perfumeLike = perfumeLikeRepository.save(PerfumeLike.createPerfumeLike(perfume, member));
+
+        perfume.saveLike(perfumeLikeRepository.countByPerfumeId(perfume));
+
+        return (perfumeLike.getId());
+    }
+
+    private void alreadyLikeThisPerfume(Member member, Perfume perfume){
+        if (perfumeLikeRepository.existsByMemberIdAndPerfumeId(member, perfume)){
+            throw new BusinessException(ErrorCode.PERFUME_ALREADY_LIKE);
+        }
     }
 }
