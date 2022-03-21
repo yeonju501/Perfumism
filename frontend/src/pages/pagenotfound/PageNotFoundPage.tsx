8c3de7b39281c886f05fb58559f70d109921f20e
우동진 <wjinh93@gmail.com>
2022-03-22 00:42:54 +0900

    [S06P22D105-161] feat: pageNotFound 구현

---
+++ frontend/src/pages/pagenotfound/PageNotFoundPage.tsx
@@ -0,0 +1,53 @@
+import PageNotFound from "assets/pagenotfound.svg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+function PageNotFoundPage() {
+	return (
+		<Container>
+			<Image src={PageNotFound} alt="" />
+			<Header>Page not found</Header>
+			<Button to="/">BACK HOME</Button>
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Header = styled.h1`
+	text-align: center;
+	font-size: 5rem;
+	margin-top: 0;
+`;
+
+const Image = styled.img`
+	width: 50rem;
+	height: 50rem;
+`;
+
+const Button = styled(Link)`
+	width: 30rem;
+	height: 5rem;
+	background-color: black;
+	border: none;
+	border-radius: 1rem;
+	color: #fff;
+	font-weight: bold;
+	font-size: 2rem;
+	text-decoration: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	:hover {
+		cursor: pointer;
+	}
+`;
+
+export default PageNotFoundPage;
