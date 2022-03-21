c56dcf52f039094bcf0dceae5ea441cc7cc15d70
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-21 10:32:48 +0900

    [S06P22D105-39] feat: PerfumeLikeRepository 레포지토리 생성

---
+++ backend/src/main/java/com/ladder/perfumism/perfume/domain/PerfumeLikeRepository.java
@@ -0,0 +1,7 @@
+package com.ladder.perfumism.perfume.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfumeLikeRepository extends JpaRepository<PerfumeLike, Long> {
+
+}
