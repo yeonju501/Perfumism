679ac90c2f1915912a61af92ed80247b045e057c
9603100 <imgoing222@gmail.com>
2022-03-21 11:13:10 +0900

    [S06P22D105-60] feat: reviews, totalpage, currentpage state 선언

--- frontend/src/components/review/ReviewList.tsx
+++ frontend/src/components/review/ReviewList.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
+interface ReviewsType {
+	review_id: number;
+	member_id: number;
+	member_name: string;
+	member_iamge: string;
+	grade: number;
+	content: string;
+	likes: number;
+}
+
 function ReviewList() {
+	const [reviews, setReviews] = useState<ReviewsType | null>(null);
+	const [totalPage, setTotalPage] = useState(0);
+	const [currentPage, setCurrentPage] = useState(0);
+
 	return;
 }
 
