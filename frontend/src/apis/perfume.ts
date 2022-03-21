e2b52082fb6635a1975c3c4dcc443ed4f3e1a36d
9603100 <imgoing222@gmail.com>
2022-03-22 01:47:23 +0900

    [S06P22D105-160] feat: perfumeApi에 isPerfumeLiked 요청 생성

--- frontend/src/apis/perfume.ts
+++ frontend/src/apis/perfume.ts
@@ -3,10 +3,12 @@ import { request } from "./request";
 
 interface PerfumeApiType {
 	getPerfume: (perfumeId: string) => Promise<AxiosResponse>;
+	isPerfumeLiked: (perfumeId: string) => Promise<AxiosResponse>;
 }
 
 const perfumeApi: PerfumeApiType = {
 	getPerfume: (perfumeId) => request.get(`perfumes/${perfumeId}`),
+	isPerfumeLiked: (perfumeId) => request.get(`auth/perfumes/perfumes/likes/${perfumeId}`),
 };
 
 export default perfumeApi;
