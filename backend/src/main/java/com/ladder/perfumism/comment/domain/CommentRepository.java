201167fc05fe252f0df3a6d84d77c95e116d4460
vtaco <woosj7129@naver.com>
2022-03-22 16:25:42 +0900

    [S06P22D105-143] feat: CommentRepository findByArticle() 구현

--- backend/src/main/java/com/ladder/perfumism/comment/domain/CommentRepository.java
+++ backend/src/main/java/com/ladder/perfumism/comment/domain/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.ladder.perfumism.comment.domain;
 
+import com.ladder.perfumism.article.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
+    Page<Comment> findByArticle(Article article, Pageable pageable);
 }
