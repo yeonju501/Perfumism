8631cfc0e939a91895fe42c794f3ba3754252fd8
우동진 <wjinh93@gmail.com>
2022-03-22 23:12:28 +0900

    [S06P22D105-165] feat: Header 생성

--- frontend/src/components/main/Navbar.tsx
+++ frontend/src/components/main/Navbar.tsx
@@ -15,4 +15,17 @@ function Navbar() {
 	);
 }
 
+const Header = styled.header<HeaderProps>`
+	background: ${({ scrollHeader }) => (scrollHeader > 2 ? "#000" : "#fff")};
+	color: ${({ scrollHeader }) => (scrollHeader > 2 ? "#fff" : "#000")};
+	width: 100%;
+	height: 10vh;
+	position: fixed;
+	z-index: 20;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	border-bottom: 1px solid black;
+`;
+
 export default Navbar;
