c76fd36325a81085b5d45205134cbef98e7d06e0
vtaco <woosj7129@naver.com>
2022-03-22 16:16:40 +0900

    [S06P22D105-143] feat: CommentService showCommentList() 구현

--- backend/src/main/java/com/ladder/perfumism/comment/service/CommentService.java
+++ backend/src/main/java/com/ladder/perfumism/comment/service/CommentService.java
@@ -3,13 +3,17 @@ package com.ladder.perfumism.comment.service;
 import com.ladder.perfumism.article.domain.Article;
 import com.ladder.perfumism.article.domain.ArticleRepository;
 import com.ladder.perfumism.comment.controller.request.CommentCreateRequest;
+import com.ladder.perfumism.comment.controller.response.CommentReadListResponse;
 import com.ladder.perfumism.comment.domain.Comment;
 import com.ladder.perfumism.comment.domain.CommentRepository;
 import com.ladder.perfumism.global.exception.BusinessException;
 import com.ladder.perfumism.global.exception.ErrorCode;
 import com.ladder.perfumism.member.domain.Member;
 import com.ladder.perfumism.member.domain.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -24,6 +28,7 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
+    @Transactional
     public void commentCreate(String email, Long articleId, CommentCreateRequest request) {
         Member member = memberRepository.findByEmail(email)
             .orElseThrow(()-> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
@@ -38,5 +43,18 @@ public class CommentService {
             .build();
 
         commentRepository.save(comment);
+    }
+
+    @Transactional
+    public CommentReadListResponse showCommentList(String email, Pageable pageable, Long articleId) {
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(()->new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
+
+        Article article = articleRepository.findById(articleId)
+            .orElseThrow(()->new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
+
+        Page<Comment> commentList = commentRepository.findAll(pageable);
+
+        return CommentReadListResponse.from(commentList);
     }
 }
