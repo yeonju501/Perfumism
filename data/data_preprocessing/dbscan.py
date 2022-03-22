1b500cdd7f87e467580b41d27317206996b0108e
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-22 13:26:22 +0900

    [S06P22D105-9] feat: 향수 클러스터 추출

--- data/data_preprocessing/dbscan.py
+++ data/data_preprocessing/dbscan.py
@@ -1,39 +1,22 @@
-from sklearn import datasets
 from sklearn.cluster import DBSCAN
-# import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-# iris = datasets.load_iris()
-
-# labels = pd.DataFrame(iris.target)
-# labels.columns=['labels']
-
-# data = pd.DataFrame(iris.data)
-# data.columns=['Sepal length','Sepal width','Petal length','Petal width']
-# data = pd.concat([data, labels], axis=1)
-
-# feature = data[['Sepal length','Sepal width','Petal length','Petal width']]
-
-# model = DBSCAN(eps=0.4, min_samples=6)
-# predict = pd.DataFrame(model.fit_predict(feature))
-# predict.columns=['predict']
-
-# r = pd.concat([feature, predict], axis=1)
-# print(predict['predict'].value_counts())
+import json
 
 df = pd.read_json('data_preprocessing.json')
-# count_vector = CountVectorizer(ngram_range=(1, 1))
-# c_vector_perfume = count_vector.fit_transform(df['main_accords']).toarray()
-
-# data = pd.DataFrame(c_vector_perfume)
 
 tfidfv = TfidfVectorizer().fit(df['main_accords'])
-dd = tfidfv.transform(df['main_accords']).toarray()
-data = pd.DataFrame(dd)
+data = tfidfv.transform(df['main_accords']).toarray()
+data = pd.DataFrame(data)
 
 model = DBSCAN(eps=0.431, min_samples=13)
 predict = pd.DataFrame(model.fit_predict(data))
-predict.columns=['predict']
-print(predict['predict'].value_counts())
+predict.columns = ['predict']
+# print(predict['predict'].value_counts())
+
+perfume_cluster = {x: [] for x in range(-1, 164)}
+for i in range(37000):
+    perfume_cluster[predict['predict'][i]].append(i + 1)
+
+with open('perfume_cluster.json', 'w') as f:
+    json.dump(perfume_cluster, f)
