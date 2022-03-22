61df75fa0d0e16ce6daa732ce8f5b5bba93f48fc
우동진 <wjinh93@gmail.com>
2022-03-22 23:11:59 +0900

    [S06P22D105-165] feat: interface 추가

---
+++ frontend/src/components/main/Navbar.tsx
@@ -0,0 +1,18 @@
+
+interface NavProps {
+	justifyContent?: string;
+}
+
+interface HeaderProps {
+	scrollHeader: number;
+}
+
+function Navbar() {
+
+
+	return (
+		
+	);
+}
+
+export default Navbar;
