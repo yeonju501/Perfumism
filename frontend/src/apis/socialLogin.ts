d9f44b5be4a0f8327b83a63fc4dc1552c5d3c722
우동진 <wjinh93@gmail.com>
2022-03-22 20:12:09 +0900

    [S06P22D105-49] feat: google 로그인함수 추가

--- frontend/src/apis/socialLogin.ts
+++ frontend/src/apis/socialLogin.ts
@@ -7,11 +7,13 @@ const KAKAO_URI = process.env.REACT_APP_KAKAO_URL;
 
 const socialLogin = {
 	google: () =>
-		(window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&response_type=token&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email`),
+		(window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email`),
 	kakao: () =>
 		(window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_URI}&response_type=code`),
 	kakaoLogin: (code: string) =>
 		request.get(`oauth2/authorization/kakao?code=${code}`).then((res) => console.log(res)),
+	googleLogin: (code: string) =>
+		request.get(`oauth2/authorization/google?code=${code}`).then((res) => console.log(res)),
 };
 
 export default socialLogin;
