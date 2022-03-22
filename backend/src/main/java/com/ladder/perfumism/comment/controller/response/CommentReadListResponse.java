d997b070f0ec73460c16365fbc3207738106817e
vtaco <woosj7129@naver.com>
2022-03-22 16:10:26 +0900

    [S06P22D105-143] feat: CommentReadListResponse 생성자 - 구현 및 빌더 구현

--- backend/src/main/java/com/ladder/perfumism/comment/controller/response/CommentReadListResponse.java
+++ backend/src/main/java/com/ladder/perfumism/comment/controller/response/CommentReadListResponse.java
@@ -1,7 +1,10 @@
 package com.ladder.perfumism.comment.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.comment.domain.Comment;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 
 public class CommentReadListResponse {
 
@@ -12,6 +15,25 @@ public class CommentReadListResponse {
     private int totalPageCount;
 
     @JsonProperty("current_page_count")
-    private int currentPageCOunt;
+    private int currentPageCount;
 
+    public CommentReadListResponse(){
+
+    }
+
+    public CommentReadListResponse(List<CommentReadResponse> commentList, int totalPageCount, int currentPageCount){
+        this.commentList = commentList;
+        this.totalPageCount = totalPageCount;
+        this.currentPageCount = currentPageCount;
+    }
+
+    public static CommentReadListResponse from(Page<Comment> commentList){
+        return new CommentReadListResponse(
+            commentList.stream()
+                .map(CommentReadResponse::from)
+                .collect(Collectors.toList()),
+            commentList.getTotalPages(),
+            commentList.getNumber()
+        );
+    }
 }
