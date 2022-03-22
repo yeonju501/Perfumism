f6f004df75ff37dd53834ab4013d28fce2452ecf
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-22 15:58:08 +0900

    [S06P22D105-9] feat: 설문 및 좋아요 기반 추천 알고리즘 기본 틀

--- data/data_preprocessing/dbscan.py
+++ data/data_preprocessing/dbscan.py
@@ -1,9 +1,25 @@
 from sklearn.cluster import DBSCAN
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-import json
+from sklearn.metrics.pairwise import cosine_similarity
+import time
+
+start = time.time()
 
 df = pd.read_json('data_preprocessing.json')
+
+input_data = {
+    "brand": "",
+    "image": "",
+    "launch_year": 0,
+    "longevity": "",
+    "main_accords": "citrus woody violet fresh musky amber vanilla sweet",
+    "notes": "",
+    "perfume": "user",
+    "sillage": "",
+}
+
+df = df.append(input_data, ignore_index=True)
 
 tfidfv = TfidfVectorizer().fit(df['main_accords'])
 data = tfidfv.transform(df['main_accords']).toarray()
@@ -12,11 +28,24 @@ data = pd.DataFrame(data)
 model = DBSCAN(eps=0.431, min_samples=13)
 predict = pd.DataFrame(model.fit_predict(data))
 predict.columns = ['predict']
-# print(predict['predict'].value_counts())
 
-perfume_cluster = {x: [] for x in range(-1, 164)}
-for i in range(37000):
-    perfume_cluster[predict['predict'][i]].append(i + 1)
+filt = (predict['predict'] == predict.iloc[-1, 0])
+new_df = df[filt]
 
-with open('perfume_cluster.json', 'w') as f:
-    json.dump(perfume_cluster, f)
+feature = new_df['main_accords'] + ' ' + new_df['notes']
+
+tfidf_vector = TfidfVectorizer(ngram_range=(1, 1))
+c_vector_perfume = tfidf_vector.fit_transform(feature)
+
+perfume_c_sim = cosine_similarity(c_vector_perfume, c_vector_perfume).argsort()[:, ::-1]
+
+if len(new_df) < 30:
+    sim_index = perfume_c_sim[len(new_df) - 1].reshape(-1)
+else:
+    sim_index = perfume_c_sim[len(new_df) - 1, :30].reshape(-1)
+sim_index = sim_index[sim_index != len(new_df) - 1]
+result = new_df.iloc[sim_index]
+
+print(result)
+
+print('종료 시간 : ', time.time() - start)
