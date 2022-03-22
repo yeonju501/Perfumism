d1fb86255b9029f215c1856b1eaf5b530d7954e7
9603100 <imgoing222@gmail.com>
2022-03-22 16:26:11 +0900

    [S06P22D105-177] style: 좋아요 버튼 이미지 가운데 위치하도록 transform 값 조정

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -14,6 +14,7 @@ export const LikeButton = styled.button<LikeButtonProps>`
 	position: ${({ center }) => (center ? "absolute" : "static")};
 	top: 50%;
 	left: 50%;
+	transform: translate(-50%, -100%);
 	display: none;
 `;
 
