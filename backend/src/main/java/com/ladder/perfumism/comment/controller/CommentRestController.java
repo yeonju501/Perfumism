503f8ff7f353ba7820a5d4bc72d71a6fe7d9a134
vtaco <woosj7129@naver.com>
2022-03-22 16:17:16 +0900

    [S06P22D105-143] feat: CommentRestController getCommentList return 구현

--- backend/src/main/java/com/ladder/perfumism/comment/controller/CommentRestController.java
+++ backend/src/main/java/com/ladder/perfumism/comment/controller/CommentRestController.java
@@ -1,9 +1,13 @@
 package com.ladder.perfumism.comment.controller;
 
 import com.ladder.perfumism.comment.controller.request.CommentCreateRequest;
+import com.ladder.perfumism.comment.controller.response.CommentReadListResponse;
 import com.ladder.perfumism.comment.service.CommentService;
 import com.sun.jndi.toolkit.url.Uri;
 import java.net.URI;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +36,12 @@ public class CommentRestController {
     }
 
     @GetMapping()
-    public ResponseEntity<CommentReadListResponse> getCommentList(){
+    public ResponseEntity<CommentReadListResponse> getCommentList(
+        @AuthenticationPrincipal String email,
+        @PageableDefault(sort = "id", direction = Direction.DESC)Pageable pageable,
+        @PathVariable(value = "article_id") Long articleId){
+
+        return ResponseEntity.ok().body(commentService.showCommentList(email,pageable,articleId));
 
     }
 
