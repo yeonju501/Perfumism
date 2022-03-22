8a69e2cebd93577521343d28badf3377de2a9c98
yeonju501 <loling970124@gmail.com>
2022-03-23 00:45:58 +0900

    [S06P22D105-134] feat: GoogleUserInfoResponse 데이터 추가

--- backend/src/main/java/com/ladder/perfumism/auth/controller/dto/response/GoogleUserInfoResponse.java
+++ backend/src/main/java/com/ladder/perfumism/auth/controller/dto/response/GoogleUserInfoResponse.java
@@ -1,5 +1,35 @@
 package com.ladder.perfumism.auth.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.auth.domain.Authority;
+import com.ladder.perfumism.member.domain.Member;
+import lombok.Getter;
+
+@Getter
 public class GoogleUserInfoResponse {
 
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("picture")
+    private String picture;
+
+    @JsonProperty("sub")
+    private String sub;
+
+    @JsonProperty("exp")
+    private String exp;
+
+    public Member toEntity() {
+        return Member.builder()
+            .kakaoId(sub)
+            .email(email)
+            .username(name)
+            .password(sub+exp)
+            .authority(Authority.ROLE_MEMBER)
+            .build();
+    }
 }
