50f5d4f8db1521105d12753d814921f395e17fc0
우동진 <wjinh93@gmail.com>
2022-03-23 15:02:48 +0900

    [S06P22D105-166] feat: route에 Main 페이지 추가

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -4,6 +4,7 @@ import SignIn from "pages/account/signin/SignIn";
 import SignUp from "pages/account/signup/SignUp";
 import GoogleLoginPage from "pages/account/socialogin/GoogleLoginPage";
 import KaKaoLoginPage from "pages/account/socialogin/KaKaoLoginPage";
+import Main from "pages/main/Main";
 import PageNotFoundPage from "pages/pagenotfound/PageNotFoundPage";
 import PerfumeDetail from "pages/perfume/PerfumeDetail";
 import Favorites from "pages/profile/Favorites";
@@ -19,7 +20,7 @@ function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" />
+				<Route path="/" element={<Main />} />
 				<Route path="/:username" element={<Profile />} />
 				<Route path="/:username/favorites" element={<Favorites />} />
 				<Route path="/:username/my-articles" element={<MyArticles />} />
@@ -33,7 +34,6 @@ function Router() {
 				<Route path="/find-password" element={<FindPassword />} />
 				<Route path="/login/oauth2/code/kakao" element={<KaKaoLoginPage />} />
 				<Route path="/login/oauth2/code/google" element={<GoogleLoginPage />} />
-				<Route path="/navbar" element={<Navbar />} />
 				<Route path="/*" element={<PageNotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
