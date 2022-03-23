1937a8df3cbb9469e95437e42c2272a006f346cb
우동진 <wjinh93@gmail.com>
2022-03-24 01:27:41 +0900

    [S06P22D105-164] feat: flex 추가

--- frontend/src/components/main/Section.tsx
+++ frontend/src/components/main/Section.tsx
@@ -5,6 +5,9 @@ interface SectionProps {
 }
 
 const Section = styled.section<SectionProps>`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 	position: relative;
 	height: 80rem;
 	background: ${(props) => `url(${props.image}) no-repeat top`};
