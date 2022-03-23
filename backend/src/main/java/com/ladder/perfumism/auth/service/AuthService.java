08b1f4710df363d776a00934141277072299f391
yeonju501 <loling970124@gmail.com>
2022-03-23 15:52:36 +0900

    [S06P22D105-179] feat: AuthService 토큰 전달 방식 변경

--- backend/src/main/java/com/ladder/perfumism/auth/service/AuthService.java
+++ backend/src/main/java/com/ladder/perfumism/auth/service/AuthService.java
@@ -1,9 +1,7 @@
 package com.ladder.perfumism.auth.service;
 
 import com.ladder.perfumism.auth.controller.dto.request.LoginRequest;
-import com.ladder.perfumism.auth.controller.dto.request.RefreshTokenRequest;
-import com.ladder.perfumism.auth.controller.dto.request.TokenRequest;
-import com.ladder.perfumism.auth.controller.dto.response.AccessTokenResponse;
+import com.ladder.perfumism.auth.controller.dto.request.ReissueRequest;
 import com.ladder.perfumism.auth.controller.dto.response.TokenResponse;
 import com.ladder.perfumism.auth.domain.RefreshToken;
 import com.ladder.perfumism.auth.domain.RefreshTokenRepository;
@@ -37,60 +35,65 @@ public class AuthService {
     }
 
     @Transactional
-    public AccessTokenResponse login(LoginRequest request, HttpServletResponse response) {
+    public void login(LoginRequest request, HttpServletResponse response) {
         Member member = memberRepository.findByEmail(request.getEmail())
             .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
         member.login(passwordEncoder, request.getPassword());
 
         TokenResponse tokenResponse = jwtTokenProvider.createToken(member.getEmail(), member.getAuthority());
-        setRefreshTokenToCookie(tokenResponse, response);
-        saveRefreshToken(member, tokenResponse);
-        return AccessTokenResponse.builder()
-            .accessToken(tokenResponse.getAccessToken())
-            .build();
+        Long refreshTokenId = saveRefreshToken(member, tokenResponse);
+        setTokenToCookie(tokenResponse.getAccessToken(), refreshTokenId, response);
     }
 
-    private void setRefreshTokenToCookie(TokenResponse tokenResponse, HttpServletResponse response) {
-        Cookie cookie = new Cookie("refreshToken", tokenResponse.getRefreshToken());
-        cookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
-        cookie.setSecure(true);
+    private void setTokenToCookie(String accessToken, Long refreshTokenId, HttpServletResponse response) {
+        Cookie accessTokenCookie = new Cookie("access_token", accessToken);
+        accessTokenCookie.setMaxAge(30 * 60); // expires in 7 days
+        accessTokenCookie.setSecure(true);
 //        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-    }
+        accessTokenCookie.setPath("/");
+        response.addCookie(accessTokenCookie);
 
-    private void saveRefreshToken(Member member, TokenResponse tokenResponse) {
-        RefreshToken refreshToken = RefreshToken.builder()
-            .key(member.getEmail())
-            .value(tokenResponse.getRefreshToken())
-            .build();
-        refreshTokenRepository.save(refreshToken);
+        Cookie refreshIdCookie = new Cookie("index", Long.toString(refreshTokenId));
+        refreshIdCookie.setMaxAge(7 * 24 * 60 * 60); // expires in 7 days
+        refreshIdCookie.setSecure(true);
+//        cookie.setHttpOnly(true);
+        refreshIdCookie.setPath("/");
+        response.addCookie(refreshIdCookie);
     }
 
     @Transactional
-    public AccessTokenResponse reissue(RefreshTokenRequest request, String accessToken, HttpServletResponse response) {
-        // Refresh Token 검증
-        jwtTokenProvider.validateRefreshToken(request.getRefreshToken());
+    public Long saveRefreshToken(Member member, TokenResponse tokenResponse) {
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(member.getEmail())
+            .orElse(RefreshToken.builder()
+                .key(member.getEmail())
+                .value(tokenResponse.getRefreshToken())
+                .build());
+        refreshTokenRepository.save(refreshToken);
+        return refreshToken.getId();
+    }
 
-        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+    @Transactional
+    public void reissue(ReissueRequest request, HttpServletResponse response) {
+        // Refresh Token 검증
+        String refreshTokenRequest = refreshTokenRepository.findById(request.getId())
+            .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_EXPIRED_REFRESH_TOKEN)).getValue();
+        jwtTokenProvider.validateRefreshToken(refreshTokenRequest);
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(request.getAccessToken());
 
         // 저장소에서 Email로 refresh token 가져옴
-        RefreshToken refreshToken = refreshTokenRepository.findById(authentication.getName())
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
             .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_LOGOUT_USER_JWT));
 
         // refresh token 일치하는지 검사
-        refreshToken.validateValue(request.getRefreshToken());
+        refreshToken.validateValue(refreshTokenRequest);
 
         // 새로운 토큰 생성
         TokenResponse tokenResponse = jwtTokenProvider.createToken(authentication.getName(),
             jwtTokenProvider.getAuthority(authentication));
 
-        setRefreshTokenToCookie(tokenResponse, response);
-
         // 새로운 refresh token
         refreshToken.updateToken(tokenResponse.getRefreshToken());
-        return AccessTokenResponse.builder()
-            .accessToken(tokenResponse.getAccessToken())
-            .build();
+        setTokenToCookie(tokenResponse.getAccessToken(), refreshToken.getId(), response);
     }
 }
