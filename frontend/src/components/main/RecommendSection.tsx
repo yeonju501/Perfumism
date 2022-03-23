e949d3171277099ada422c2a094b17d12904d520
우동진 <wjinh93@gmail.com>
2022-03-23 15:07:16 +0900

    [S06P22D105-166] feat: styled component 구현

--- frontend/src/components/main/RecommendSection.tsx
+++ frontend/src/components/main/RecommendSection.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Background from "assets/background.webp";
 
 interface SectionProps {
 	image: string;
@@ -10,3 +13,25 @@ function RecommendSection() {
 }
 
 export default RecommendSection;
+
+const Section = styled.section<SectionProps>`
+	width: 100%;
+	height: 70rem;
+	background-image: url(${Background});
+`;
+
+
+const Button = styled(Link)`
+	width: 35rem;
+	color: inherit;
+	text-align: center;
+	text-decoration: none;
+	font-size: 2.5rem;
+	font-weight: 700;
+	position: absolute;
+	padding: 2rem;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	border: 1px solid #000;
+`;
