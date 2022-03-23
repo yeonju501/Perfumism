49d2705cf64c9badcfb5d27a7bdd84308c7e3fff
우동진 <wjinh93@gmail.com>
2022-03-24 02:13:40 +0900

    [S06P22D105-165] feat: icons, linklist 삭제

--- frontend/src/components/main/index.ts
+++ frontend/src/components/main/index.ts
@@ -2,6 +2,4 @@ export { default as Button } from "./Button";
 export { default as Section } from "./Section";
 export { default as RecommendSection } from "./RecommendSection";
 export { default as PerfumesSection } from "./PerfumesSection";
-export { default as Icons } from "./Icons";
-export { default as Search } from "./Search";
 export { default as Phrases } from "./Phrases";
