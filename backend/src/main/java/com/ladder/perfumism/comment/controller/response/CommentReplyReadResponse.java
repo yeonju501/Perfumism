bcc50987535336c4ec0b6b5a4305f020ec103b31
vtaco <woosj7129@naver.com>
2022-03-23 15:59:45 +0900

    [S06P22D105-148] feat: CommentReplyReadResponse 생성자 구현

--- backend/src/main/java/com/ladder/perfumism/comment/controller/response/CommentReplyReadResponse.java
+++ backend/src/main/java/com/ladder/perfumism/comment/controller/response/CommentReplyReadResponse.java
@@ -1,6 +1,7 @@
 package com.ladder.perfumism.comment.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.comment.domain.Comment;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -39,4 +40,34 @@ public class CommentReplyReadResponse {
     @JsonProperty("updateAt")
     @ApiModelProperty(position = 7, notes = "수정 시간", example = "2023,4,14,14,59,51,0000000")
     private LocalDateTime updateAt;
+
+    public CommentReplyReadResponse(){
+
+    }
+
+    public CommentReplyReadResponse(
+        Long commentId, Long memberId, String memberName, Long articleId,
+        Long mainCommentId, String content, LocalDateTime createAt, LocalDateTime updateAt){
+        this.commentId = commentId;
+        this.memberId = memberId;
+        this.memberName = memberName;
+        this.articleId = articleId;
+        this.mainCommentId = mainCommentId;
+        this.content = content;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public static CommentReplyReadResponse from(Comment comment){
+        return new CommentReplyReadResponse(
+            comment.getId(),
+            comment.getMember().getId(),
+            comment.getMember().getUsername(),
+            comment.getArticle().getId(),
+            comment.getMainComment().getId(),
+            comment.getContent(),
+            comment.getCreatedAt(),
+            comment.getUpdatedAt()
+        );
+    }
 }
