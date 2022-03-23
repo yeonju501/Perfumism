f52a7ab7021fb41f6df3cfeea30435e911cbd612
우동진 <wjinh93@gmail.com>
2022-03-23 15:35:59 +0900

    [S06P22D105-167] feat: position 향수 목록 바로가기 섹션 구현

--- frontend/src/components/main/PerfumesSection.tsx
+++ frontend/src/components/main/PerfumesSection.tsx
@@ -1,5 +1,12 @@
+import Background from "assets/background2.webp";
+import { Section, Button } from "./index";
+
 function PerfumesSection() {
-	return <div>PerfumesSection</div>;
+	return (
+		<Section image={Background}>
+			<Button to="/perfumes">향수 목록 바로가기</Button>
+		</Section>
+	);
 }
 
 export default PerfumesSection;
