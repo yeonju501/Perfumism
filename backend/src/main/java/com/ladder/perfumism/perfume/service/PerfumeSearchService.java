e07fb2ca30d636157f8db9c592acbf199a9c4880
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-23 15:54:30 +0900

    [S06P22D105-74] feat: PerfumeSearchService 에 브랜드 검색 기능 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeSearchService.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeSearchService.java
@@ -3,10 +3,12 @@ package com.ladder.perfumism.perfume.service;
 import com.ladder.perfumism.global.exception.BusinessException;
 import com.ladder.perfumism.global.exception.ErrorCode;
 import com.ladder.perfumism.perfume.controller.dto.response.PerfumeListResponse;
+import com.ladder.perfumism.perfume.domain.Brand;
 import com.ladder.perfumism.perfume.domain.BrandRepository;
 import com.ladder.perfumism.perfume.domain.Perfume;
 import com.ladder.perfumism.perfume.domain.PerfumeAccordRepository;
 import com.ladder.perfumism.perfume.domain.PerfumeRepository;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -33,10 +35,19 @@ public class PerfumeSearchService {
         checkKeywordLength(keyword);
 
         Page<Perfume> perfumes;
-        switch (type){
+        switch (type) {
             case "name":
-                perfumes = perfumeRepository.findByNameContainingIgnoreCase(keyword, pageable);
+                perfumes = perfumeRepository.findByNameContainsIgnoreCase(keyword, pageable);
                 break;
+            case "brand":
+                List<Brand> brands = brandRepository.findByNameContainsIgnoreCase(keyword);
+                if (brands.isEmpty()) {
+                    throw new BusinessException(ErrorCode.BRAND_NOT_FOUND_BY_NAME);
+                }
+                perfumes = perfumeRepository.findByBrandId(brands, pageable);
+                break;
+//            case "accord":
+//                break;
             default:
                 throw new BusinessException(ErrorCode.SEARCH_NOT_EXIST_TYPE);
         }
