35eb6656e3d6867cf9fe2eb92622b0ba3c5f5f25
우동진 <wjinh93@gmail.com>
2022-03-24 01:27:01 +0900

    [S06P22D105-164] feat: position absolute 삭제

--- frontend/src/components/main/Button.tsx
+++ frontend/src/components/main/Button.tsx
@@ -8,12 +8,9 @@ const Button = styled(Link)`
 	text-decoration: none;
 	font-size: 2.5rem;
 	font-weight: 700;
-	position: absolute;
 	padding: 2rem;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
 	border: 1px solid #000;
+	margin: 0 auto;
 `;
 
 export default Button;
