c20786189660c5d245875d406da9c310cb45b23c
우동진 <wjinh93@gmail.com>
2022-03-23 23:20:15 +0900

    [S06P22D105-50] refactor: 불필요 코드 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import GlobalStyle from "styles/globalstyle";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-	return (
-		<>
-			<GlobalStyle />
-			<Router />
-			<ToastContainer style={{ fontSize: "1.4rem" }} />
-		</>
-	);
-}
+return (
+	<>
+		<GlobalStyle />
+		<Router />
+		<ToastContainer style={{ fontSize: "1.4rem" }} />
+	</>
+);
 
 export default App;
