93d2e4c02f359a6d9cfb3f444fbb887d286986ae
9603100 <imgoing222@gmail.com>
2022-03-25 02:13:46 +0900

    [S06P22D105-54] feat: Infinite scroll 코드 임시 작성 후 주석 처리

--- frontend/src/pages/perfume/Perfumes.tsx
+++ frontend/src/pages/perfume/Perfumes.tsx
@@ -1,6 +1,7 @@
 import perfumeApi from "apis/perfume";
 import PerfumeList from "components/perfume/PerfumeList";
 import { useEffect, useState } from "react";
+import useIntersectionObserver from "./hooks/useIntersectionObserver";
 
 interface PerfumeType {
 	perfume_id: string;
@@ -14,22 +15,55 @@ function Perfumes() {
 	const [perfumes, setPerfumes] = useState<PerfumeType[]>([]);
 	const [totalPage, setTotalPage] = useState(0);
 	const [currentPage, setCurrentPage] = useState(0);
+	const [isLoading, setIsLoading] = useState(false);
+	console.log(0);
+	console.log(perfumes);
+	console.log(currentPage, totalPage);
 
 	useEffect(() => {
-		getPerfumes();
+		// console.log(1);
+		// perfumeApi.getPerfumes(0).then((res) => {
+		// 	setPerfumes(res.data.perfumes);
+		// 	setTotalPage(res.data.total_page_count);
+		// });
 	}, []);
 
 	const getPerfumes = async () => {
-		await perfumeApi.getPerfumes(currentPage).then((res) => {
-			setPerfumes(res.data.perfumes);
-			setTotalPage(res.data.total_page_count);
-			setCurrentPage(res.data.current_page_count);
-		});
+		// console.log("getPerfumes 실행");
+		// setIsLoading(true);
+		// console.log("setloading true 실행");
+		// await new Promise((resolve) => setTimeout(resolve, 1000));
+		// setCurrentPage((prev) => prev + 1);
+		// console.log("currentpage + 1 실행");
+		// console.log(`현재페이지 ${currentPage}`);
+		// await perfumeApi.getPerfumes(currentPage).then((res) => {
+		// 	console.log(res.data);
+		// 	setPerfumes((prev) => prev.concat(res.data.perfumes));
+		// 	setCurrentPage(res.data.current_page_count);
+		// });
+		// setIsLoading(false);
 	};
+
+	const onIntersect: IntersectionObserverCallback = async ([entry], observer) => {
+		// if (entry.isIntersecting && !isLoading) {
+		// 	console.log(3);
+		// 	observer.unobserve(entry.target);
+		// 	await getPerfumes();
+		// 	observer.observe(entry.target);
+		// }
+	};
+
+	const { setTarget } = useIntersectionObserver({
+		root: null,
+		rootMargin: "0px",
+		threshold: 0.5,
+		onIntersect,
+	});
 
 	return (
 		<div>
 			<PerfumeList perfumes={perfumes} />
+			<div ref={setTarget}>{isLoading && <p>Loading..</p>}</div>
 		</div>
 	);
 }
