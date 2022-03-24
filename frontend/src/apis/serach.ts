0e020381aa8b2d0298344e5b904e7e44e79a6d23
우동진 <wjinh93@gmail.com>
2022-03-25 01:15:15 +0900

    [S06P22D105-170] feat: api 오타 수정

--- frontend/src/apis/serach.ts
+++ frontend/src/apis/serach.ts
@@ -9,11 +9,11 @@ interface ReviewApiType {
 
 const searchApi: ReviewApiType = {
 	searchName: (keyword) =>
-		request.get(`perfumes/search?keyword=${keyword}page=0&size=20&type=name`),
+		request.get(`perfumes/search?keyword=${keyword}&page=0&size=20&type=name`),
 	searchBrand: (keyword) =>
-		request.get(`perfumes/search?keyword=${keyword}page=0&size=20&type=brand`),
+		request.get(`perfumes/search?keyword=${keyword}&page=0&size=20&type=brand`),
 	searchAccord: (keyword) =>
-		request.get(`perfumes/search?keyword=${keyword}page=0&size=20&type=Accord`),
+		request.get(`perfumes/search?keyword=${keyword}&page=0&size=20&type=accord`),
 };
 
 export default searchApi;
