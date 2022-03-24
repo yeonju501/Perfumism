0f63eefd682e4ad031a7a5b10214828d6ddb8d28
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-24 15:07:53 +0900

    [S06P22D105-187] feat: Answer 컴포넌트 생성

---
+++ frontend/src/components/recommend/Answer.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+function Answer() {
+	return <></>;
+}
+
+const Container = styled.div`
+	height: 80vh;
+	font-size: 1.5rem;
+	display: flex;
+	flex-direction: column;
+`;
+
+const Title = styled.h1`
+	color: #000;
+	font-size: 4rem;
+	font-weight: 800;
+	text-align: center;
+	margin: 10% auto 2%;
+`;
+
+export default Answer;
