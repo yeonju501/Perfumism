47b246625c5d19768153c52aa698c5ff619579e4
yeonju501 <loling970124@gmail.com>
2022-03-25 22:23:40 +0900

    [S06P22D105-194] feat: NotificationRepository 추가

---
+++ backend/src/main/java/com/ladder/perfumism/notification/domain/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.ladder.perfumism.notification.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
