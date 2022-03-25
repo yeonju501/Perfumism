bfc10559a49d3f8e2748143a782258418c85eacf
9603100 <imgoing222@gmail.com>
2022-03-25 17:21:55 +0900

    [S06P22D105-197] feat: 함수 제거 후 useLikeButton hook 사용

--- frontend/src/components/button/LikeButton.tsx
+++ frontend/src/components/button/LikeButton.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as heart } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
-import perfumeApi from "apis/perfume";
+import useLikeButton from "./hooks/useLikeButton";
 
 interface LikeButtonProps {
 	perfumeId: string;
@@ -10,22 +9,8 @@ interface LikeButtonProps {
 }
 
 function LikeButton({ center, perfumeId }: LikeButtonProps) {
-	const [isLiked, setIsLiked] = useState(false);
-
-	useEffect(() => {
-		isPerfumeLiked();
-	}, []);
-
-	const isPerfumeLiked = async () => {
-		await perfumeApi.isPerfumeLiked(perfumeId).then((res) => setIsLiked(res.data.is_liked));
-	};
-
-	const handleHeartClick = async () => {
-		isLiked
-			? await perfumeApi.deleteFavoritePerfume(perfumeId)
-			: await perfumeApi.addFavoritePerfume(perfumeId);
-		setIsLiked((prev) => !prev);
-	};
+	const type = "perfume";
+	const { isLiked, handleHeartClick } = useLikeButton({ type, typeId: perfumeId });
 
 	return (
 		<Button center={center} isLiked={isLiked} onClick={handleHeartClick}>
