f81bb676cdc93679327fdd296caa381b142160cb
9603100 <imgoing222@gmail.com>
2022-03-27 01:31:54 +0900

    [S06P22D105-208] feat: 무한스크롤 구현

--- frontend/src/pages/perfume/Perfumes.tsx
+++ frontend/src/pages/perfume/Perfumes.tsx
@@ -16,47 +16,35 @@ function Perfumes() {
 	const [totalPage, setTotalPage] = useState(0);
 	const [currentPage, setCurrentPage] = useState(0);
 	const [isLoading, setIsLoading] = useState(false);
-	console.log(0);
-	console.log(perfumes);
-	console.log(currentPage, totalPage);
 
 	useEffect(() => {
-		// console.log(1);
-		// perfumeApi.getPerfumes(0).then((res) => {
-		// 	setPerfumes(res.data.perfumes);
-		// 	setTotalPage(res.data.total_page_count);
-		// });
-	}, []);
+		if (currentPage > totalPage) return;
+		getPerfumes();
+	}, [currentPage]);
 
 	const getPerfumes = async () => {
-		// console.log("getPerfumes 실행");
-		// setIsLoading(true);
-		// console.log("setloading true 실행");
-		// await new Promise((resolve) => setTimeout(resolve, 1000));
-		// setCurrentPage((prev) => prev + 1);
-		// console.log("currentpage + 1 실행");
-		// console.log(`현재페이지 ${currentPage}`);
-		// await perfumeApi.getPerfumes(currentPage).then((res) => {
-		// 	console.log(res.data);
-		// 	setPerfumes((prev) => prev.concat(res.data.perfumes));
-		// 	setCurrentPage(res.data.current_page_count);
-		// });
-		// setIsLoading(false);
+		setIsLoading(true);
+		await new Promise((resolve) => setTimeout(resolve, 800));
+		await perfumeApi.getPerfumes(currentPage).then((res) => {
+			setPerfumes((prev) => prev.concat(res.data.perfumes));
+			setTotalPage(res.data.total_page_count);
+			setCurrentPage(res.data.current_page_count);
+		});
+		setIsLoading(false);
 	};
 
 	const onIntersect: IntersectionObserverCallback = async ([entry], observer) => {
-		// if (entry.isIntersecting && !isLoading) {
-		// 	console.log(3);
-		// 	observer.unobserve(entry.target);
-		// 	await getPerfumes();
-		// 	observer.observe(entry.target);
-		// }
+		if (entry.isIntersecting && !isLoading) {
+			observer.unobserve(entry.target);
+			setCurrentPage((prev) => prev + 1);
+			observer.observe(entry.target);
+		}
 	};
 
 	const { setTarget } = useIntersectionObserver({
 		root: null,
 		rootMargin: "0px",
-		threshold: 0.5,
+		threshold: 1,
 		onIntersect,
 	});
 
