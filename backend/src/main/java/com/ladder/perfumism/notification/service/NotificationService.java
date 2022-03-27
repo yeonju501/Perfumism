ae42a222e2b54cb4c051d0d3845941b8b2b56225
yeonju501 <loling970124@gmail.com>
2022-03-27 11:52:42 +0900

    [S06P22D105-205] feat: NotificationService showAllNotifications 추가

--- backend/src/main/java/com/ladder/perfumism/notification/service/NotificationService.java
+++ backend/src/main/java/com/ladder/perfumism/notification/service/NotificationService.java
@@ -1,8 +1,14 @@
 package com.ladder.perfumism.notification.service;
 
 import com.ladder.perfumism.comment.domain.Comment;
+import com.ladder.perfumism.member.domain.Member;
+import com.ladder.perfumism.member.service.MemberService;
+import com.ladder.perfumism.notification.controller.dto.response.NotificationResponse;
 import com.ladder.perfumism.notification.domain.Notification;
 import com.ladder.perfumism.notification.domain.NotificationRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final MemberService memberService;
 
-    public NotificationService(NotificationRepository notificationRepository) {
+    public NotificationService(NotificationRepository notificationRepository,
+        MemberService memberService) {
         this.notificationRepository = notificationRepository;
+        this.memberService = memberService;
     }
 
     @Transactional
@@ -20,23 +29,29 @@ public class NotificationService {
         Notification notification = Notification.builder()
             .member(comment.getArticle().getMember())
             .type("comment")
-            .article_id(comment.getArticle().getId())
-            .content_id(comment.getId())
-            .message(comment.getArticle().getTitle())
+            .comment(comment)
             .build();
         notificationRepository.save(notification);
     }
 
     @Transactional
-    public void createReplyNotification(Comment reply){
+    public void createReplyNotification(Comment reply) {
         Notification notification = Notification.builder()
             .member(reply.getParentId().getMember())
             .type("reply")
-            .article_id(reply.getArticle().getId())
-            .content_id(reply.getParentId().getId())
-            .message(reply.getParentId().getContent())
+            .comment(reply)
             .build();
         notificationRepository.save(notification);
     }
 
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> showAllNotifications(String email) {
+        Member member = memberService.findByEmail(email);
+        LocalDateTime start = LocalDateTime.now().minusMonths(3);
+        LocalDateTime end = LocalDateTime.now();
+        List<Notification> notifications = notificationRepository.findAllByMemberAndCreatedAtBetween(member, start, end);
+        return notifications.stream()
+            .map(NotificationResponse::from)
+            .collect(Collectors.toList());
+    }
 }
