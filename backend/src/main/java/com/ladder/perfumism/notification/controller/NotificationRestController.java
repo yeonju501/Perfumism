6d7db8066410a23f5cfff6a54d271a5797e58773
yeonju501 <loling970124@gmail.com>
2022-03-27 21:22:39 +0900

    [S06P22D105-210] feat: NotificationRestController readNotification 추가

--- backend/src/main/java/com/ladder/perfumism/notification/controller/NotificationRestController.java
+++ backend/src/main/java/com/ladder/perfumism/notification/controller/NotificationRestController.java
@@ -3,10 +3,15 @@ package com.ladder.perfumism.notification.controller;
 import com.ladder.perfumism.notification.controller.dto.response.NotificationResponse;
 import com.ladder.perfumism.notification.service.NotificationService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +27,23 @@ public class NotificationRestController {
     }
 
     @GetMapping("/auth/notifications")
+    @ApiOperation(value = "모든 알림 목록 조회", notes = "(로그인 필요) 모든 알림 목록을 조회하는 API")
     public ResponseEntity<List<NotificationResponse>> showAllNotifications(@AuthenticationPrincipal String email) {
         return ResponseEntity.ok().body(notificationService.showAllNotifications(email));
     }
 
     @GetMapping("/auth/notifications/unread")
+    @ApiOperation(value = "안읽은 알림 목록 조회", notes = "(로그인 필요) 안읽은 알림 목록을 조회하는 API")
     public ResponseEntity<List<NotificationResponse>> showUnreadNotifications(@AuthenticationPrincipal String email) {
         return ResponseEntity.ok().body(notificationService.showUnreadNotifications(email));
+    }
+
+    @PutMapping("/auth/notifications/{notificationId}")
+    @ApiOperation(value = "알림 확인 처리", notes = "(로그인 필요) 알림을 확인 처리하는 API")
+    @ApiImplicitParam(name = "notificationId", value = "알림 ID", required = true)
+    public ResponseEntity<Void> readNotification(@ApiParam(hidden = true) @AuthenticationPrincipal String email,
+        @PathVariable Long notificationId) {
+        notificationService.readNotification(email, notificationId);
+        return ResponseEntity.noContent().build();
     }
 }
