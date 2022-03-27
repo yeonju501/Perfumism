5d2e69faa479e46a98d359c1fb642425efcfaae7
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-27 15:30:48 +0900

    [S06P22D105-188] feat: 워드클라우드 Container 설정

--- frontend/src/components/recommend/WordCloud.tsx
+++ frontend/src/components/recommend/WordCloud.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
 function WordCloud() {
-	return <h1>wordCloud</h1>;
+	return (
+		<Container>
+			<h1>wordCloud</h1>
+		</Container>
+	);
 }
+
+const Container = styled.div`
+	min-height: 30vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 export default WordCloud;
