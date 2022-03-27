dd34db899e5fdf8ea1187eb247798788e0e39164
yeonju501 <loling970124@gmail.com>
2022-03-27 11:51:34 +0900

    [S06P22D105-205] feat: Notification 변경

--- backend/src/main/java/com/ladder/perfumism/notification/domain/Notification.java
+++ backend/src/main/java/com/ladder/perfumism/notification/domain/Notification.java
@@ -1,10 +1,12 @@
 package com.ladder.perfumism.notification.domain;
 
+import com.ladder.perfumism.comment.domain.Comment;
 import com.ladder.perfumism.global.domain.BaseEntity;
 import com.ladder.perfumism.member.domain.Member;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,21 +24,16 @@ public class Notification extends BaseEntity {
     @Column(name = "notification_id")
     private Long id;
 
-    @ManyToOne(targetEntity = Member.class)
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @Column(name = "type")
     private String type;
 
-    @Column(name = "article_id")
-    private Long article_id;
-
-    @Column(name = "content_id")
-    private Long content_id;
-
-    @Column(name = "message")
-    private String message;
+    @ManyToOne(targetEntity = Comment.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 
     @Column(name = "read_at")
     private LocalDateTime readAt;
@@ -45,12 +42,10 @@ public class Notification extends BaseEntity {
     }
 
     @Builder
-    public Notification(Member member, String type, Long article_id, Long content_id, String message, LocalDateTime readAt){
+    public Notification(Member member, String type, Comment comment, LocalDateTime readAt) {
         this.member = member;
         this.type = type;
-        this.article_id = article_id;
-        this.content_id = content_id;
-        this.message = message;
+        this.comment = comment;
         this.readAt = readAt;
     }
 }
