192e7baf4bcf7350c5469f37330d69bfd7bb555d
yeonju501 <loling970124@gmail.com>
2022-03-27 11:52:02 +0900

    [S06P22D105-205] feat: NotificationRepository findAllByMemberAndCreatedAtBetween 추가

--- backend/src/main/java/com/ladder/perfumism/notification/domain/NotificationRepository.java
+++ backend/src/main/java/com/ladder/perfumism/notification/domain/NotificationRepository.java
@@ -1,7 +1,11 @@
 package com.ladder.perfumism.notification.domain;
 
+import com.ladder.perfumism.member.domain.Member;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+    List<Notification> findAllByMemberAndCreatedAtBetween(Member member, LocalDateTime start, LocalDateTime end);
 }
