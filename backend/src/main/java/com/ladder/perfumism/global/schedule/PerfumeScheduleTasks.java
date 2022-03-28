0ddff9a3d55286909850b9966e3538abda766970
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-28 16:09:23 +0900

    [S06P22D105-189] feat: PerfumeScheduleTasks 생성

---
+++ backend/src/main/java/com/ladder/perfumism/global/schedule/PerfumeScheduleTasks.java
@@ -0,0 +1,10 @@
+package com.ladder.perfumism.global.schedule;
+
+import com.ladder.perfumism.perfume.service.PerfumeService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PerfumeScheduleTasks {
+
+}
