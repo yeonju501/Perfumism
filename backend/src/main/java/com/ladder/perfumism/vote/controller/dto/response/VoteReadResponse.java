f70401f380ce6cad8419a625afa1a1bd08ae92a7
vtaco <woosj7129@naver.com>
2022-03-28 18:37:01 +0900

    [S06P22D105-228] feat: VoteReadResponse Json 및 응답 값

--- backend/src/main/java/com/ladder/perfumism/vote/controller/dto/response/VoteReadResponse.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/dto/response/VoteReadResponse.java
@@ -1,5 +1,31 @@
 package com.ladder.perfumism.vote.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.vote.domain.VoteItem;
+import lombok.Getter;
+
+@Getter
 public class VoteReadResponse {
 
+    @JsonProperty("vote_item_id")
+    private Long id;
+
+    @JsonProperty("content")
+    private String content;
+
+    public VoteReadResponse(){
+
+    }
+
+    public VoteReadResponse(Long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public static VoteReadResponse from(VoteItem voteItem){
+        return new VoteReadResponse(
+            voteItem.getId(),
+            voteItem.getContent()
+        );
+    }
 }
