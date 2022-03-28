c43dc77acaa22a68c539ee1405648ad19f96aecb
우동진 <wjinh93@gmail.com>
2022-03-28 13:49:09 +0900

    [S06P22D105-171] feat: numberofunread api 추가

--- frontend/src/apis/alert.ts
+++ frontend/src/apis/alert.ts
@@ -1,14 +1,16 @@
-import { AxiosResponse } from "axios";
+import { Axios, AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface AlertApiType {
 	getAllNotifications: () => Promise<AxiosResponse>;
 	getUnreadNotifications: () => Promise<AxiosResponse>;
+	getNumberOfUnread: () => Promise<AxiosResponse>;
 }
 
 const alertApi: AlertApiType = {
 	getAllNotifications: () => request.get("auth/notifications"),
 	getUnreadNotifications: () => request.get("auth/notifications/unread"),
+	getNumberOfUnread: () => request.get("auth/notifications/count-unread"),
 };
 
 export default alertApi;
