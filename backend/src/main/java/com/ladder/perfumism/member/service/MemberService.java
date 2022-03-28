bdbb5a597028ae3a4d4a0adbd8770916c9e2469e
yeonju501 <loling970124@gmail.com>
2022-03-28 17:52:48 +0900

    [S06P22D105-215] feat: MemberService changeProfileImage 추가

--- backend/src/main/java/com/ladder/perfumism/member/service/MemberService.java
+++ backend/src/main/java/com/ladder/perfumism/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ladder.perfumism.member.service;
 
 import com.ladder.perfumism.global.exception.BusinessException;
 import com.ladder.perfumism.global.exception.ErrorCode;
+import com.ladder.perfumism.image.ImageUploader;
 import com.ladder.perfumism.member.controller.dto.request.ChangePasswordRequest;
 import com.ladder.perfumism.member.controller.dto.request.CheckDuplicateRequest;
 import com.ladder.perfumism.member.controller.dto.request.FindPasswordRequest;
@@ -21,12 +22,14 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final MailService mailService;
+    private final ImageUploader imageUploader;
 
     public MemberService(MemberRepository memberRepository,
-        PasswordEncoder passwordEncoder, MailService mailService) {
+        PasswordEncoder passwordEncoder, MailService mailService, ImageUploader imageUploader) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
         this.mailService = mailService;
+        this.imageUploader = imageUploader;
     }
 
     @Transactional
@@ -46,18 +49,6 @@ public class MemberService {
         member.saveDeletedTime();
     }
 
-    private void checkDuplicateEmail(String email) {
-        if (memberRepository.existsByEmail(email)) {
-            throw new BusinessException(ErrorCode.MEMBER_EMAIL_DUPLICATED);
-        }
-    }
-
-    private void checkDuplicateUsername(String username) {
-        if (memberRepository.existsByUsername(username)) {
-            throw new BusinessException(ErrorCode.MEMBER_USERNAME_DUPLICATED);
-        }
-    }
-
     @Transactional(readOnly = true)
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email)
@@ -72,12 +63,6 @@ public class MemberService {
         return CodeResponse.from(code);
     }
 
-    @Transactional
-    public void changePassword(ChangePasswordRequest request) {
-        Member member = findByEmail(request.getEmail());
-        member.changePassword(passwordEncoder, request.getPassword());
-    }
-
     @Transactional(readOnly = true)
     public CheckDuplicateResponse checkDuplicateEmail(CheckDuplicateRequest request) {
         return CheckDuplicateResponse.from(memberRepository.existsByEmail(request.getValue()));
@@ -89,10 +74,35 @@ public class MemberService {
     }
 
     @Transactional
+    public void changePassword(ChangePasswordRequest request) {
+        Member member = findByEmail(request.getEmail());
+        member.changePassword(passwordEncoder, request.getPassword());
+    }
+
+    @Transactional
     public void changeMemberInfo(String email, MemberUpdateRequest request) {
         Member member = memberRepository.findByEmail(email).
             orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
         member.changeUsername(request.getUsername());
         member.changePassword(passwordEncoder, request.getPassword());
+    }
+
+    @Transactional
+    public void changeProfileImage(String email, String url) {
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_EMAIL));
+        member.changeImage(url);
+    }
+
+    private void checkDuplicateEmail(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new BusinessException(ErrorCode.MEMBER_EMAIL_DUPLICATED);
+        }
+    }
+
+    private void checkDuplicateUsername(String username) {
+        if (memberRepository.existsByUsername(username)) {
+            throw new BusinessException(ErrorCode.MEMBER_USERNAME_DUPLICATED);
+        }
     }
 }
