c0972f834c683dbd6a3b47ce9b3713a613f2459f
vtaco <woosj7129@naver.com>
2022-03-28 15:33:55 +0900

    [S06P22D105-227] feat: putVote Swagger

--- backend/src/main/java/com/ladder/perfumism/vote/controller/VoteRestController.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/VoteRestController.java
@@ -3,6 +3,9 @@ package com.ladder.perfumism.vote.controller;
 import com.ladder.perfumism.vote.controller.dto.request.VoteCreateRequest;
 import com.ladder.perfumism.vote.domain.VoteItem;
 import com.ladder.perfumism.vote.service.VoteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth/votes")
+@Api(tags = {"투표"})
 public class VoteRestController {
 
     private final VoteService voteService;
@@ -24,6 +28,8 @@ public class VoteRestController {
     }
 
     @PostMapping("/{article_id}")
+    @ApiOperation(value = "투표 만들기", notes = "<b>(로그인 필요)</b> 투표를 만드는 API")
+    @ApiImplicitParam(name = "article_id", value = "투표를 생성한 게시글", required = true)
     public ResponseEntity<Void> postVote(
         @AuthenticationPrincipal String email,
         @PathVariable(value = "article_id") Long articleId,
--- backend/src/main/java/com/ladder/perfumism/vote/controller/dto/request/VoteCreateRequest.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/dto/request/VoteCreateRequest.java
@@ -2,6 +2,7 @@ package com.ladder.perfumism.vote.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ladder.perfumism.vote.domain.VoteItem;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.Getter;
 
@@ -9,9 +10,11 @@ import lombok.Getter;
 public class VoteCreateRequest {
 
     @JsonProperty("title")
+    @ApiModelProperty(required = true, position = 0, notes = "투표 제목", example = "투표 합시다")
     private String title;
 
     @JsonProperty("vote_item_list")
+    @ApiModelProperty(required = true, position = 1, notes = "투표 항목", example = "[first, second, third]", dataType = "List")
     private List<String> voteItemList;
 
     public VoteCreateRequest(){
