bc6b774b75453ff45e87dc021a5a75d6ec45a834
9603100 <imgoing222@gmail.com>
2022-03-29 04:22:48 +0900

    [S06P22D105-251] feat: useInfiniteScroll 훅 사용해 변수 지정

--- frontend/src/pages/profile/Favorites.tsx
+++ frontend/src/pages/profile/Favorites.tsx
@@ -1,4 +1,10 @@
+import useInfiniteScroll from "pages/perfume/hooks/useInfiniteScroll";
+
 function Favorites() {
+	const { setTarget, perfumes, isLoading } = useInfiniteScroll({
+		type: "favoritePerfumes",
+	});
+
 	return <div>내가 좋아요한 향수 목록</div>;
 }
 
