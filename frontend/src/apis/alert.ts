4cd48b1623e9246977f7f22e045f845f19f451a6
우동진 <wjinh93@gmail.com>
2022-03-28 13:58:19 +0900

    [S06P22D105-171] feat: 남은 메시지 갯수 api 추가

--- frontend/src/apis/alert.ts
+++ frontend/src/apis/alert.ts
@@ -1,16 +1,16 @@
-import { Axios, AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface AlertApiType {
 	getAllNotifications: () => Promise<AxiosResponse>;
 	getUnreadNotifications: () => Promise<AxiosResponse>;
-	getNumberOfUnread: () => Promise<AxiosResponse>;
+	getNumOfUnread: () => Promise<AxiosResponse>;
 }
 
 const alertApi: AlertApiType = {
 	getAllNotifications: () => request.get("auth/notifications"),
 	getUnreadNotifications: () => request.get("auth/notifications/unread"),
-	getNumberOfUnread: () => request.get("auth/notifications/count-unread"),
+	getNumOfUnread: () => request.get("auth/notifications/count-unread"),
 };
 
 export default alertApi;
