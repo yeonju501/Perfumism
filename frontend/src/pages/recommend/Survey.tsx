df66205dae0a4cb3fe0762a61e6106c9a15e12c2
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-28 14:23:24 +0900

    [S06P22D105-238] feat: 이미지 import

--- frontend/src/pages/recommend/Survey.tsx
+++ frontend/src/pages/recommend/Survey.tsx
@@ -1,7 +1,15 @@
 import { useSearchParams, useLocation } from "react-router-dom";
 import { SurveyItem } from "components/recommend";
 import styled from "styled-components";
-import SeaSrc from "assets/바다.jpg";
+import FruitSrc from "assets/survey/과일.jpg";
+import FlowerSrc from "assets/survey/꽃.jpg";
+import NatureSrc from "assets/survey/자연.jpg";
+import SeaSrc from "assets/survey/바다.jpg";
+import SweetSrc from "assets/survey/달콤한.jpg";
+import PowderySrc from "assets/survey/포근한.jpg";
+import SmoothSrc from "assets/survey/부드러운.jpg";
+import OrientalSrc from "assets/survey/몽환적.jpg";
+import GrassSrc from "assets/survey/풀향의.jpg";
 
 function Survey() {
 	const [searchParams, setSearchParams] = useSearchParams();
@@ -22,9 +30,6 @@ function Survey() {
 			질문번호: "2",
 			질문: "질문2",
 			답변: [
-				{ url: SeaSrc, content: "바다" },
-				{ url: SeaSrc, content: "바다" },
-				{ url: SeaSrc, content: "바다" },
 				{ url: SeaSrc, content: "바다" },
 				{ url: SeaSrc, content: "바다" },
 				{ url: SeaSrc, content: "바다" },
