bc8206a0dea6b3977fd0026e802a2138dada22ac
vtaco <woosj7129@naver.com>
2022-03-28 18:37:49 +0900

    [S06P22D105-228] feat: Vote*Repository JPA findBy

--- backend/src/main/java/com/ladder/perfumism/vote/domain/VoteRepository.java
+++ backend/src/main/java/com/ladder/perfumism/vote/domain/VoteRepository.java
@@ -1,7 +1,10 @@
 package com.ladder.perfumism.vote.domain;
 
+import com.ladder.perfumism.article.domain.Article;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoteRepository extends JpaRepository<Vote,Long> {
 
+    Optional<Vote> findByArticle(Article article);
 }
