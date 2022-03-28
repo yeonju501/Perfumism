b9f4c7d3f04f2d746fd2f7a7053fa8034622a873
vtaco <woosj7129@naver.com>
2022-03-28 15:27:48 +0900

    [S06P22D105-227] feat: VoteRestController postVote() 구현

--- backend/src/main/java/com/ladder/perfumism/vote/controller/VoteRestController.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/VoteRestController.java
@@ -1,12 +1,16 @@
 package com.ladder.perfumism.vote.controller;
 
 import com.ladder.perfumism.vote.controller.dto.request.VoteCreateRequest;
+import com.ladder.perfumism.vote.domain.VoteItem;
 import com.ladder.perfumism.vote.service.VoteService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,11 +23,14 @@ public class VoteRestController {
         this.voteService = voteService;
     }
 
-    @PostMapping
+    @PostMapping("/{article_id}")
     public ResponseEntity<Void> postVote(
         @AuthenticationPrincipal String email,
+        @PathVariable(value = "article_id") Long articleId,
         @RequestBody VoteCreateRequest request){
 
-//        voteService
+        voteService.createVote(email,articleId,request);
+
+        return ResponseEntity.noContent().build();
     }
 }
