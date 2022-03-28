cf9cc3ae7b1e27268153977937be85fe3e5adfda
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-28 16:10:31 +0900

    [S06P22D105-189] feat: PerfumeScheduleTasks 에 매월 1일 0시 1분 마다 실행되는 스케쥴 메서드 추가

--- backend/src/main/java/com/ladder/perfumism/global/schedule/PerfumeScheduleTasks.java
+++ backend/src/main/java/com/ladder/perfumism/global/schedule/PerfumeScheduleTasks.java
@@ -7,4 +7,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class PerfumeScheduleTasks {
 
+    private final PerfumeService perfumeService;
+
+    public PerfumeScheduleTasks(PerfumeService perfumeService) {
+        this.perfumeService = perfumeService;
+    }
+
+    @Scheduled(cron = "0 1 0 1 * *")
+    // 초 분 시 일 월 요일
+    // 즉 매월 1일 0시 1분에 새로고침
+    public void refreshingMonthlyPerfume() {
+        perfumeService.refreshingMonthlyPerfumeList();
+    }
 }
