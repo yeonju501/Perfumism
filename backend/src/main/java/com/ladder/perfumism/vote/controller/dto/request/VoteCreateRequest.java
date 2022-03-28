5ad4d43c9b5e43ac978408b8b0df53e68f1b14c2
vtaco <woosj7129@naver.com>
2022-03-28 15:26:35 +0900

    [S06P22D105-227] feat: VoteCreateRequest 구현

--- backend/src/main/java/com/ladder/perfumism/vote/controller/dto/request/VoteCreateRequest.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/dto/request/VoteCreateRequest.java
@@ -1,5 +1,25 @@
 package com.ladder.perfumism.vote.controller.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.vote.domain.VoteItem;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
 public class VoteCreateRequest {
 
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("vote_item_list")
+    private List<String> voteItemList;
+
+    public VoteCreateRequest(){
+
+    }
+
+    public VoteCreateRequest(String title, List<String> voteItemList){
+        this.title = title;
+        this.voteItemList = voteItemList;
+    }
 }
