a50b6511681a79561c37fb42083c0ba1330cccc0
우동진 <wjinh93@gmail.com>
2022-03-28 14:12:01 +0900

    [S06P22D105-171] feat: 읽음 처리 api구현

--- frontend/src/apis/alert.ts
+++ frontend/src/apis/alert.ts
@@ -5,12 +5,14 @@ interface AlertApiType {
 	getAllNotifications: () => Promise<AxiosResponse>;
 	getUnreadNotifications: () => Promise<AxiosResponse>;
 	getNumOfUnread: () => Promise<AxiosResponse>;
+	readNotification: (id: number) => Promise<AxiosResponse>;
 }
 
 const alertApi: AlertApiType = {
 	getAllNotifications: () => request.get("auth/notifications"),
 	getUnreadNotifications: () => request.get("auth/notifications/unread"),
 	getNumOfUnread: () => request.get("auth/notifications/count-unread"),
+	readNotification: (id) => request.put(`auth/notifications/${id}`),
 };
 
 export default alertApi;
