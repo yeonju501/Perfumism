8b0bc21890545a05bd497a3785e868ae63d10272
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-28 16:06:16 +0900

    [S06P22D105-189] feat: PerfumeService 에 이달의 향수 계산하는 메서드 추가

--- backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeService.java
+++ backend/src/main/java/com/ladder/perfumism/perfume/service/PerfumeService.java
@@ -10,11 +10,17 @@ import com.ladder.perfumism.perfume.domain.BrandRepository;
 import com.ladder.perfumism.perfume.domain.Perfume;
 import com.ladder.perfumism.perfume.domain.PerfumeAccord;
 import com.ladder.perfumism.perfume.domain.PerfumeAccordRepository;
+import com.ladder.perfumism.perfume.domain.PerfumeMonthly;
+import com.ladder.perfumism.perfume.domain.PerfumeMonthlyRepository;
 import com.ladder.perfumism.perfume.domain.PerfumeRepository;
 import com.ladder.perfumism.perfume.domain.SimilarPerfume;
 import com.ladder.perfumism.perfume.domain.SimilarPerfumeRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,14 +32,17 @@ public class PerfumeService {
     private final PerfumeAccordRepository perfumeAccordRepository;
     private final SimilarPerfumeRepository similarPerfumeRepository;
     private final BrandRepository brandRepository;
+    private final PerfumeMonthlyRepository perfumeMonthlyRepository;
 
     public PerfumeService(PerfumeRepository perfumeRepository,
         PerfumeAccordRepository perfumeAccordRepository,
-        SimilarPerfumeRepository similarPerfumeRepository, BrandRepository brandRepository) {
+        SimilarPerfumeRepository similarPerfumeRepository, BrandRepository brandRepository,
+        PerfumeMonthlyRepository perfumeMonthlyRepository) {
         this.perfumeRepository = perfumeRepository;
         this.perfumeAccordRepository = perfumeAccordRepository;
         this.similarPerfumeRepository = similarPerfumeRepository;
         this.brandRepository = brandRepository;
+        this.perfumeMonthlyRepository = perfumeMonthlyRepository;
     }
 
     @Transactional(readOnly = true)
@@ -63,8 +72,20 @@ public class PerfumeService {
     }
 
     @Transactional(readOnly = true)
-    public Perfume findById(Long perfumeId){
+    public Perfume findById(Long perfumeId) {
         return perfumeRepository.findById(perfumeId)
             .orElseThrow(() -> new BusinessException(ErrorCode.PERFUME_NOT_FOUND_BY_ID));
+    }
+
+    @Transactional
+    public void refreshingMonthlyPerfumeList() {
+        LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now().minusMonths(1), LocalTime.of(0, 0, 0));
+        LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59));
+
+        List<PerfumeMonthly> monthlies = perfumeMonthlyRepository.CountLikePerfume(startDatetime, endDatetime,
+            PageRequest.of(0, 3));
+
+        perfumeMonthlyRepository.deleteAll();
+        perfumeMonthlyRepository.saveAll(monthlies);
     }
 }
