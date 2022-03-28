ea7fb5593d1f6d2013fed9cebb91a728224894ff
9603100 <imgoing222@gmail.com>
2022-03-29 00:19:28 +0900

    [S06P22D105-241] feat: 향수 브랜드 페이지 구현

--- frontend/src/pages/perfume/PerfumeBrand.tsx
+++ frontend/src/pages/perfume/PerfumeBrand.tsx
@@ -1,5 +1,26 @@
+import PerfumeList from "components/perfume/PerfumeList";
+import { useParams } from "react-router-dom";
+import useInfiniteScroll from "./hooks/useInfiniteScroll";
+
+type Params = {
+	brandName: string;
+};
+
 function PerfumeBrand() {
-	return <div>향수 브랜드 페이지</div>;
+	const { brandName } = useParams() as Params;
+
+	const { setTarget, perfumes, isLoading } = useInfiniteScroll({
+		type: "brandPerfumes",
+		brandName,
+	});
+
+	return (
+		<div>
+			<h1>{brandName} 의 향수</h1>
+			<PerfumeList perfumes={perfumes} />
+			<div ref={setTarget}>{isLoading && <p>Loading..</p>}</div>
+		</div>
+	);
 }
 
 export default PerfumeBrand;
