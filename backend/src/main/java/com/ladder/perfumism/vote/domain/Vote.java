db4746e2d4abfabfe852a3e157c00f3767a4f3be
vtaco <woosj7129@naver.com>
2022-03-28 13:42:50 +0900

    [S06P22D105-227] feat: Vote Entity 생성

--- backend/src/main/java/com/ladder/perfumism/vote/domain/Vote.java
+++ backend/src/main/java/com/ladder/perfumism/vote/domain/Vote.java
@@ -1,5 +1,32 @@
 package com.ladder.perfumism.vote.domain;
 
-public class Vote {
+import com.ladder.perfumism.article.domain.Article;
+import com.ladder.perfumism.global.domain.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import org.hibernate.annotations.Where;
 
+@Entity
+@Getter
+@Where(clause = "deleted_at is null")
+public class Vote extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vote_id")
+    private Long id;
+
+    @OneToOne(targetEntity = Article.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article member;
+
+    @Column(name = "title", nullable = false)
+    private String title;
 }
