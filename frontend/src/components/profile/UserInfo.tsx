80877d68262e62ca0700e1cc303e2de8817b8c24
9603100 <imgoing222@gmail.com>
2022-03-30 06:45:52 +0900

    [S06P22D105-259] feat: handleImageChange 함수 구현

--- frontend/src/components/profile/UserInfo.tsx
+++ frontend/src/components/profile/UserInfo.tsx
@@ -1,4 +1,7 @@
+import { profileApi } from "apis";
 import anonymous from "assets/anonymous.jpg";
+import useSetImage from "hooks/useSetImage";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
 import styled from "styled-components";
@@ -6,10 +9,20 @@ import styled from "styled-components";
 function UserInfo() {
 	const { email, image, social_id, username } = useSelector((state: RootState) => state.user);
 
+	const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+		const imageFile = useSetImage(e);
+		const formData = new FormData();
+		if (imageFile) {
+			formData.append("image", imageFile);
+			await profileApi.setUserImage(formData).then(() => alert("프로필 이미지가 변경되었습니다"));
+			// dispatch
+		}
+	};
+
 	return (
 		<div>
-			<UserImage src={anonymous} alt="user profile" />
-			<input type="file" />
+			<UserImage src={image ? image : anonymous} alt="user profile" />
+			<input type="file" onChange={handleImageChange} />
 			<p>{username}</p>
 			<p>{email || social_id}</p>
 		</div>
