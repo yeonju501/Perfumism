49b9754bb3601ecdf28a3f8085478be31d875fba
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-30 01:17:31 +0900

    [S06P22D105-218] feat: Dropdown 생성 및 디자인 적용

--- frontend/src/components/community/Dropdown.tsx
+++ frontend/src/components/community/Dropdown.tsx
@@ -1,15 +1,81 @@
+import { useState } from "react";
 import styled from "styled-components";
 
+const options = ["전체", "추천", "골라", "잡담"];
+
 function Dropdown() {
-	return <Container></Container>;
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+	const toggling = () => setIsOpen(!isOpen);
+
+	const onOptionClicked = (value: string) => () => {
+		setSelectedOption(value);
+		setIsOpen(false);
+		console.log(selectedOption);
+	};
+	return (
+		<Main>
+			<DropDownContainer>
+				<DropDownHeader onClick={toggling}>{selectedOption || "전체"}</DropDownHeader>
+				{isOpen && (
+					<DropDownListContainer>
+						<DropDownList>
+							{options.map((option) => (
+								<ListItem onClick={onOptionClicked(option)} key={Math.random()}>
+									{option}
+								</ListItem>
+							))}
+						</DropDownList>
+					</DropDownListContainer>
+				)}
+			</DropDownContainer>
+		</Main>
+	);
 }
 
-const Container = styled.div`
-	width: 400px;
+const Main = styled("div")`
+	font-family: sans-serif;
+`;
 
-	&:hover {
-		cursor: pointer;
+const DropDownContainer = styled("div")`
+	width: 8em;
+	margin: 0 auto;
+`;
+
+const DropDownHeader = styled("div")`
+	text-align: center;
+	margin-bottom: 0.8em;
+	padding: 0.4em 1em 0.4em 1em;
+	border: 2px solid #e5e5e5;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+	font-weight: 500;
+	font-size: 1.3rem;
+	background: #ffffff;
+`;
+
+const DropDownListContainer = styled("div")`
+	position: absolute;
+	width: 8rem;
+`;
+
+const DropDownList = styled("ul")`
+	text-align: center;
+	padding: 0;
+	margin: 0;
+	background: #ffffff;
+	border: 2px solid #e5e5e5;
+	box-sizing: border-box;
+	font-size: 1.3rem;
+	font-weight: 500;
+	&:first-child {
+		padding-top: 0.8em;
 	}
+`;
+
+const ListItem = styled("li")`
+	list-style: none;
+	margin-bottom: 0.8em;
 `;
 
 export default Dropdown;
