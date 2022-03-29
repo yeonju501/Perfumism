c28f768fcb5e2b83953698e2edaad4802d7a1d5d
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-29 09:20:30 +0900

    [S06P22D105-224] feat: 설문 기반 추천 알고리즘 함수화

--- data/data_algorithms/algorithms/dbscan.py
+++ data/data_algorithms/algorithms/dbscan.py
@@ -63,6 +63,62 @@ def recommend_like_based(accord_list):
 
     return result
 
+
+def recommend_survey(answer_list):
+    accord_list = []
+    accord_list += mapping_table[0][answer_list[0]] + mapping_table[1][answer_list[1]]
+  
+    input_data = {
+        "id": "",
+        "perfume": "user",
+        "brand": "",
+        "image": "",
+        "launch_year": 0,
+        "main_accords": accord_list,
+        "notes": "",
+        "longevity": "",
+        "sillage": "",
+        "total_survey": 0,
+        "similar_perfume": []
+    }
+
+    if answer_list[4] == 0:
+        df = pd.read_json("./data_algorithms/algorithms/popular.json")
+    else:
+        df = pd.read_json("unpopular.json")
+
+    df = df.append(input_data, ignore_index=True)
+
+    if answer_list[4] == 0:
+        new_df = df
+    else:
+        tfidfv = TfidfVectorizer().fit(df['main_accords'])
+        data = tfidfv.transform(df['main_accords']).toarray()
+        data = pd.DataFrame(data)
+
+        model = DBSCAN(eps=0.5, min_samples=20)
+        predict = pd.DataFrame(model.fit_predict(data))
+        predict.columns = ['predict']
+
+        filt = (predict['predict'] == predict.iloc[-1, 0])
+        new_df = df[filt]
+
+    feature = new_df['main_accords'] + ' ' + new_df['notes']
+
+    tfidf_vector = TfidfVectorizer(ngram_range=(1, 1))
+    c_vector_perfume = tfidf_vector.fit_transform(feature)
+
+    perfume_c_sim = cosine_similarity(c_vector_perfume, c_vector_perfume).argsort()[:, ::-1]
+
+    if len(new_df) < 30:
+        sim_index = perfume_c_sim[len(new_df) - 1].reshape(-1)
+    else:
+        sim_index = perfume_c_sim[len(new_df) - 1, :30].reshape(-1)
+    sim_index = sim_index[sim_index != len(new_df) - 1]
+    result = new_df.iloc[sim_index][:3].to_dict('list')['id']
+
+    return result
+
 if __name__ == '__main__':
     arr = "wine vanilla sweet woody aromatic leather fruity warm spicy powdery animalic fresh violet"
     result = recommend_like_based(arr)
