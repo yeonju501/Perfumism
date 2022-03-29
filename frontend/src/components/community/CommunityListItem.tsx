0cab20f4ace6df3959fdc009c288db9e9623b66a
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-30 02:21:47 +0900

    [S06P22D105-218] feat: 커뮤니티 리스트 컴포넌트 생성

---
+++ frontend/src/components/community/CommunityListItem.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function CommunityListItem() {
+	return <Container></Container>;
+}
+
+const Container = styled.div`
+	width: 100%;
+`;
+
+export default CommunityListItem;
