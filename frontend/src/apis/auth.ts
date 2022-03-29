8ac29cc073b9ca1aeca50179e36c41801ae6c66b
우동진 <wjinh93@gmail.com>
2022-03-30 00:23:41 +0900

    [S06P22D105-50] feat: reissue api 수정

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { request } from "./request";
+import { request, authRequest } from "./request";
 import cookie from "react-cookies";
 
 interface UserApiType {
@@ -21,7 +21,7 @@ const authApi: UserApiType = {
 	signup: (userInfo) => request.post("members/join", userInfo),
 	isExist: (name, value) => request.post(`members/exist-${name}`, value),
 	signin: (userInfo) => request.post("members/login", userInfo),
-	reissue: (data) => request.post("auth/members/reissue", data),
+	reissue: (data) => authRequest.post("members/reissue", data),
 	findPassword: (email) => request.post("members/find-pw", email),
 	changePassword: (email, password) => request.put("members/change-pw", { email, password }),
 	logout: () => cookie.remove("access_token", { path: "/" }),
