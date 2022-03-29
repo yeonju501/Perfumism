206118ce8df9e7a43da93625b9d15cde714ef73d
vtaco <woosj7129@naver.com>
2022-03-30 01:31:59 +0900

    [S06P22D105-236] feat: ArticleImageResponse 구현 - 이미지 url 응답

--- backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleImageResponse.java
+++ backend/src/main/java/com/ladder/perfumism/article/controller/dto/response/ArticleImageResponse.java
@@ -1,7 +1,28 @@
 package com.ladder.perfumism.article.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ladder.perfumism.article.domain.ArticleImage;
 import com.ladder.perfumism.global.domain.BaseEntity;
+import lombok.Getter;
 
+@Getter
 public class ArticleImageResponse extends BaseEntity {
 
+    @JsonProperty("article_image_id")
+    private Long articleImageId;
+
+    @JsonProperty("image_url")
+    private String image_url;
+
+    public ArticleImageResponse(Long articleImageId, String image_url){
+        this.articleImageId = articleImageId;
+        this.image_url = image_url;
+    }
+
+    public static ArticleImageResponse from(ArticleImage articleImage){
+        return new ArticleImageResponse(
+            articleImage.getId(),
+            articleImage.getUrl()
+        );
+    }
 }
