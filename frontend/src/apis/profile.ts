0fc454980d7c9fa48f27e586fe0599274c170049
9603100 <imgoing222@gmail.com>
2022-03-30 06:45:18 +0900

    [S06P22D105-259] feat: 프로필 이미지 설정 요청 api 함수 생성

--- frontend/src/apis/profile.ts
+++ frontend/src/apis/profile.ts
@@ -1,14 +1,16 @@
 import { AxiosResponse } from "axios";
-import { request } from "./request";
+import { imageRequest, request } from "./request";
 
 interface ProfileApiType {
 	getFavorites: () => Promise<AxiosResponse>;
 	getUserInfo: () => Promise<AxiosResponse>;
+	setUserImage: (formData: FormData) => Promise<AxiosResponse>;
 }
 
 const profileApi: ProfileApiType = {
 	getFavorites: () => request.get(`auth/perfumes/likes/my-favorite`),
 	getUserInfo: () => request.get(`auth/members`),
+	setUserImage: (formData) => imageRequest.post(`auth/members/img`, formData),
 };
 
 export default profileApi;
