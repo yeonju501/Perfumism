ac0a05bfa655be274776d90a43a8cef7cf931097
vtaco <woosj7129@naver.com>
2022-03-29 15:42:30 +0900

    [S06P22D105-231] feat: VoteReadResponse vote_member_count 응답에 추가 - 투표자 수

--- backend/src/main/java/com/ladder/perfumism/vote/controller/dto/response/VoteReadResponse.java
+++ backend/src/main/java/com/ladder/perfumism/vote/controller/dto/response/VoteReadResponse.java
@@ -16,19 +16,24 @@ public class VoteReadResponse {
     @ApiModelProperty(position = 1, notes = "투표 항목", example = "first")
     private String content;
 
+    @JsonProperty("vote_member_count")
+    private int vote_member_count;
+
     public VoteReadResponse(){
 
     }
 
-    public VoteReadResponse(Long id, String content){
+    public VoteReadResponse(Long id, String content, int vote_member_count){
         this.id = id;
         this.content = content;
+        this.vote_member_count = vote_member_count;
     }
 
     public static VoteReadResponse from(VoteItem voteItem){
         return new VoteReadResponse(
             voteItem.getId(),
-            voteItem.getContent()
+            voteItem.getContent(),
+            voteItem.getVote_member_count()
         );
     }
 }
