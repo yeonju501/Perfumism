9383f45c218dc3e98f26ffdb89516cdd4beb23e9
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-29 13:27:21 +0900

    [S06P22D105-252] feat: 좋아요 기반 api 생성

--- frontend/src/apis/recommend.ts
+++ frontend/src/apis/recommend.ts
@@ -9,11 +9,13 @@ interface RecommendApiType {
 		a4: number,
 		a5: number,
 	) => Promise<AxiosResponse>;
+	likeBasedRecommend: (id: number) => Promise<AxiosResponse>;
 }
 
 const recommendApi: RecommendApiType = {
 	surveyRecommend: (a1, a2, a3, a4, a5) =>
 		request.get(`algorithms/survey/${a1}/${a2}/${a3}/${a4}/${a5}`),
+	likeBasedRecommend: (id) => request.get(`algorithms/${id}/like-based`),
 };
 
 export default recommendApi;
