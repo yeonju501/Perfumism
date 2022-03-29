bd2985e77c7b770d94adb233bcceb70a76c1685b
9603100 <imgoing222@gmail.com>
2022-03-29 20:46:22 +0900

    [S06P22D105-255] feat: handleCategoryClick 함수 구현

--- frontend/src/pages/perfume/hooks/useInfiniteScroll.tsx
+++ frontend/src/pages/perfume/hooks/useInfiniteScroll.tsx
@@ -1,6 +1,8 @@
 import perfumeApi from "apis/perfume";
 import profileApi from "apis/profile";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 import useIntersectionObserver from "./useIntersectionObserver";
 
 interface PerfumeType {
@@ -22,33 +24,45 @@ const useInfiniteScroll = ({ type, brandName }: useInfiniteScrollProps) => {
 	const [currentPage, setCurrentPage] = useState(0);
 	const [isLoading, setIsLoading] = useState(false);
 
+	const filter = useSelector((state: RootState) => state.filter);
+
 	useEffect(() => {
 		if (currentPage && currentPage >= totalPage) return;
 		getPerfumes();
 	}, [currentPage]);
 
 	const getPerfumes = async () => {
-		console.log(currentPage);
 		setIsLoading(true);
 		await new Promise((resolve) => setTimeout(resolve, 800));
-		if (type === "perfumes")
-			await perfumeApi.getPerfumes(currentPage).then((res) => {
-				setPerfumes((prev) => prev.concat(res.data.perfumes));
-				setTotalPage(res.data.total_page_count);
-				setCurrentPage(res.data.current_page_count);
-			});
-		else if (type === "brandPerfumes")
-			await perfumeApi.getBrandPerfumes(brandName, currentPage).then((res) => {
-				setPerfumes((prev) => prev.concat(res.data.perfumes));
-				setTotalPage(res.data.total_page_count);
-				setCurrentPage(res.data.current_page_count);
-			});
-		else if (type === "favoritePerfumes")
-			await profileApi.getFavorites().then((res) => {
-				setPerfumes((prev) => prev.concat(res.data.perfumes));
-				setTotalPage(res.data.total_page_count);
-				setCurrentPage(res.data.current_page_count);
-			});
+		if (filter.accord === "" && filter.sort === "") {
+			if (type === "perfumes")
+				await perfumeApi.getPerfumes(currentPage).then((res) => {
+					setPerfumes((prev) => prev.concat(res.data.perfumes));
+					setTotalPage(res.data.total_page_count);
+					setCurrentPage(res.data.current_page_count);
+				});
+			else if (type === "brandPerfumes")
+				await perfumeApi.getBrandPerfumes(brandName, currentPage).then((res) => {
+					setPerfumes((prev) => prev.concat(res.data.perfumes));
+					setTotalPage(res.data.total_page_count);
+					setCurrentPage(res.data.current_page_count);
+				});
+			else if (type === "favoritePerfumes")
+				await profileApi.getFavorites().then((res) => {
+					setPerfumes((prev) => prev.concat(res.data.perfumes));
+					setTotalPage(res.data.total_page_count);
+					setCurrentPage(res.data.current_page_count);
+				});
+		} else {
+			if (type === "perfumes")
+				await perfumeApi
+					.getPerfumesByAccord(filter.accord, currentPage, filter.sort, filter.order)
+					.then((res) => {
+						setPerfumes((prev) => prev.concat(res.data.perfumes));
+						setTotalPage(res.data.total_page_count);
+						setCurrentPage(res.data.current_page_count);
+					});
+		}
 		setIsLoading(false);
 	};
 
