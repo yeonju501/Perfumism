73ec28464eeaee65e0dcbf8b9f15051478189853
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-29 15:45:38 +0900

    [S06P22D105-218] feat: 커뮤니티 파일 생성

---
+++ frontend/src/pages/community/Community.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function Community() {
+	return <Container></Container>;
+}
+
+const Container = styled.div`
+	height: 80vh;
+`;
+
+export default Community;
