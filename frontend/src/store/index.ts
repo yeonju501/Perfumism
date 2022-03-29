5c6c5ad9a48349e515ca8941ae39d901b63afeed
9603100 <imgoing222@gmail.com>
2022-03-29 16:07:43 +0900

    [S06P22D105-55] feat: filter reducer 생성

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,11 @@
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
+import filter from "./filter";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+	filter,
+});
 
 const persistConfig = {
 	key: "root",
