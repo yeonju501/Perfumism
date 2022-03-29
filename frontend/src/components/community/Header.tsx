c28ab4350e4b44c2380a166d49f1b9ad14056ec7
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-29 16:16:26 +0900

    [S06P22D105-218] feat: 메인페이지 헤더 기본 틀 생성

--- frontend/src/components/community/Header.tsx
+++ frontend/src/components/community/Header.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
 function Header() {
-	return <></>;
+	return (
+		<Container>
+			<div>dropdown</div>
+			<div>글쓰기버튼</div>
+		</Container>
+	);
 }
+
+const Container = styled.div`
+	display: flex;
+`;
 
 export default Header;
