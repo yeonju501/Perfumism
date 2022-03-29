647c7aebdc8a58a8635ccd9387d56aa5b39d410d
9603100 <imgoing222@gmail.com>
2022-03-29 16:53:18 +0900

    [S06P22D105-55] feat: handleCategoryClick 함수 구현

--- frontend/src/components/perfume/PerfumeFilter.tsx
+++ frontend/src/components/perfume/PerfumeFilter.tsx
@@ -1,32 +1,43 @@
+import { useDispatch } from "react-redux";
+import { SET_FILTER } from "store/filter";
+
+const categories = [
+	"All",
+	"woody",
+	"sweet",
+	"fresh",
+	"floral",
+	"musky",
+	"fruity",
+	"green",
+	"aromatic",
+	"aquatic",
+	"vanilla",
+	"amber",
+	"citrus",
+	"powdery",
+	"aldehydic",
+	"tropical",
+	"animalic",
+	"fresh spicy",
+	"white floral",
+	"warm spicy",
+];
+
 function PerfumeFilter() {
-	const categories = [
-		"All",
-		"woody",
-		"sweet",
-		"fresh",
-		"floral",
-		"musky",
-		"fruity",
-		"green",
-		"aromatic",
-		"aquatic",
-		"vanilla",
-		"amber",
-		"citrus",
-		"powdery",
-		"aldehydic",
-		"tropical",
-		"animalic",
-		"fresh spicy",
-		"white floral",
-		"warm spicy",
-	];
+	const dispatch = useDispatch();
+
+	const handleCategoryClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const category = e.target as HTMLElement;
+		const accord = category.innerText;
+		dispatch(SET_FILTER({ accord, sort: "desc" }));
+	};
 
 	return (
 		<div>
 			<div>
 				{categories.map((category) => (
-					<button>{category}</button>
+					<button onClick={handleCategoryClick}>{category}</button>
 				))}
 			</div>
 			<div>드롭다운 필터</div>
