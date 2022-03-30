6780a4dbb6fc83959b5c15b48dcf412410d2f8ae
우동진 <wjinh93@gmail.com>
2022-03-30 19:39:42 +0900

    [S06P22D105-169] feat: 이달의 향수 컴포넌트 구현

--- frontend/src/components/main/MonthPerfume.tsx
+++ frontend/src/components/main/MonthPerfume.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 interface Props {
 	perfume_id: number;
@@ -12,12 +13,45 @@ interface Props {
 	};
 }
 
-interface Perfumes {
-	perfumes: Props[];
+interface Perfume {
+	perfume: Props;
 }
 
-function MonthPerfume({ perfumes }: Perfumes) {
-	return <div>MonthOfPerfume</div>;
+function MonthPerfume({ perfume }: Perfume) {
+	return (
+		<Container to={`perfume/${perfume.perfume_id}`}>
+			<Image src={perfume.image} />
+			<PerfumeName>{perfume.perfume_name}</PerfumeName>
+			<BrandAndGrade>
+				{perfume.brand.brand_name} {perfume.average_grade}
+			</BrandAndGrade>
+		</Container>
+	);
 }
 
 export default MonthPerfume;
+
+const Container = styled(Link)`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	position: relative;
+	color: #000;
+	width: 100%;
+`;
+
+const Image = styled.img`
+	width: 20rem;
+	height: 30rem;
+`;
+
+const PerfumeName = styled.p`
+	font-size: 2rem;
+	font-weight: 800;
+`;
+
+const BrandAndGrade = styled.p`
+	font-size: 1.3rem;
+`;
