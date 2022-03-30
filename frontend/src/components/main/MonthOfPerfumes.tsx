19ee1dfd14a2d13c015ce7cdabf3e1d64c777135
우동진 <wjinh93@gmail.com>
2022-03-30 13:39:44 +0900

    [S06P22D105-169] feat: 이달의 향수 생성

---
+++ frontend/src/components/main/MonthOfPerfumes.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function MonthOfPerfumes() {
+	return <div>MonthOfPerfumes</div>;
+}
+
+export default MonthOfPerfumes;
+
+const Section = styled.section`
+	display: flex;
+	width: 100%;
+	height: 50rem;
+`;
