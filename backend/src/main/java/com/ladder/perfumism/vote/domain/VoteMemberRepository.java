19afdea07fa997a209ed5c44dfe3775253865a26
vtaco <woosj7129@naver.com>
2022-03-30 15:39:11 +0900

    [S06P22D105-248] feat: Vote**Repository soft delete Query

--- backend/src/main/java/com/ladder/perfumism/vote/domain/VoteMemberRepository.java
+++ backend/src/main/java/com/ladder/perfumism/vote/domain/VoteMemberRepository.java
@@ -3,6 +3,9 @@ package com.ladder.perfumism.vote.domain;
 import com.ladder.perfumism.member.domain.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoteMemberRepository extends JpaRepository<VoteMember,Long> {
 
@@ -12,4 +15,9 @@ public interface VoteMemberRepository extends JpaRepository<VoteMember,Long> {
 
     Integer countByVote(Vote vote);
 
+    @Modifying
+    @Query(nativeQuery = true, value = "update vote_member vm "
+        + "set vm.deleted_at = current_timestamp "
+        + "where vm.article_id = (:article_id)")
+    Integer updateDeletedAtByArticle(@Param("article_id") Long article_id);
 }
