6f5c2f39424b4431f8a0cc84aad9b94fb5043571
9603100 <imgoing222@gmail.com>
2022-03-30 12:06:03 +0900

    [S06P22D105-263] feat: getBrandPerfumes 함수 props 변경

--- frontend/src/apis/perfume.ts
+++ frontend/src/apis/perfume.ts
@@ -13,7 +13,11 @@ interface PerfumeApiType {
 	isPerfumeLiked: (perfumeId: string | number) => Promise<AxiosResponse>;
 	addFavoritePerfume: (perfumeId: string | number) => Promise<AxiosResponse>;
 	deleteFavoritePerfume: (perfumeId: string | number) => Promise<AxiosResponse>;
-	getBrandPerfumes: (brandName: string | undefined, currentPage: number) => Promise<AxiosResponse>;
+	getBrandPerfumes: (
+		brandName: string | undefined,
+		currentPage: number,
+		sort: string,
+	) => Promise<AxiosResponse>;
 }
 
 const perfumeApi: PerfumeApiType = {
@@ -27,9 +31,9 @@ const perfumeApi: PerfumeApiType = {
 	isPerfumeLiked: (perfumeId) => request.get(`auth/perfumes/likes/${perfumeId}`),
 	addFavoritePerfume: (perfumeId) => request.post(`auth/perfumes/likes/${perfumeId}`),
 	deleteFavoritePerfume: (perfumeId) => request.delete(`auth/perfumes/likes/${perfumeId}`),
-	getBrandPerfumes: (brandName, currentPage) =>
+	getBrandPerfumes: (brandName, currentPage, sort) =>
 		request.get(
-			`/perfumes/search/?keyword=${brandName}&page=${currentPage}&size=10&sort=totalSurvey%2Cdesc&type=brand`,
+			`/perfumes/search/?keyword=${brandName}&page=${currentPage}&size=10&sort=${sort}%2Cdesc&type=brand`,
 		),
 };
 
