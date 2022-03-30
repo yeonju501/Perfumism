d461b183efaba533037c6627cb3d8c9ee9bf3c55
9603100 <imgoing222@gmail.com>
2022-03-31 04:13:23 +0900

    [S06P22D105-268] refactor: 향수 목록 useInfiniteScroll 함수 수정

--- frontend/src/pages/perfume/Perfumes.tsx
+++ frontend/src/pages/perfume/Perfumes.tsx
@@ -1,9 +1,22 @@
+import perfumeApi from "apis/perfume";
 import PerfumeFilter from "components/perfume/PerfumeFilter";
 import PerfumeList from "components/perfume/PerfumeList";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 import useInfiniteScroll from "./hooks/useInfiniteScroll";
 
 function Perfumes() {
-	const { setTarget, perfumes, isLoading } = useInfiniteScroll({ type: "perfumes" });
+	const { accord, sort, order } = useSelector((state: RootState) => state.filter);
+
+	const { setTarget, perfumes, isLoading } = useInfiniteScroll({
+		requestApi: (currentPage) => {
+			return accord
+				? perfumeApi.getPerfumesByAccord(accord, currentPage, sort, order)
+				: perfumeApi.getPerfumes(currentPage, sort, order);
+		},
+	});
+
+	console.log(1);
 
 	return (
 		<div>
