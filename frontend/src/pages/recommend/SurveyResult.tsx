56f679195c9322d396e500df69bd7deb701ae149
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-30 17:51:03 +0900

    [S06P22D105-218] feat: 추천 향수 데이터 바인딩

--- frontend/src/pages/recommend/SurveyResult.tsx
+++ frontend/src/pages/recommend/SurveyResult.tsx
@@ -7,31 +7,13 @@ import styled from "styled-components";
 interface CustomizedState {
 	recommendData: {
 		perfume_id: string;
-		name: string;
+		perfume_name: string;
 		image: string;
 	}[];
 }
 
 function SurveyResult() {
 	const { recommendData } = useLocation().state as CustomizedState;
-
-	const perfumeData = [
-		{
-			perfume_id: recommendData[0]["perfume_id"],
-			perfume_name: recommendData[0]["name"],
-			image: recommendData[0]["image"],
-		},
-		{
-			perfume_id: recommendData[1]["perfume_id"],
-			perfume_name: recommendData[1]["name"],
-			image: recommendData[1]["image"],
-		},
-		{
-			perfume_id: recommendData[2]["perfume_id"],
-			perfume_name: recommendData[2]["name"],
-			image: recommendData[2]["image"],
-		},
-	];
 	const accordData = {
 		accords: ["powdery", "woody", "violet"],
 	};
@@ -40,7 +22,7 @@ function SurveyResult() {
 		<Container>
 			<AccordRecommend accords={accordData["accords"]} />
 			<WordCloud />
-			<PerfumeRecommend perfumeData={perfumeData} />
+			<PerfumeRecommend perfumeData={recommendData} />
 		</Container>
 	);
 }
