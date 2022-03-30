b54059017ad433f6958c39a7d55689e45e5389f0
우동진 <wjinh93@gmail.com>
2022-03-30 20:51:01 +0900

    [S06P22D105-170] feat: 두번째 함수 추가

--- frontend/src/components/navbar/hooks/useOutside.tsx
+++ frontend/src/components/navbar/hooks/useOutside.tsx
@@ -3,13 +3,15 @@ import { useEffect } from "react";
 interface Props<T> {
 	Ref: React.RefObject<T>;
 	setFunction: React.Dispatch<React.SetStateAction<boolean>>;
+	setSecondFunction?: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function useOutside({ Ref, setFunction }: Props<HTMLElement>) {
+function useOutside({ Ref, setFunction, setSecondFunction }: Props<HTMLElement>) {
 	useEffect(() => {
 		function handleClickOutside(e: MouseEvent) {
 			if (Ref.current && !Ref.current.contains(e.target as Node)) {
 				setFunction(false);
+				if (setSecondFunction !== undefined) setSecondFunction("");
 			}
 		}
 		document.addEventListener("mousedown", handleClickOutside);
