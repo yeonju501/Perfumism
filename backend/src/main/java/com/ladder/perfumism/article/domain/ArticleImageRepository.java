5ea300ac36d0f699132c486e2eff0d7ec86fc7df
vtaco <woosj7129@naver.com>
2022-03-30 15:38:43 +0900

    [S06P22D105-248] feat: ArticleImageRepository soft delete Query

--- backend/src/main/java/com/ladder/perfumism/article/domain/ArticleImageRepository.java
+++ backend/src/main/java/com/ladder/perfumism/article/domain/ArticleImageRepository.java
@@ -3,8 +3,17 @@ package com.ladder.perfumism.article.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleImageRepository extends JpaRepository<ArticleImage, Long> {
 
     List<ArticleImage> findByArticle(Article article);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "update article_image ai "
+        + "set ai.deleted_at = current_timestamp "
+        + "where ai.article_id = (:article_id)")
+    Integer updateDeletedAtByArticle(@Param("article_id") Long article_id);
 }
