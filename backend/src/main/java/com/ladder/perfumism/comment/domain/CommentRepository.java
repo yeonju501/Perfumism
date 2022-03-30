12d21e68d3a8f661f9274f48945ba4747b43ce79
vtaco <woosj7129@naver.com>
2022-03-30 15:08:04 +0900

    [S06P22D105-248] feat: Comment Soft Delete 구현

--- backend/src/main/java/com/ladder/perfumism/comment/domain/CommentRepository.java
+++ backend/src/main/java/com/ladder/perfumism/comment/domain/CommentRepository.java
@@ -4,8 +4,17 @@ import com.ladder.perfumism.article.domain.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     Page<Comment> findAllByParentIdIsNullAndArticle(Article article, Pageable pageable);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "update comment c "
+        + "set c.deleted_at = current_timestamp "
+        + "where c.comment_id = (:comment_id)")
+    Integer updateDeletedAtByArticle(@Param("comment_id") Long commentId);
 }
