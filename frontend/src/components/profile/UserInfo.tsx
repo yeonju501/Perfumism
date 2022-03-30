3af5bd313ff2c578aa989fadd6f670edf170333f
9603100 <imgoing222@gmail.com>
2022-03-30 11:19:38 +0900

    [S06P22D105-261] fix: formData key값 image에서 img로 수정

--- frontend/src/components/profile/UserInfo.tsx
+++ frontend/src/components/profile/UserInfo.tsx
@@ -1,21 +1,25 @@
 import { profileApi } from "apis";
 import anonymous from "assets/anonymous.jpg";
 import useSetImage from "hooks/useSetImage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store";
+import { SET_USER } from "store/user";
 import styled from "styled-components";
 
 function UserInfo() {
+	const dispatch = useDispatch();
 	const { email, image, social_id, username } = useSelector((state: RootState) => state.user);
 
 	const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		const imageFile = useSetImage(e);
 		const formData = new FormData();
-		// if (imageFile) {
-		// 	formData.append("image", imageFile);
-		// 	await profileApi.setUserImage(formData).then(() => alert("프로필 이미지가 변경되었습니다"));
-		// 	dispatch
-		// }
+		if (imageFile) {
+			formData.append("img", imageFile);
+			await profileApi.setUserImage(formData).then(async () => {
+				const res = await profileApi.getUserInfo();
+				await dispatch(SET_USER(res.data));
+			});
+		}
 	};
 
 	return (
@@ -30,5 +34,7 @@ function UserInfo() {
 
 const UserImage = styled.img`
 	border-radius: 50%;
+	width: 7rem;
+	height: 7rem;
 `;
 export default UserInfo;
