876477941f54ea0524c30aa568f76663f8474c4a
9603100 <imgoing222@gmail.com>
2022-03-31 15:37:57 +0900

    [S06P22D105-61] feat: createReview prop 수정 및 updateReview 함수 추가

--- frontend/src/apis/review.ts
+++ frontend/src/apis/review.ts
@@ -3,10 +3,11 @@ import { request } from "./request";
 
 interface ReviewApiType {
 	createReview: (
-		review: { grade: number; content: string },
+		review: { grade: number; content: string | undefined },
 		perfumeId: string,
 	) => Promise<AxiosResponse>;
 	getReviews: (perfumeId: string, currentPage: number) => Promise<AxiosResponse>;
+	updateReviews: (reviewId: number) => Promise<AxiosResponse>;
 	deleteReview: (reviewId: number) => Promise<AxiosResponse>;
 	getLatestReviews: () => Promise<AxiosResponse<{ reviews: [] }>>;
 	isReviewLiked: (reviewId: number | string) => Promise<AxiosResponse>;
@@ -16,6 +17,7 @@ const reviewApi: ReviewApiType = {
 	createReview: (review, perfumeId) => request.post(`auth/reviews/perfumes/${perfumeId}`, review),
 	getReviews: (perfumeId, currentPage) =>
 		request.get(`reviews/perfumes/${perfumeId}?page=${currentPage}&size=5`),
+	updateReviews: (reviewId) => request.put(`auth/reviews/${reviewId}`),
 	deleteReview: (reviewId) => request.delete(`auth/reviews/${reviewId}`),
 	getLatestReviews: () => request.get("reviews/latest"),
 	isReviewLiked: (reviewId) => request.get(`auth/reviews/likes/${reviewId}`),
