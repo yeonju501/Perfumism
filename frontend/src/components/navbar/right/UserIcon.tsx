24f6aa4a081578e2122b3bb919bc43b300e68e19
9603100 <imgoing222@gmail.com>
2022-03-31 11:08:08 +0900

    [S06P22D105-61] feat: 로그아웃시 localstorage에서 persist 삭제

--- frontend/src/components/navbar/right/UserIcon.tsx
+++ frontend/src/components/navbar/right/UserIcon.tsx
@@ -6,6 +6,7 @@ import cookie from "react-cookies";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+import { persistor } from "index";
 
 interface Props {
 	usericon?: string;
@@ -18,6 +19,7 @@ function UserIcon() {
 
 	const logout = () => {
 		authApi.logout();
+		persistor.purge();
 		location.reload();
 	};
 
