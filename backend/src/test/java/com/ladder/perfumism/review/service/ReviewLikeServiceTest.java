de5a9cc7dad7776d4045f3d7c7d453070adde43a
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-31 14:37:29 +0900

    [S06P22D105-139] test: ReviewLikeServiceTest 추가

---
+++ backend/src/test/java/com/ladder/perfumism/review/service/ReviewLikeServiceTest.java
@@ -0,0 +1,15 @@
+package com.ladder.perfumism.review.service;
+
+import com.ladder.perfumism.review.domain.ReviewLikeRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewLikeServiceTest {
+
+    @InjectMocks
+    private ReviewLikeRepository reviewLikeRepository;
+
+
+}
