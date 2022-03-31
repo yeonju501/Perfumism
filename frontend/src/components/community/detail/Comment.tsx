7048e12f7a1ef5a2e4f4eed357a24d2554f23c14
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-01 00:37:25 +0900

    [S06P22D105-272] feat: 댓글 폼 초안 생성

--- frontend/src/components/community/detail/Comment.tsx
+++ frontend/src/components/community/detail/Comment.tsx
@@ -1,13 +1,22 @@
+import Textarea from "components/review/Textarea";
 import styled from "styled-components";
 
 function Comment() {
 	return (
 		<Container>
-			<p>댓글</p>
+			<Textarea>댓글 입력</Textarea>
+			<div>댓글 1</div>
+			<div>댓글 2</div>
 		</Container>
 	);
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	min-height: 20vh;
+	border-style: inset;
+`;
 
 export default Comment;
