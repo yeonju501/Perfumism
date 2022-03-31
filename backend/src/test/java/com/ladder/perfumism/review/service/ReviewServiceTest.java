996ad45292bf9866c9cb2cafe121e49b11b4eda3
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-31 11:17:09 +0900

    [S06P22D105-225] test: ReviewServiceTest 에 "리뷰 저장" 테스트 추가

--- backend/src/test/java/com/ladder/perfumism/review/service/ReviewServiceTest.java
+++ backend/src/test/java/com/ladder/perfumism/review/service/ReviewServiceTest.java
@@ -1,13 +1,17 @@
 package com.ladder.perfumism.review.service;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.*;
 
 import com.ladder.perfumism.auth.domain.Authority;
+import com.ladder.perfumism.global.exception.BusinessException;
 import com.ladder.perfumism.member.domain.Member;
+import com.ladder.perfumism.member.service.MemberService;
 import com.ladder.perfumism.perfume.domain.Brand;
 import com.ladder.perfumism.perfume.domain.Perfume;
 import com.ladder.perfumism.perfume.service.PerfumeService;
+import com.ladder.perfumism.review.controller.dto.request.ReviewWriteRequest;
 import com.ladder.perfumism.review.controller.dto.response.ReviewPageResponse;
 import com.ladder.perfumism.review.domain.Review;
 import com.ladder.perfumism.review.domain.ReviewRepository;
@@ -32,11 +36,20 @@ public class ReviewServiceTest {
     private static final int FIRST_PAGE = 0;
     private static final int DEFAULT_SIZE = 10;
 
+    // 의존성 주입이 필요한 목 객체
     @InjectMocks
     private ReviewService reviewService;
 
+    // 여기서는 Service 의 메서드를 테스트하는 공간이므로
+    // 테스트 하려는 Service 속에 있는 타 Service 나 Repository 는 Mock 으로 가상의 객체를 만들어서 테스트 한다.
+    // 여기서는 reviewService 가 주요 테스트 목표이므로, ReviewService 내에 있는 모든 Repository 활동들은 given() 으로 처리해야한다.
+    // 여기는 Service 에 관한 테스트 공간이지 Repository 에 대한 공간이 아니므로 Repository 에 대한 값을 정확히 받을 필요가 없기 때문.
+    // Repository 에 대한 테스트는 해당 레포지토리 테스트 클래스를 새로 파서 하길 바람.
     @Mock
     private PerfumeService perfumeService;
+
+    @Mock
+    private MemberService memberService;
 
     @Mock
     private ReviewRepository reviewRepository;
@@ -56,6 +69,25 @@ public class ReviewServiceTest {
     }
 
     @Test
+    @DisplayName("리뷰 저장")
+    public void writeReviewTest() {
+        // given
+        String email = "test@test.com";
+        // given() 은 ()속의 메서드를 테스트 진행 중에 만난다면 willReturn 의 ()속의 값을 반환해주는 메서드다.
+        // any()는 "어떤 값이 들어오든 간에" 라는 의미를 갖고 있다.
+        given(memberService.findByEmail(any())).willReturn(member);
+        given(perfumeService.findById(any())).willReturn(perfume);
+        ReviewWriteRequest request = new ReviewWriteRequest(0, "testContent");
+        given(reviewRepository.save(any())).willReturn(review);
+
+        // when
+        Review result = reviewService.writeReview(email, perfume.getId(), request);
+
+        // then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
     @DisplayName("리뷰 조회")
     public void getReviewPageTest() {
         // given
@@ -70,7 +102,7 @@ public class ReviewServiceTest {
         // when
         ReviewPageResponse result = reviewService.getReviewPage(1L, pageable);
 
-        //then
+        // then
         System.out.println(
             "Response: id = " + result.getReviewResponses().get(0).getReviewId() + ", content = " + result.getReviewResponses()
                 .get(0).getContent());
