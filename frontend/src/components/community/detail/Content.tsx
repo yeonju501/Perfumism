453e3d3b585bbbde185eeae6351a5d34d212dfb4
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-31 23:33:03 +0900

    [S06P22D105-272] feat: articleData props

--- frontend/src/components/community/detail/Content.tsx
+++ frontend/src/components/community/detail/Content.tsx
@@ -1,11 +1,34 @@
 import styled from "styled-components";
 
-function Content() {
+interface ArticleProps {
+	articleData: {
+		article_id: number;
+		member_id: number;
+		member_name: string;
+		member_image: string;
+		subject: string;
+		title: string;
+		content: string;
+		createAt: string;
+		updateAt: string;
+		deleteAt: string;
+		vote_exist: boolean;
+		image_url_list: {
+			article_image_id: number;
+			createdAt: string;
+			deletedAt: string;
+			updatedAt: string;
+			image_url: string;
+		}[];
+	};
+}
+
+function Content({ articleData }: ArticleProps) {
 	return (
 		<Container>
-			<p>제목</p>
-			<p>작성자</p>
-			<p>내용</p>
+			<Title>제목</Title>
+			<SubTitle>작성자</SubTitle>
+			<Body>내용</Body>
 		</Container>
 	);
 }
@@ -15,6 +38,25 @@ const Container = styled.div`
 	display: flex;
 	margin-top: 8rem;
 	flex-direction: column;
+	border-style: inset;
+`;
+
+const Title = styled.p`
+	border-bottom-style: groove;
+	font-size: 2rem;
+	margin: 1rem 0;
+`;
+
+const SubTitle = styled.p`
+	border-bottom-style: groove;
+	font-size: 1.5rem;
+	margin: 1rem 0;
+`;
+
+const Body = styled.p`
+	min-height: 20vh;
+	font-size: 1.5rem;
+	margin: 1rem 0;
 `;
 
 export default Content;
