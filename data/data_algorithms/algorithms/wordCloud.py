72098e09b6d82a72dac1bb4605391cba57e21e3c
yeonju501 <loling970124@gmail.com>
2022-03-31 12:45:15 +0900

    [S06P22D105-267] feat: django wordCloud.py 리턴값 설정

--- data/data_algorithms/algorithms/wordCloud.py
+++ data/data_algorithms/algorithms/wordCloud.py
@@ -1,6 +1,8 @@
 from wordcloud import WordCloud
 from collections import Counter
 import matplotlib.pyplot as plt
+from django.utils.timezone import now
+import uuid
 
 word_list = ["citrus", "woody", "powdery", "iris", "violet", "fresh", "musky", "amber", "earthy", "fruity", "leather", "powdery", "sweet", "violet", "animalic", "patchouli", "vanilla", "floral", "woody", "leather", "fruity", "woody", "aromatic", "warm", "spicy", "sweet", "powdery", "animalic", "fresh", "spicy", "violet"]
 
@@ -10,8 +12,12 @@ def word_cloud(word_list):
     wc = WordCloud(font_path="./data_algorithms/algorithms/tvN 즐거운이야기 Bold.ttf", width=400, height=400, background_color="white", max_font_size=250)
     cloud = wc.generate_from_frequencies(counts)
 
-    cloud.to_file('./perfumism/static/test.jpg')
-    # plt.figure()
-    # plt.axis("off")
-    # plt.imshow(cloud)
+    filename = '%s' %(
+       './static/' + now().strftime('%Y%m%d') + '_' + str(uuid.uuid4()) + '.jpg'
+    )
+    cloud.to_file(filename)
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(cloud)
     # plt.show()
+    return filename
