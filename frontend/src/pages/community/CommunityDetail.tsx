e54685182cf01b0134e7354dcfa9178a5b4298ed
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-31 15:48:21 +0900

    [S06P22D105-272] feat: 커뮤니티 상세 페이지 파일 생성

---
+++ frontend/src/pages/community/CommunityDetail.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function CommunityDetaul() {
+	return <Container></Container>;
+}
+
+const Container = styled.div`
+	height: 80vh;
+`;
+
+export default CommunityDetaul;
