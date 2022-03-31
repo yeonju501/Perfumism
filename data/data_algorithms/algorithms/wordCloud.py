24194aa2e2c25160f0fe1e74bb2ab8fdf3d074d1
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-31 14:08:53 +0900

    [S06P22D105-107] feat: accord 추출 추가

--- data/data_algorithms/algorithms/wordCloud.py
+++ data/data_algorithms/algorithms/wordCloud.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 from django.utils.timezone import now
 import uuid
 
-word_list = ["citrus", "woody", "powdery", "iris", "violet", "fresh", "musky", "amber", "earthy", "fruity", "leather", "powdery", "sweet", "violet", "animalic", "patchouli", "vanilla", "floral", "woody", "leather", "fruity", "woody", "aromatic", "warm", "spicy", "sweet", "powdery", "animalic", "fresh", "spicy", "violet"]
-
 def word_cloud(word_list):
     counts = Counter(word_list)
+    main_accords = [counts.most_common(3)[0][0], counts.most_common(3)[1][0], counts.most_common(3)[2][0]]
 
-    wc = WordCloud(font_path="./data_algorithms/algorithms/tvN 즐거운이야기 Bold.ttf", width=400, height=400, background_color="white", max_font_size=250)
+    wc = WordCloud(font_path="./tvN 즐거운이야기 Bold.ttf", width=300, height=300, background_color="white", max_font_size=250)
     cloud = wc.generate_from_frequencies(counts)
 
     filename = '%s' %(
@@ -20,4 +19,12 @@ def word_cloud(word_list):
     plt.axis("off")
     plt.imshow(cloud)
     # plt.show()
-    return filename
+
+    result = [filename, main_accords]
+    return result
+
+if __name__ == '__main__':
+    # arr = "wine vanilla sweet woody aromatic leather fruity warm spicy powdery animalic fresh violet"
+    word_list = ["citrus", "woody", "powdery", "iris", "violet", "fresh", "musky", "amber", "earthy", "fruity", "leather", "powdery", "sweet", "violet", "animalic", "patchouli", "vanilla", "floral", "woody", "leather", "fruity", "woody", "aromatic", "warm", "spicy", "sweet", "powdery", "animalic", "fresh", "spicy", "violet"]
+    result = word_cloud(word_list)
+    print(result)
