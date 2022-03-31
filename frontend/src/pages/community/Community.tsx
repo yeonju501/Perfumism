c044d4436ed620e5cbdc7a72259c719942b34051
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-03-31 09:39:49 +0900

    [S06P22D105-218] feat: article props 변경

--- frontend/src/pages/community/Community.tsx
+++ frontend/src/pages/community/Community.tsx
@@ -2,10 +2,64 @@ import { Header, CommunityList, Pagination } from "components/community";
 import styled from "styled-components";
 
 function Community() {
+	const articleData = [
+		{
+			articleList: [
+				{
+					article_id: 3,
+					member_id: 1,
+					member_name: "우사앙주운",
+					subject: "TALK",
+					title: "제목입니다",
+					content: "내용입니다",
+					createAt: "2022-3-13 14:59:51",
+					updateAt: "2023-4-14 14:59:51",
+					deleteAt: "2023-4-15 14:59:51",
+				},
+			],
+			total_page_count: 2,
+			current_page_count: 1,
+		},
+		{
+			articleList: [
+				{
+					article_id: 2,
+					member_id: 1,
+					member_name: "우사앙주운",
+					subject: "TALK",
+					title: "제목입니다",
+					content: "내용입니다",
+					createAt: "2022-3-13 14:59:51",
+					updateAt: "2023-4-14 14:59:51",
+					deleteAt: "2023-4-15 14:59:51",
+				},
+			],
+			total_page_count: 4,
+			current_page_count: 1,
+		},
+		{
+			articleList: [
+				{
+					article_id: 1,
+					member_id: 1,
+					member_name: "우사앙주운",
+					subject: "TALK",
+					title: "제목입니다",
+					content: "내용입니다",
+					createAt: "2022-3-13 14:59:51",
+					updateAt: "2023-4-14 14:59:51",
+					deleteAt: "2023-4-15 14:59:51",
+				},
+			],
+			total_page_count: 1,
+			current_page_count: 1,
+		},
+	];
+
 	return (
 		<Container>
 			<Header />
-			<CommunityList />
+			<CommunityList articleData={articleData} />
 			<Pagination />
 		</Container>
 	);
