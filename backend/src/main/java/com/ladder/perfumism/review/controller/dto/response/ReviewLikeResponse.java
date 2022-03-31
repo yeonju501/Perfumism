714f08721194fcc518e2f81dccbdedf946eaccdd
IcedCaffeLatte <tmdrl0160@naver.com>
2022-03-31 15:27:29 +0900

    [S06P22D105-139] test: ReviewLikeResponse 에 테스트를 위해 Getter 추가

--- backend/src/main/java/com/ladder/perfumism/review/controller/dto/response/ReviewLikeResponse.java
+++ backend/src/main/java/com/ladder/perfumism/review/controller/dto/response/ReviewLikeResponse.java
@@ -2,7 +2,9 @@ package com.ladder.perfumism.review.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
 
+@Getter
 public class ReviewLikeResponse {
 
     @JsonProperty("is_liked")
