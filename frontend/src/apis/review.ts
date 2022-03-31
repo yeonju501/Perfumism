7b7a438bdcfafa430fa47fe1b72084e6e2719782
9603100 <imgoing222@gmail.com>
2022-03-31 16:26:42 +0900

    [S06P22D105-61] feat: updateReview 함수 수정

--- frontend/src/apis/review.ts
+++ frontend/src/apis/review.ts
@@ -7,7 +7,10 @@ interface ReviewApiType {
 		perfumeId: string,
 	) => Promise<AxiosResponse>;
 	getReviews: (perfumeId: string, currentPage: number) => Promise<AxiosResponse>;
-	updateReviews: (reviewId: number) => Promise<AxiosResponse>;
+	updateReview: (
+		review: { grade: number; content: string | undefined },
+		reviewId: number | undefined,
+	) => Promise<AxiosResponse>;
 	deleteReview: (reviewId: number) => Promise<AxiosResponse>;
 	getLatestReviews: () => Promise<AxiosResponse<{ reviews: [] }>>;
 	isReviewLiked: (reviewId: number | string) => Promise<AxiosResponse>;
@@ -17,7 +20,7 @@ const reviewApi: ReviewApiType = {
 	createReview: (review, perfumeId) => request.post(`auth/reviews/perfumes/${perfumeId}`, review),
 	getReviews: (perfumeId, currentPage) =>
 		request.get(`reviews/perfumes/${perfumeId}?page=${currentPage}&size=5`),
-	updateReviews: (reviewId) => request.put(`auth/reviews/${reviewId}`),
+	updateReview: (review, reviewId) => request.put(`auth/reviews/${reviewId}`, review),
 	deleteReview: (reviewId) => request.delete(`auth/reviews/${reviewId}`),
 	getLatestReviews: () => request.get("reviews/latest"),
 	isReviewLiked: (reviewId) => request.get(`auth/reviews/likes/${reviewId}`),
