6a85366696c4e305788169a394ee6ba11c431f4d
9603100 <imgoing222@gmail.com>
2022-03-31 18:17:49 +0900

    [S06P22D105-276] feat: 리뷰 좋아요 등록 및 취소 구현

--- frontend/src/components/review/LikeButton.tsx
+++ frontend/src/components/review/LikeButton.tsx
@@ -2,14 +2,24 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as heart } from "@fortawesome/free-solid-svg-icons";
 import useLikeButton from "../button/hooks/useLikeButton";
+import { reviewApi } from "apis";
 
 interface LikeButtonProps {
 	reviewId: number;
 }
 
 function LikeButton({ reviewId }: LikeButtonProps) {
-	const type = "perfume";
-	const { isLiked, handleHeartClick } = useLikeButton({ type, typeId: reviewId });
+	const { isLiked, handleHeartClick } = useLikeButton({
+		getIsLiked: () => {
+			return reviewApi.isReviewLiked(reviewId);
+		},
+		addLike: () => {
+			return reviewApi.addReviewLike(reviewId);
+		},
+		cancelLike: () => {
+			return reviewApi.cancelReviewLike(reviewId);
+		},
+	});
 
 	return (
 		<Button isLiked={isLiked} onClick={handleHeartClick}>
