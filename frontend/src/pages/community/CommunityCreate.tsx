6dbba589280301fd8293a21b78dce1e5330f022e
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-02 00:34:14 +0900

    [S06P22D105-219] feat: 글작성 hooks 적용

--- frontend/src/pages/community/CommunityCreate.tsx
+++ frontend/src/pages/community/CommunityCreate.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { Container, FormContainer } from "components/community/create/Container";
 import { Dropdown, Label, Input, Textarea, Button } from "components/community";
+import { formValidator } from "utils";
+import useForm from "../account/hooks/useForm";
+import communityApi from "apis/community";
 
 function CommunityCreate() {
 	const navigate = useNavigate();
@@ -8,6 +11,30 @@ function CommunityCreate() {
 	const handleListButtonClick = () => {
 		navigate("/community");
 	};
+
+	const { handleChange, handleSubmit, errors } = useForm({
+		initialValues: {
+			subject: "",
+			title: "",
+			content: "",
+		},
+
+		onSubmit: async () => {
+			try {
+				await communityApi.communityCreate();
+			} catch (error) {
+				console.log(error);
+			}
+		},
+
+		validate: ({ subject, title, content }) => {
+			const errors: { [key: string]: string } = {};
+			if (!formValidator.validateEmailForm(title)) errors.email = "올바른 이메일을 입력해주세요.";
+			if (!formValidator.validatePassword(content))
+				errors.password = "대문자와 특수문자를 1자 이상 포함해주세요.";
+			return errors;
+		},
+	});
 
 	return (
 		<Container>
