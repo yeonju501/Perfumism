066b5497dc29f13a4413d9ed6b84539227db6a6b
9603100 <imgoing222@gmail.com>
2022-04-01 09:02:13 +0900

    [S06P22D105-279] feat: Pagination 컴포넌트와 PaginationButton 컴포넌트 생성

---
+++ frontend/src/components/Pagination.tsx
@@ -0,0 +1,48 @@
+import PaginationButton from "./button/PaginationButton";
+
+interface PaginationProps {
+	total: number;
+	page: number;
+	setData: React.Dispatch<React.SetStateAction<dataType>>;
+}
+
+interface articleList {
+	article_id: number;
+	subject: string;
+	title: string;
+	created_at: string;
+}
+
+interface dataType {
+	articleList: articleList[];
+	total_page_count: number;
+	current_page_count: number;
+}
+
+function Pagination({ total, page, setData }: PaginationProps) {
+	return (
+		<>
+			<nav>
+				<button
+					onClick={() => setData((prev) => ({ ...prev, currentPage: page - 1 }))}
+					disabled={page === 0}
+				>
+					&lt;
+				</button>
+				{Array(total)
+					.fill("")
+					.map((_, i) => (
+						<PaginationButton key={i + 1} setData={setData} idx={i} text={String(i + 1)} />
+					))}
+				<button
+					onClick={() => setData((prev) => ({ ...prev, currentPage: page + 1 }))}
+					disabled={page === total - 1}
+				>
+					&gt;
+				</button>
+			</nav>
+		</>
+	);
+}
+
+export default Pagination;
