d0e4dc90399a449a0ef46c9be043bb0d4b9aa613
우동진 <wjinh93@gmail.com>
2022-04-01 15:06:54 +0900

    [S06P22D105-277] feat: useEffect 및 useState 구현

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,3 +1,4 @@
+import { profileApi } from "apis";
 import Button from "components/profile/Button";
 import { Container, FormContainer } from "components/profile/Container";
 import { Input, Radio } from "components/profile/Input";
@@ -5,23 +6,44 @@ import Label from "components/profile/Label";
 import Sidebar from "components/profile/Sidebar";
 import UserInfo from "components/profile/UserInfo";
 import UserName from "components/profile/UserName";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
 import styled from "styled-components";
 
-function Profile() {
-	const { gender, image, username } = useSelector((state: RootState) => state.user);
-	return (
-		<Container>
-			<Sidebar />
-			<Section>
-				<UserInfo />
-				{username && <UserName value={username} gender={gender}></UserName>}
+interface Data {
+	id: number;
+	email: string;
+	username: string;
+	gender: null | number;
+	image: null | number;
+	social_id: null | number;
+}
 
-				{/* 추가정보 */}
-				<Button>회원 탈퇴</Button>
-			</Section>
-		</Container>
+function Profile() {
+	const [userInfo, setUserInfo] = useState<Data | null>(null);
+
+	useEffect(() => {
+		const getUserInfo = async () => {
+			const res = await profileApi.getUserInfo();
+			setUserInfo(res.data);
+		};
+		getUserInfo();
+	}, []);
+
+	return (
+		userInfo && (
+			<Container>
+				<Sidebar />
+				<Section>
+					<UserInfo />
+					<UserName value={userInfo.username} gender={userInfo.gender as number}></UserName>
+
+					{/* 추가정보 */}
+					<Button>회원 탈퇴</Button>
+				</Section>
+			</Container>
+		)
 	);
 }
 
