ed50368860ca7f5b7299ea2687d3a9a8a0f112e0
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-01 22:59:35 +0900

    [S06P22D105-218] feat: 더미 데이터 삭제

--- frontend/src/pages/community/Community.tsx
+++ frontend/src/pages/community/Community.tsx
@@ -24,39 +24,10 @@ function Community() {
 		}
 	};
 
-	const articleDatas = {
-		articleList: [
-			{
-				article_id: 2,
-				member_id: 12,
-				member_name: "asdf",
-				subject: "TALK",
-				title: "ㅎㅇ",
-				content: "ㅂㅇ",
-				created_at: "2022-03-31 14:25:42",
-				updated_at: null,
-				deleted_at: null,
-			},
-			{
-				article_id: 1,
-				member_id: 1,
-				member_name: "tata",
-				subject: "TALK",
-				title: "제목입니다",
-				content: "내용입니다",
-				created_at: "2022-03-31 12:23:09",
-				updated_at: null,
-				deleted_at: null,
-			},
-		],
-		total_page_count: 1,
-		current_page_count: 0,
-	};
-
 	return (
 		<Container>
 			<MainHeader />
-			<CommunityList articleList={articleDatas.articleList} />
+			<CommunityList articleList={articleList} />
 			<Pagination />
 		</Container>
 	);
