39a14bb7996e6a8ca30e4dcf028d415049a31ccd
9603100 <imgoing222@gmail.com>
2022-04-02 02:09:45 +0900

    [S06P22D105-288] feat: 조건부 렌더링 및 state 수정 위해 props 추가

--- frontend/src/pages/profile/Favorites.tsx
+++ frontend/src/pages/profile/Favorites.tsx
@@ -3,7 +3,7 @@ import PerfumeList from "components/perfume/PerfumeList";
 import useInfiniteScroll from "pages/perfume/hooks/useInfiniteScroll";
 
 function Favorites() {
-	const { setTarget, perfumes, isLoading } = useInfiniteScroll({
+	const { setTarget, perfumes, isLoading, setPerfumes } = useInfiniteScroll({
 		requestApi: (currentPage) => {
 			return profileApi.getFavorites(currentPage);
 		},
@@ -12,7 +12,7 @@ function Favorites() {
 	return (
 		<div>
 			<h1>내가 좋아요한 향수 목록</h1>
-			<PerfumeList perfumes={perfumes} />
+			<PerfumeList perfumes={perfumes} favorites setPerfumes={setPerfumes} />
 			<div ref={setTarget}>{isLoading && <p>Loading..</p>}</div>
 		</div>
 	);
