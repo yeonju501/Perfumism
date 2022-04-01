2847401f6a99fb8af997e86d5cd3a2db497ae6c7
우동진 <wjinh93@gmail.com>
2022-04-01 16:15:05 +0900

    [S06P22D105-290] feat: 사용안하는 코드 삭제

--- frontend/src/pages/account/socialogin/GoogleLoginPage.tsx
+++ frontend/src/pages/account/socialogin/GoogleLoginPage.tsx
@@ -1,10 +1,8 @@
 import socialLogin from "apis/socialLogin";
 import Spinner from "components/Spinner";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 function GoogleLoginPage() {
-	const navigate = useNavigate();
 	useEffect(() => {
 		const params = new URL(document.location.toString()).searchParams;
 		const code = params.get("code");
