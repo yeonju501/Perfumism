8c136348d01fe18f149f21f53d45281e2d00fa6d
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-01 13:44:01 +0900

    [S06P22D105-280] test: 향수 검색 서비스 Test에 "향수 검색 - 브랜드" Test 추가

--- backend/src/test/java/com/ladder/perfumism/perfume/service/PerfumeSearchServiceTest.java
+++ backend/src/test/java/com/ladder/perfumism/perfume/service/PerfumeSearchServiceTest.java
@@ -3,7 +3,6 @@ package com.ladder.perfumism.perfume.service;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 import com.ladder.perfumism.perfume.controller.dto.response.PerfumeListResponse;
 import com.ladder.perfumism.perfume.domain.Accord;
@@ -26,7 +25,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 @ExtendWith(MockitoExtension.class)
 public class PerfumeSearchServiceTest {
@@ -53,14 +51,13 @@ public class PerfumeSearchServiceTest {
 
     @BeforeEach
     void setup() {
-        brand1 = new Brand(1L, "Brand BBB");
-        brand2 = new Brand(2L, "Brand CCC");
-        brand3 = new Brand(3L, "Brand AAA");
+        brand1 = new Brand(1L, "Brand AAA");
+        brand2 = new Brand(3L, "Brand BBB");
         perfume1 = new Perfume(4L, "Perfume AAA", brand1, "testImage", 2000, 0.0, "testTopNote",
             "testMiddleNotes", "testBaseNotes", 0L, "testLongevity", "testSillage", 0);
-        perfume2 = new Perfume(5L, "Perfume BBB", brand2, "testImage", 2000, 0.0, "testTopNote",
+        perfume2 = new Perfume(5L, "Perfume BBB", brand1, "testImage", 2000, 0.0, "testTopNote",
             "testMiddleNotes", "testBaseNotes", 0L, "testLongevity", "testSillage", 0);
-        perfume3 = new Perfume(6L, "Perfume CCC", brand3, "testImage", 2000, 0.0, "testTopNote",
+        perfume3 = new Perfume(6L, "Perfume CCC", brand2, "testImage", 2000, 0.0, "testTopNote",
             "testMiddleNotes", "testBaseNotes", 0L, "testLongevity", "testSillage", 0);
         accord1 = new Accord(7L, "다다다", "CCC");
         accord2 = new Accord(8L, "가가가", "AAA");
@@ -91,5 +88,31 @@ public class PerfumeSearchServiceTest {
         assertThat(result.getPerfumeSimpleResponses().get(0).getName()).isEqualTo("Perfume AAA");
         assertThat(result.getPerfumeSimpleResponses().get(1).getName()).isEqualTo("Perfume BBB");
         assertThat(result.getPerfumeSimpleResponses().get(2).getName()).isEqualTo("Perfume CCC");
+    }
+
+    @Test
+    @DisplayName("향수 검색 - 브랜드")
+    void perfumeSearchByBrand() {
+        // given
+        String type = "brand";
+        String keyword = "Brand AAA";
+
+        List<Brand> brands = new ArrayList<>();
+        brands.add(brand1);
+        given(brandRepository.findByNameStartsWithIgnoreCase(keyword)).willReturn(brands);
+
+        Pageable pageable = PageRequest.of(FIRST_PAGE, DEFAULT_SIZE);
+        List<Perfume> perfumes = new ArrayList<>();
+        perfumes.add(perfume1);
+        perfumes.add(perfume2);
+        Page<Perfume> perfumePage = new PageImpl<>(perfumes);
+        given(perfumeRepository.findByBrandId(brands, pageable)).willReturn(perfumePage);
+
+        // when
+        PerfumeListResponse result = perfumeSearchService.getPerfumeSearch(pageable, type, keyword);
+
+        // then
+        assertThat(result.getPerfumeSimpleResponses().get(0).getBrand().getName()).isEqualTo(keyword);
+        assertThat(result.getPerfumeSimpleResponses().get(1).getBrand().getName()).isEqualTo(keyword);
     }
 }
