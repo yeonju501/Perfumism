df9a9b4f225ae048fd9e444e1e6c8eb2baecefcb
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-01 14:03:27 +0900

    [S06P22D105-219] feat: CommunityCreate 페이지 파일 생성

---
+++ frontend/src/pages/community/CommunityCreate.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function CommunityCreate() {
+	return <Container></Container>;
+}
+
+const Container = styled.div`
+	min-height: 80vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export default CommunityCreate;
