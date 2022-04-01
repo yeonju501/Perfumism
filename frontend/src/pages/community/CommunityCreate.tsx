70057a6dd74a3155037321c99b8da371c088bf53
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-01 14:47:49 +0900

    [S06P22D105-219] feat: 글작성 기본 구조 생성

--- frontend/src/pages/community/CommunityCreate.tsx
+++ frontend/src/pages/community/CommunityCreate.tsx
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 
 function CommunityCreate() {
-	return <Container></Container>;
+	return (
+		<Container>
+			<form action="">
+				<select name="">
+					<option value="">말머리선택</option>
+					<option value="추천">추천</option>
+					<option value="골라">골라</option>
+					<option value="잡담">잡담</option>
+				</select>
+				<label htmlFor="title">제목</label>
+				<input name="title" type="text" />
+				<label htmlFor="content">내용</label>
+				<textarea name="title" />
+			</form>
+			<div>
+				<button>목록</button>
+				<button>등록</button>
+			</div>
+		</Container>
+	);
 }
 
 const Container = styled.div`
