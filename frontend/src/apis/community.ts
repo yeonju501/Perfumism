511ff0b2913b46b34e4c917fe62a8a19cbece932
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-01 23:33:31 +0900

    [S06P22D105-272] feat: 게시글 상세조회 api 함수 생성

--- frontend/src/apis/community.ts
+++ frontend/src/apis/community.ts
@@ -3,10 +3,12 @@ import { request, imageRequest } from "./request";
 
 interface CommunityApiType {
 	getCommunityList: (currentPage: number) => Promise<AxiosResponse>;
+	getCommunityDetail: (articleId: number) => Promise<AxiosResponse>;
 }
 
 const communityApi: CommunityApiType = {
 	getCommunityList: (currentPage: number) => request.get(`auth/articles/?page=${currentPage}`),
+	getCommunityDetail: (articleId: number) => request.get(`auth/articles/detail/${articleId}`),
 };
 
 export default communityApi;
