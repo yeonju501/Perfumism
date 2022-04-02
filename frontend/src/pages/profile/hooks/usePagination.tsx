cf517cd09512a48c79b23a3ec22e1b01974e85a5
9603100 <imgoing222@gmail.com>
2022-04-02 22:57:28 +0900

    [S06P22D105-296] feat: usePagination hook 생성 후 함수 구현

---
+++ frontend/src/pages/profile/hooks/usePagination.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+interface DataType {
+	articleList?: ArticleListType[];
+	commentList?: CommentListType[];
+	total_page_count: number;
+	current_page_count: number;
+}
+
+interface ArticleListType {
+	article_id: number;
+	member_id: number;
+	member_name: string;
+	subject: string;
+	title: string;
+	content: string;
+	created_at: string;
+	updated_at: string;
+	deleted_at: string;
+}
+
+interface CommentListType {
+	comment_id: number;
+	content: string;
+	created_at: string;
+}
+
+interface usePaginationProps {
+	requestApi: (currentPage: number) => Promise<any>;
+}
+function usePagination({ requestApi }: usePaginationProps) {
+	const [data, setData] = useState<DataType>({
+		articleList: [],
+		commentList: [],
+		total_page_count: 0,
+		current_page_count: 0,
+	});
+
+	const { articleList, commentList } = data;
+	const totalPage = data.total_page_count;
+	const currentPage = data.current_page_count;
+
+	useEffect(() => {
+		getData();
+	}, [currentPage]);
+
+	const getData = async () => {
+		const res = await requestApi(currentPage);
+		setData(res.data);
+	};
+
+	return { articleList, commentList, currentPage, totalPage, setData };
+}
+
+export default usePagination;
