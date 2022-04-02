d5ff877d7bee07f120261c59d939737084d3afa9
9603100 <imgoing222@gmail.com>
2022-04-02 22:58:06 +0900

    [S06P22D105-296] refactor: 내가 쓴 글 페이지 hook 사용해 리팩토링

--- frontend/src/pages/profile/MyArticles.tsx
+++ frontend/src/pages/profile/MyArticles.tsx
@@ -3,76 +3,50 @@ import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-interface articleList {
-	article_id: number;
-	member_id: number;
-	member_name: string;
-	subject: string;
-	title: string;
-	content: string;
-	created_at: string;
-	updated_at: string;
-	deleted_at: string;
-}
-
-interface dataType {
-	articleList: articleList[];
-	total_page_count: number;
-	current_page_count: number;
-}
+import usePagination from "./hooks/usePagination";
 
 function MyArticles() {
-	const [data, setData] = useState<dataType>({
-		articleList: [],
-		total_page_count: 0,
-		current_page_count: 0,
+	const { articleList, currentPage, totalPage, setData } = usePagination({
+		requestApi: (currentPage: number) => {
+			return profileApi.getMyArticles(currentPage);
+		},
 	});
-	const { articleList } = data;
-	const totalPage = data.total_page_count;
-	const currentPage = data.current_page_count;
-
-	useEffect(() => {
-		getData();
-	}, [currentPage]);
-
-	const getData = async () => {
-		const res = await profileApi.getMyArticles(currentPage);
-		setData(res.data);
-	};
-
 	return (
 		<>
-			<Table>
-				<thead>
-					<tr>
-						<th>말머리</th>
-						<th>제목</th>
-						<th>작성 날짜</th>
-					</tr>
-				</thead>
-				<tbody>
+			{articleList && (
+				<>
 					{articleList.length > 0 ? (
-						articleList.map((article, idx) => (
-							<Tr key={idx}>
-								<Td>{article.subject}</Td>
-								<Td>
-									<Link to="">{article.title}</Link>
-								</Td>
-								<Td>{article.created_at.slice(0, 10)}</Td>
-							</Tr>
-						))
+						<>
+							<Table>
+								<thead>
+									<tr>
+										<th>말머리</th>
+										<th>제목</th>
+										<th>작성 날짜</th>
+									</tr>
+								</thead>
+								<tbody>
+									{articleList.map((article, idx) => (
+										<Tr key={idx}>
+											<Td>{article.subject}</Td>
+											<Td>
+												<Link to="">{article.title}</Link>
+											</Td>
+											<Td>{article.created_at.slice(0, 10)}</Td>
+										</Tr>
+									))}
+								</tbody>
+							</Table>
+							{articleList.length > 0 && (
+								<Footer>
+									<Pagination page={currentPage} total={totalPage} setData={setData} />
+								</Footer>
+							)}
+						</>
 					) : (
-						<tr>
-							<td>아직 작성한 글이 없습니다</td>
-						</tr>
+						<p>작성한 글이 없습니다</p>
 					)}
-				</tbody>
-			</Table>
-			{articleList.length > 0 && (
-				<Footer>
-					<Pagination page={currentPage} total={totalPage} setData={setData} />
-				</Footer>
+				</>
 			)}
 		</>
 	);
@@ -94,4 +68,5 @@ const Td = styled.td``;
 const Footer = styled.footer`
 	text-align: center;
 `;
+
 export default MyArticles;
