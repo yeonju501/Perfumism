d63c5a947a973e3405d4ebf92ca95847f7c377fd
yeonju501 <loling970124@gmail.com>
2022-04-02 14:26:05 +0900

    [S06P22D105-294] feat: 장고 s3 설정 변경

--- data/data_algorithms/views.py
+++ data/data_algorithms/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from data_algorithms.models import Accord, Member, Perfume
 from data_algorithms.serializers.accord import AccordListSerializer, AccordSerializer
 from data_algorithms.serializers.perfume import PerfumeListSerializer
-from perfumism.settings import AWS_S3_ACCESS_KEY_ID, AWS_S3_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME,AWS_IMG_DIRECTORY
+from perfumism.settings import AWS_S3_ACCESS_KEY_ID, AWS_S3_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_IMG_DIRECTORY
 from .algorithms.dbscan import recommend_like_based, recommend_survey
 from .algorithms.wordCloud import word_cloud 
 from rest_framework.renderers import JSONRenderer
@@ -33,7 +33,7 @@ def like_based(request, member_pk):
 
     s3r = boto3.resource('s3', aws_access_key_id = AWS_S3_ACCESS_KEY_ID, aws_secret_access_key = AWS_S3_SECRET_ACCESS_KEY)
     data = open(AWS_IMG_DIRECTORY+filename[8:], 'rb')
-    s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key = filename[8:], Body=data, ContentType='jpg')
+    s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key = 'static/' + filename[8:], Body=data, ContentType='jpg')
 
 
 
@@ -59,9 +59,10 @@ def survey(request, a1, a2, a3, a4, a5):
     perfume_list = result[0]
     filename = result[1]
     accords = result[2]
+
     s3r = boto3.resource('s3', aws_access_key_id = AWS_S3_ACCESS_KEY_ID, aws_secret_access_key = AWS_S3_SECRET_ACCESS_KEY)
-    data = open(filename, 'rb')
-    s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key = 'second', body=data, ContentType='jpg')
+    data = open(AWS_IMG_DIRECTORY+filename[8:], 'rb')
+    s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key = 'static/' + filename[8:], Body=data, ContentType='jpg')
 
 
     perfumes = []
