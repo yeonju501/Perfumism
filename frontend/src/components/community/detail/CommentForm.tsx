edd16421032d186aa19e936e7b54281dfafb597e
9603100 <imgoing222@gmail.com>
2022-04-04 16:25:09 +0900

    [S06P22D105-310] feat: useReviewForm 사용해 댓글 작성 기능 구현

--- frontend/src/components/community/detail/CommentForm.tsx
+++ frontend/src/components/community/detail/CommentForm.tsx
@@ -1,18 +1,32 @@
+import communityApi from "apis/community";
 import { CreateButton } from "components/button/Button";
+import useReviewForm from "components/review/hooks/useReviewForm";
 import Textarea from "components/review/Textarea";
 import styled from "styled-components";
 
-function CommentForm() {
+interface CommentFormProps {
+	articleId: number;
+}
+
+function CommentForm({ articleId }: CommentFormProps) {
+	const { handleInputChange, handleFormSubmit, content } = useReviewForm({
+		sendReviewData: () => {
+			return communityApi.createComment(articleId);
+		},
+	});
+
 	return (
-		<FormContainer>
-			<Textarea placeholder="댓글을 입력하세요"></Textarea>
+		<FormContainer onSubmit={handleFormSubmit}>
+			<Textarea
+				placeholder="댓글을 입력하세요"
+				value={content}
+				onChange={handleInputChange}
+			></Textarea>
 			<CreateButton>작성</CreateButton>
 		</FormContainer>
 	);
 }
 
-const FormContainer = styled.div`
-	display: flex;
-`;
+const FormContainer = styled.form``;
 
 export default CommentForm;
