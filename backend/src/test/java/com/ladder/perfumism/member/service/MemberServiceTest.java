2e53f2c92312f3cd3cfbbc31e43b647aebe1e532
yeonju501 <loling970124@gmail.com>
2022-04-04 12:35:43 +0900

    [S06P22D105-291] feat: MemberServiceTest 세팅

--- backend/src/test/java/com/ladder/perfumism/member/service/MemberServiceTest.java
+++ backend/src/test/java/com/ladder/perfumism/member/service/MemberServiceTest.java
@@ -1,5 +1,45 @@
 package com.ladder.perfumism.member.service;
 
+import static com.ladder.perfumism.member.util.MemberFixture.EMAIL;
+import static com.ladder.perfumism.member.util.MemberFixture.PASSWORD;
+import static com.ladder.perfumism.member.util.MemberFixture.USERNAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.ladder.perfumism.global.exception.BusinessException;
+import com.ladder.perfumism.global.exception.ErrorCode;
+import com.ladder.perfumism.member.controller.dto.request.MemberSaveRequest;
+import com.ladder.perfumism.member.domain.Member;
+import com.ladder.perfumism.member.domain.MemberRepository;
+import com.ladder.perfumism.member.util.MemberFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
 
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    private MemberSaveRequest memberSaveRequest;
+
+    @BeforeEach
+    void setup() {
+        memberSaveRequest = MemberFixture.createMemberSaveRequest(EMAIL, PASSWORD, USERNAME);
+    }
 }
