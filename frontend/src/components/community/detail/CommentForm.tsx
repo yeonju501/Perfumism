1b4d6f7ba29a6e1d7041d15bba71788750fa2dd2
9603100 <imgoing222@gmail.com>
2022-04-04 16:10:17 +0900

    [S06P22D105-310] feat: CommentForm 컴포넌트 구현

--- frontend/src/components/community/detail/CommentForm.tsx
+++ frontend/src/components/community/detail/CommentForm.tsx
@@ -1,5 +1,18 @@
+import { CreateButton } from "components/button/Button";
+import Textarea from "components/review/Textarea";
+import styled from "styled-components";
+
 function CommentForm() {
-	return;
+	return (
+		<FormContainer>
+			<Textarea placeholder="댓글을 입력하세요"></Textarea>
+			<CreateButton>작성</CreateButton>
+		</FormContainer>
+	);
 }
+
+const FormContainer = styled.div`
+	display: flex;
+`;
 
 export default CommentForm;
