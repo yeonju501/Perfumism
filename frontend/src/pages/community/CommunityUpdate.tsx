c97e7cae48fe558d5e77c476af97526276d1a13f
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-04 09:46:13 +0900

    [S06P22D105-221] feat: CommunityUpdate.tsx 생성

---
+++ frontend/src/pages/community/CommunityUpdate.tsx
@@ -0,0 +1,11 @@
+import { Container, FormContainer } from "components/community/create/Container";
+
+function CommnuityUpdate() {
+	return (
+		<Container>
+			<FormContainer></FormContainer>
+		</Container>
+	);
+}
+
+export default CommnuityUpdate;
