c969e137d17c0d712a191321596f95faa66574cf
9603100 <imgoing222@gmail.com>
2022-04-04 13:20:57 +0900

    [S06P22D105-325] refactor: 불필요한 type 정리

--- frontend/src/components/perfume/PerfumeDetailRecommendation.tsx
+++ frontend/src/components/perfume/PerfumeDetailRecommendation.tsx
@@ -4,34 +4,13 @@ import styled from "styled-components";
 import PerfumeList from "./PerfumeList";
 
 interface PerfumeDataProps {
-	perfumeData: PerfumeDataType;
-}
-
-interface PerfumeDataType {
-	perfume_id: number;
-	perfume_name: string;
-	brand: {
-		brand_id: number;
-		brand_name: string;
+	perfumeData: {
+		perfume_name: string;
+		brand: {
+			brand_name: string;
+		};
+		similar_perfume: [];
 	};
-	image: string;
-	launch_year: number;
-	average_grade: number;
-	top_notes: string;
-	middle_notes: string | null;
-	base_notes: string | null;
-	total_survey: number;
-	longevity: string;
-	sillage: string;
-	accords: AccordType[];
-	similar_perfume: [];
-	likes: number;
-}
-
-interface AccordType {
-	accord_id: number;
-	kor_name: string;
-	eng_name: string;
 }
 
 function PerfumeDetailRecommendation({ perfumeData }: PerfumeDataProps) {
