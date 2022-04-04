2be70d6dcac8e76a6a7bbf4d9b81a450f0d8a4e4
9603100 <imgoing222@gmail.com>
2022-04-04 16:43:13 +0900

    [S06P22D105-310] refactor: Comment 임시 할당 값 정리

--- frontend/src/components/community/detail/Comment.tsx
+++ frontend/src/components/community/detail/Comment.tsx
@@ -8,9 +8,7 @@ interface CommentProps {
 function Comment({ articleId }: CommentProps) {
 	return (
 		<Container>
-			<CommentForm articleId={1} />
-			<div>댓글 1</div>
-			<div>댓글 2</div>
+			<CommentForm articleId={articleId} />
 		</Container>
 	);
 }
