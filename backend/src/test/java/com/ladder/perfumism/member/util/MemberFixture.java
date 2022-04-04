f3541a7861c6717c18bc181f7ac05604c94e6d8e
yeonju501 <loling970124@gmail.com>
2022-04-04 12:33:45 +0900

    [S06P22D105-291] feat: MemberFixture 추가

---
+++ backend/src/test/java/com/ladder/perfumism/member/util/MemberFixture.java
@@ -0,0 +1,14 @@
+package com.ladder.perfumism.member.util;
+
+import com.ladder.perfumism.member.controller.dto.request.MemberSaveRequest;
+
+public class MemberFixture {
+
+    public static final String EMAIL = "email@email.com";
+    public static final String PASSWORD = "Password123!";
+    public static final String USERNAME = "username";
+
+    public static MemberSaveRequest createMemberSaveRequest(String email, String password, String username) {
+        return new MemberSaveRequest(email, password, username);
+    }
+}
