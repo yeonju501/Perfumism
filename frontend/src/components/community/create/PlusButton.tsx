165b748d3aa4c2a09e40f9ae9018783ff4a254a0
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-05 01:36:22 +0900

    [S06P22D105-331] feat: plusButton Container 설정

--- frontend/src/components/community/create/PlusButton.tsx
+++ frontend/src/components/community/create/PlusButton.tsx
@@ -1,11 +1,19 @@
 import PlusSrc from "assets/plus.png";
+import styled from "styled-components";
 
 function PlusButton() {
 	return (
-		<>
+		<Container>
 			<img src={PlusSrc} alt="" />
-		</>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	cursor: pointer;
+`;
 
 export default PlusButton;
