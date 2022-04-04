eea51376964eb00aab1c8f30dc03078d005f11e4
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-04 09:32:42 +0900

    [S06P22D105-220] feat: 게시글 삭제 함수 추가

--- frontend/src/components/community/detail/Header.tsx
+++ frontend/src/components/community/detail/Header.tsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import communityApi from "apis/community";
 import styled from "styled-components";
 import { Button } from "../index";
 
 interface ArticleIdType {
-	articleId: string;
+	articleId: string | undefined;
 }
 
 function Header({ articleId }: ArticleIdType) {
+	const handleDeleteClick = async (articleId: string) => {
+		if (window.confirm("리뷰를 삭제 하시겠습니까?")) {
+			await communityApi.deleteCommunity(articleId);
+			navigate("/community");
+		}
+	};
+
 	const navigate = useNavigate();
 
 	const handleListButtonClick = () => {
@@ -18,7 +26,7 @@ function Header({ articleId }: ArticleIdType) {
 			<Button onClick={handleListButtonClick}>목록</Button>
 			<div>
 				<Button>수정</Button>
-				<Button>삭제</Button>
+				<Button onClick={() => handleDeleteClick(articleId)}>삭제</Button>
 			</div>
 		</Container>
 	);
