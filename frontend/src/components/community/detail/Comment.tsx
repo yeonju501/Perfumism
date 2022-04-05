5f8e7863e2a556e70e8805b13bac9a3753f4fea4
9603100 <imgoing222@gmail.com>
2022-04-05 10:14:13 +0900

    [S06P22D105-311] feat: 리뷰 작성 후 실시간 업데이트를 위해 state 생성 후 컴포넌트의 props로 할당

--- frontend/src/components/community/detail/Comment.tsx
+++ frontend/src/components/community/detail/Comment.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import styled from "styled-components";
 import CommentForm from "./CommentForm";
+import CommentList from "./CommentList";
 
 interface CommentProps {
 	articleId: number;
 }
 
 function Comment({ articleId }: CommentProps) {
+	const [updateReviews, setUpdateReviews] = useState(false);
+
 	return (
 		<Container>
-			<CommentForm articleId={articleId} />
+			<CommentForm articleId={articleId} setUpdateReviews={setUpdateReviews} />
+			<CommentList articleId={articleId} updateReviews={updateReviews} />
 		</Container>
 	);
 }
