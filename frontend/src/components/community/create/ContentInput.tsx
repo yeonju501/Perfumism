e67e206d5f7330f1c154ee1eacd67fbb62410f9c
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-05 17:43:22 +0900

    [S06P22D105-304] feat: ContentInput 높이 및 폰트 사이즈 수정

--- frontend/src/components/community/create/ContentInput.tsx
+++ frontend/src/components/community/create/ContentInput.tsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 const ContentInput = styled.textarea`
 	width: 100%;
-	height: 60rem;
-	line-height: 5rem;
+	height: 40rem;
+	font-size: 2.5rem;
+	line-height: 3rem;
 	border: 1px solid #dde0e2;
-	padding: 0 1.6rem;
+	padding: 1.5rem;
 	&:focus {
 		outline: none;
 	}
