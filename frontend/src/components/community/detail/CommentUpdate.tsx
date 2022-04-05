3309d23acdd3eed9d9ae7096aa160e13db77d76b
9603100 <imgoing222@gmail.com>
2022-04-05 14:11:23 +0900

    [S06P22D105-312] feat: CommentUpdate 내부 함수 구현

--- frontend/src/components/community/detail/CommentUpdate.tsx
+++ frontend/src/components/community/detail/CommentUpdate.tsx
@@ -1,8 +1,59 @@
+import communityApi from "apis/community";
 import { CreateButton } from "components/button/Button";
 import { FormContainer } from "components/review/Container";
+import useReviewForm from "components/review/hooks/useReviewForm";
 import Textarea from "components/review/Textarea";
+import { useEffect } from "react";
 
-function CommentUpdate() {
+interface ReviewUpdateProp {
+	commentId: number;
+	articleId: number;
+	oldContent: string;
+	setIsEditable: React.Dispatch<React.SetStateAction<number>>;
+	setReviews: React.Dispatch<React.SetStateAction<ReviewType[]>>;
+}
+
+interface ReviewType {
+	comment_id: number;
+	review_id: number;
+	member_id: number;
+	member_name: string;
+	member_image: string;
+	grade: number;
+	content: string;
+	likes: number;
+}
+
+function CommentUpdate({
+	oldContent,
+	commentId,
+	articleId,
+	setIsEditable,
+	setReviews,
+}: ReviewUpdateProp) {
+	const { handleInputChange, handleFormSubmit, content, setContent } = useReviewForm({
+		sendReviewData: () => {
+			return communityApi.updateComment(articleId, commentId, { content });
+		},
+	});
+
+	useEffect(() => {
+		setContent(oldContent);
+	}, []);
+
+	const handleReviewUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+		await handleFormSubmit(e);
+		setIsEditable(-1);
+		setReviews((reviews) =>
+			reviews.map((review) => {
+				if (review.comment_id === commentId) {
+					review.content = content;
+				}
+				return review;
+			}),
+		);
+	};
+
 	return (
 		<FormContainer onSubmit={handleReviewUpdate}>
 			<Textarea
