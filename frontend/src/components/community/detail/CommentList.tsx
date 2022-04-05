dae03a15fd7c610f196ba7c0e2efacf5c5089e05
9603100 <imgoing222@gmail.com>
2022-04-05 10:12:10 +0900

    [S06P22D105-311] feat: useReviewListForm hook 사용해 댓글 목록 출력

--- frontend/src/components/community/detail/CommentList.tsx
+++ frontend/src/components/community/detail/CommentList.tsx
@@ -2,21 +2,55 @@ import { ShowMoreButton } from "components/button/Button";
 import ReviewButtons from "components/review/ReviewButtons";
 import ReviewUpdate from "components/review/ReviewUpdate";
 import styled from "styled-components";
+import communityApi from "apis/community";
+import useReviewListForm from "components/review/hooks/useReviewListForm";
 
-function CommentList() {
+interface CommentListProp {
+	articleId: number;
+	updateReviews: boolean;
+}
+
+function CommentList({ updateReviews, articleId }: CommentListProp) {
+	console.log(articleId);
+
+	const {
+		userId,
+		reviews,
+		isEditable,
+		setReviews,
+		setTotalPage,
+		setCurrentPage,
+		setIsEditable,
+		handleShowMoreClick,
+		handleDeleteClick,
+		handleUpdateClick,
+		isLastPage,
+	} = useReviewListForm({
+		updateReviews,
+		getReviews: async (currentPage: number) => {
+			const res = await communityApi.getComments(articleId, currentPage);
+			setReviews((prev) => prev.concat(res.data.commentList));
+			setTotalPage(res.data.total_page_count);
+			setCurrentPage(res.data.current_page_count + 1);
+		},
+		deleteReviewData: (commentId: number) => {
+			return communityApi.deleteComment(articleId, commentId);
+		},
+	});
+
 	return reviews.length > 0 ? (
 		<ul>
 			{reviews.map((review) => (
-				<ReviewItem key={review.review_id}>
+				<ReviewItem key={review.comment_id}>
 					<p>{review.member_name}</p>
 					{userId === review.member_id && (
 						<ReviewButtons
 							handleDeleteClick={handleDeleteClick}
 							handleUpdateClick={handleUpdateClick}
-							reviewId={review.review_id}
+							reviewId={review.comment_id}
 						/>
 					)}
-					{review.review_id === isEditable ? (
+					{review.comment_id === isEditable ? (
 						<ReviewUpdate
 							oldContent={review.content}
 							oldGrade={review.grade}
