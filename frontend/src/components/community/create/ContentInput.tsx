76c83a6b6747025e4665c253d4d704612821f2d8
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-05 19:07:18 +0900

    [S06P22D105-304] feat: 글작성 페이지 제목 width 수정

--- frontend/src/components/community/create/ContentInput.tsx
+++ frontend/src/components/community/create/ContentInput.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const ContentInput = styled.textarea`
 	width: 100%;
 	height: 40rem;
-	font-size: 2.5rem;
+	font-size: 2rem;
 	line-height: 3rem;
 	border: 1px solid #dde0e2;
 	padding: 1.5rem;
