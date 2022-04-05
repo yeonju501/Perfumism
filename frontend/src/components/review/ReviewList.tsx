ac2719c57eecb1b23668c6e631ff68c235431c30
9603100 <imgoing222@gmail.com>
2022-04-05 09:08:30 +0900

    [S06P22D105-311] refactor: useReviewListForm 사용해 리팩토링

--- frontend/src/components/review/ReviewList.tsx
+++ frontend/src/components/review/ReviewList.tsx
@@ -1,70 +1,38 @@
 import reviewApi from "apis/review";
-import { DeleteButton, ShowMoreButton, UpdateButton } from "components/button/Button";
-import { useEffect, useState } from "react";
+import { ShowMoreButton } from "components/button/Button";
 import LikeButton from "./LikeButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useSelector } from "react-redux";
-import { RootState } from "store";
 import { faStar as star } from "@fortawesome/free-solid-svg-icons";
 import ReviewUpdate from "./ReviewUpdate";
 import styled from "styled-components";
 import ReviewButtons from "./ReviewButtons";
+import useReviewListForm from "./hooks/useReviewListForm";
 
 interface ReviewListPropType {
 	perfumeId: string;
 	updateReviews: boolean;
 }
 
-interface ReviewType {
-	review_id: number;
-	member_id: number;
-	member_name: string;
-	member_image: string;
-	grade: number;
-	content: string;
-	likes: number;
-}
-
 function ReviewList({ perfumeId, updateReviews }: ReviewListPropType) {
-	const [reviews, setReviews] = useState<ReviewType[]>([]);
-	const [totalPage, setTotalPage] = useState(0);
-	const [currentPage, setCurrentPage] = useState(0);
-	const [isLastPage, setIsLastPage] = useState(false);
-	const [isEditable, setIsEditable] = useState(-1);
-
-	const userId = useSelector((state: RootState) => state.user.id);
-
-	useEffect(() => {
-		if (totalPage && currentPage >= totalPage) setIsLastPage(true);
-	}, [currentPage]);
-
-	useEffect(() => {
-		setCurrentPage(0);
-		setReviews([]);
-		getReviews(0);
-	}, [updateReviews]);
-
-	const getReviews = async (currentPage: number) => {
-		const res = await reviewApi.getReviews(perfumeId, currentPage);
-		setReviews((prev) => prev.concat(res.data.reviews));
-		setTotalPage(res.data.total_page_count);
-		setCurrentPage(res.data.current_page_count + 1);
-	};
-
-	const handleShowMoreClick = () => {
-		getReviews(currentPage);
-	};
-
-	const handleDeleteClick = async (reviewId: number) => {
-		if (window.confirm("리뷰를 삭제 하시겠습니까?")) {
-			await reviewApi.deleteReview(reviewId);
-			setReviews((reviews) => reviews.filter((review) => review.review_id !== reviewId));
-		}
-	};
-
-	const handleUpdateClick = (reviewId: number) => {
-		setIsEditable(reviewId);
-	};
+	const {
+		userId,
+		reviews,
+		isEditable,
+		setReviews,
+		setIsEditable,
+		handleShowMoreClick,
+		handleDeleteClick,
+		handleUpdateClick,
+		isLastPage,
+	} = useReviewListForm({
+		updateReviews,
+		getReviewData: (currentPage: number) => {
+			return reviewApi.getReviews(perfumeId, currentPage);
+		},
+		deleteReviewData: (reviewId: number) => {
+			return reviewApi.deleteReview(reviewId);
+		},
+	});
 
 	const changeReviewLikes = async (reviewId: number) => {
 		const res = await reviewApi.isReviewLiked(reviewId);
