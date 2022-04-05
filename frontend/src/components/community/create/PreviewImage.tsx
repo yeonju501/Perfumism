0292a32a8924e7728fdf07cf88381d8c4f765391
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-05 15:30:40 +0900

    [S06P22D105-304] feat: 이미지 프리뷰 css 추가

--- frontend/src/components/community/create/PreviewImage.tsx
+++ frontend/src/components/community/create/PreviewImage.tsx
@@ -5,9 +5,24 @@ interface PreviewImgPorps {
 }
 
 function PreviewImage({ previewImg }: PreviewImgPorps) {
-	return <Container></Container>;
+	console.log(previewImg);
+	return (
+		<Container>
+			{previewImg.map((imgUrl) => (
+				<PreviewImg src={imgUrl} alt="" />
+			))}
+		</Container>
+	);
 }
 
-export const Container = styled.div``;
+const Container = styled.div`
+	display: flex;
+`;
+
+const PreviewImg = styled.img`
+	width: 15rem;
+	height: 15rem;
+	margin: 1rem;
+`;
 
 export default PreviewImage;
