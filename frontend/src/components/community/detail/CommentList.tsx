a1b81b2d1a0240a9521a6912a3323d79a37564cf
9603100 <imgoing222@gmail.com>
2022-04-05 09:11:31 +0900

    [S06P22D105-311] feat: CommentList 기본 UI 구성

--- frontend/src/components/community/detail/CommentList.tsx
+++ frontend/src/components/community/detail/CommentList.tsx
@@ -1,5 +1,51 @@
+import { ShowMoreButton } from "components/button/Button";
+import ReviewButtons from "components/review/ReviewButtons";
+import ReviewUpdate from "components/review/ReviewUpdate";
+import styled from "styled-components";
+
 function CommentList() {
-	return;
+	return reviews.length > 0 ? (
+		<ul>
+			{reviews.map((review) => (
+				<ReviewItem key={review.review_id}>
+					<p>{review.member_name}</p>
+					{userId === review.member_id && (
+						<ReviewButtons
+							handleDeleteClick={handleDeleteClick}
+							handleUpdateClick={handleUpdateClick}
+							reviewId={review.review_id}
+						/>
+					)}
+					{review.review_id === isEditable ? (
+						<ReviewUpdate
+							oldContent={review.content}
+							oldGrade={review.grade}
+							reviewId={review.review_id}
+							setIsEditable={setIsEditable}
+							setReviews={setReviews}
+						/>
+					) : (
+						<CommentContent>{review.content}</CommentContent>
+					)}
+				</ReviewItem>
+			))}
+			<ShowMoreButton onClick={handleShowMoreClick} isLastPage={isLastPage}>
+				Show More
+			</ShowMoreButton>
+		</ul>
+	) : (
+		<p style={{ color: "#555555", padding: "0 1rem", fontSize: "1.2rem" }}>
+			작성된 댓글이 없습니다
+		</p>
+	);
 }
+
+const ReviewItem = styled.li`
+	border: 0.3px solid #dedede;
+	margin-bottom: 3rem;
+	padding: 1rem 2rem;
+`;
+
+const CommentContent = styled.p``;
 
 export default CommentList;
