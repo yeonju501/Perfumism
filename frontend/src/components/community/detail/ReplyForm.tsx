4090b5567bb95f2f35b8d9588f6b177dfd7944d7
9603100 <imgoing222@gmail.com>
2022-04-05 17:44:41 +0900

    [S06P22D105-335] feat: useReviewForm으로 ReplyForm 내부 함수 구현

--- frontend/src/components/community/detail/ReplyForm.tsx
+++ frontend/src/components/community/detail/ReplyForm.tsx
@@ -1,8 +1,27 @@
+import communityApi from "apis/community";
 import { CreateButton } from "components/button/Button";
 import { FormContainer } from "components/review/Container";
+import useReviewForm from "components/review/hooks/useReviewForm";
 import Textarea from "components/review/Textarea";
 
-function ReplyForm() {
+interface CommentFormProps {
+	articleId: number;
+	commentId: number;
+	setUpdateReviews: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function ReplyForm({ articleId, commentId, setUpdateReviews }: CommentFormProps) {
+	const { handleInputChange, handleFormSubmit, content } = useReviewForm({
+		sendReviewData: () => {
+			return communityApi.createReply(articleId, commentId, { content });
+		},
+	});
+
+	const handleSubmitReview = async (e: React.FormEvent<HTMLFormElement>) => {
+		await handleFormSubmit(e);
+		setUpdateReviews((prev) => !prev);
+	};
+
 	return (
 		<FormContainer onSubmit={handleSubmitReview}>
 			<Textarea
