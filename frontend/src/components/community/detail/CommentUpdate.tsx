57377f180ee1dbf4fd12717a2308747b2c5a922e
9603100 <imgoing222@gmail.com>
2022-04-05 14:03:05 +0900

    [S06P22D105-312] feat: CommentUpdate UI 구성

--- frontend/src/components/community/detail/CommentUpdate.tsx
+++ frontend/src/components/community/detail/CommentUpdate.tsx
@@ -1,5 +1,18 @@
+import { CreateButton } from "components/button/Button";
+import { FormContainer } from "components/review/Container";
+import Textarea from "components/review/Textarea";
+
 function CommentUpdate() {
-	return;
+	return (
+		<FormContainer onSubmit={handleReviewUpdate}>
+			<Textarea
+				value={content}
+				onChange={handleInputChange}
+				placeholder="댓글을 수정하세요"
+			></Textarea>
+			<CreateButton>수정</CreateButton>
+		</FormContainer>
+	);
 }
 
 export default CommentUpdate;
