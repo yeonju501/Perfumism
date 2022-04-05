45317ac7c9ff1f8b1453e8718f98278ca166fe77
bangbbang <97500667+GJBBang@users.noreply.github.com>
2022-04-05 11:46:43 +0900

    [S06P22D105-222] feat: 투표 추가 버튼 클릭 시 모달창 띄우기

--- frontend/src/components/community/create/PlusButton.tsx
+++ frontend/src/components/community/create/PlusButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PlusSrc from "assets/plus.png";
+import VoteCreate from "./VoteCreate";
 import styled from "styled-components";
 
 interface ImgProps {
@@ -9,8 +10,10 @@ interface ImgProps {
 
 function PlusButton({ setSelectedImg, setPreviewImg }: ImgProps) {
 	const [isOpen, setIsOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const toggling = () => setIsOpen(!isOpen);
+	const handleShow = () => setIsModalOpen(!isModalOpen);
 
 	const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const nowSelectImgList = e.target.files;
@@ -32,11 +35,12 @@ function PlusButton({ setSelectedImg, setPreviewImg }: ImgProps) {
 					<div>
 						<input type="file" multiple accept="image/*" onChange={handleImageUpload} />
 						<br />
-						<button>투표 추가</button>
+						<button onClick={handleShow}>투표 추가</button>
 					</div>
 				)}
 				<ButtonImg src={PlusSrc} alt="" onClick={toggling} />
 			</ButtonContainer>
+			{isModalOpen && <VoteCreate isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />}
 		</>
 	);
 }
--- frontend/src/components/community/create/VoteCreate.tsx
+++ frontend/src/components/community/create/VoteCreate.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
-function VoteCreate() {
-	const [show, setShow] = useState(false);
+interface Props {
+	isModalOpen: boolean;
+	setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+function VoteCreate({ isModalOpen, setIsModalOpen }: Props) {
+	const handleClose = () => setIsModalOpen(false);
 
 	return (
 		<>
-			<Button variant="primary" onClick={handleShow}>
-				Launch static backdrop modal
-			</Button>
-
-			<Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+			<Modal show={isModalOpen} onHide={handleClose} backdrop="static" keyboard={false}>
 				<Modal.Header closeButton>
 					<Modal.Title>Modal title</Modal.Title>
 				</Modal.Header>
