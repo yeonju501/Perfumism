54f5aebfb9ba12414019950a0976ae636e398290
우동진 <wjinh93@gmail.com>
2022-04-05 15:25:27 +0900

    [S06P22D105-332] feat: search api 수정

--- frontend/src/apis/search.ts
+++ frontend/src/apis/search.ts
@@ -2,11 +2,12 @@ import { AxiosResponse } from "axios";
 import { request } from "./request";
 
 interface ReviewApiType {
-	searchPerfume: (keyword: string) => Promise<AxiosResponse<{ perfumes: [] }>>;
+	searchPerfume: (keyword: string, currentPage: number) => Promise<AxiosResponse<{ perfumes: [] }>>;
 }
 
 const searchApi: ReviewApiType = {
-	searchPerfume: (keyword) => request.get(`perfumes/search-all?keyword=${keyword}&page=0&size=10`),
+	searchPerfume: (keyword, currentPage) =>
+		request.get(`perfumes/search-all?keyword=${keyword}&page=${currentPage}&size=100`),
 };
 
 export default searchApi;
