98274fc4e1c0204457acfd8bd788f5e3d14a7406
우동진 <wjinh93@gmail.com>
2022-04-06 11:33:31 +0900

    [S06P22D105-330] feat: profile route 구현

---
+++ frontend/src/routes/ProfileRoute.tsx
@@ -0,0 +1,19 @@
+import Sidebar from "components/profile/Sidebar";
+import { Outlet } from "react-router";
+import styled from "styled-components";
+
+function ProfileRoute() {
+	return (
+		<Container>
+			<Sidebar />
+			<Outlet />
+		</Container>
+	);
+}
+
+export default ProfileRoute;
+
+const Container = styled.div`
+	display: flex;
+	min-height: 100vh;
+`;
