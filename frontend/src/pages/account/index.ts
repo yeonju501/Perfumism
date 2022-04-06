65ae5320ff5eec9a7acfa70e8ae08a425e37afed
우동진 <wjinh93@gmail.com>
2022-04-06 14:17:59 +0900

    [S06P22D105-266] feat: index.ts 구현

---
+++ frontend/src/pages/account/index.ts
@@ -0,0 +1,8 @@
+export { default as SignUp } from "./signup/SignUp";
+export { default as SignIn } from "./signin/SignIn";
+export { default as GoogleLoginPage } from "./socialogin/GoogleLoginPage";
+export { default as KaKaoLoginPage } from "./socialogin/KaKaoLoginPage";
+export { default as FindPassword } from "./find-pw/FindPassword";
+export { default as InputCodePage } from "./find-pw/InputCodePage";
+export { default as ChangePasswordPage } from "./find-pw/ChangePasswordPage";
+export { default as SuccessPage } from "./find-pw/SuccessPage";
