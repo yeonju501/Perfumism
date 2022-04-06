7c70fc3ab7ef5d41aaa4a8e0f20a60e64cf47296
우동진 <wjinh93@gmail.com>
2022-04-06 11:35:18 +0900

    [S06P22D105-330] feat: to에 주소 추가

--- frontend/src/components/profile/Sidebar.tsx
+++ frontend/src/components/profile/Sidebar.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 
 function Sidebar() {
+	const username = useSelector((state: RootState) => state.user.username);
+
 	return (
 		<Container>
-			<LinkItem to="">회원 정보수정</LinkItem>
+			<LinkItem to={`/profile/${username}`}>회원 정보수정</LinkItem>
 			<LinkItem to="/profile/favorites">좋아요한 향수 관리</LinkItem>
 			<LinkItem to="/profile/my-articles">내가 작성한 글</LinkItem>
 			<LinkItem to="/profile/my-comments">내가 작성한 댓글</LinkItem>
