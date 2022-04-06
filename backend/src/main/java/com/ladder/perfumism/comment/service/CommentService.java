ac2ee1f928bbe88da0de3e60365dce81c38f44e2
IcedCaffeLatte <tmdrl0160@naver.com>
2022-04-07 00:47:41 +0900

    [S06P22D105-282] feat: CommentService 에 쓸데없는 import 삭제

--- backend/src/main/java/com/ladder/perfumism/comment/service/CommentService.java
+++ backend/src/main/java/com/ladder/perfumism/comment/service/CommentService.java
@@ -15,7 +15,6 @@ import com.ladder.perfumism.notification.service.NotificationService;
 import java.util.Objects;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
