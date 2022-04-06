1a3ede353d799d3cbffd644cf4882699b705f2dd
우동진 <wjinh93@gmail.com>
2022-04-06 14:20:45 +0900

    [S06P22D105-266] feat: profile page index.ts 구현

---
+++ frontend/src/pages/profile/index.ts
@@ -0,0 +1,5 @@
+export { default as Favorites } from "./Favorites";
+export { default as MyArticles } from "./MyArticles";
+export { default as MyComments } from "./MyComments";
+export { default as Profile } from "./Profile";
+export { default as ChangePassword } from "./ChangePassword";
