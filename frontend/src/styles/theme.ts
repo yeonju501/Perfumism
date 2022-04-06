1a35adef250dd7c40bff4207c2dc616849e9bb96
우동진 <wjinh93@gmail.com>
2022-04-06 20:30:13 +0900

    [S06P22D105-351] feat: mobileXS 추가

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -4,11 +4,13 @@ const size = {
 	tabletS: "1060px",
 	mobile: "790px",
 	mobileS: "600px",
+	mobileXS: "450px",
 };
 
 const theme = {
 	mobile: `(max-width: ${size.mobile})`,
 	mobileS: `(max-width:${size.mobileS})`,
+	mobileXS: `(max-width:${size.mobileXS})`,
 	tabletS: `(max-width: ${size.tabletS})`,
 };
 
