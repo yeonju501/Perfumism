f1e51c66b070c37aed91e323e8aaa8e4d9a6681c
9603100 <imgoing222@gmail.com>
2022-04-08 02:04:47 +0900

    [S06P22D105-366] style: 반응형 프로필 사진 수정 버튼 위치 조정

--- frontend/src/components/profile/UserInfo.tsx
+++ frontend/src/components/profile/UserInfo.tsx
@@ -52,6 +52,9 @@ const ImageContainer = styled.div`
 	}
 	position: relative;
 	margin-bottom: 5rem;
+	@media ${(props) => props.theme.mobileS} {
+		margin-left: 20%;
+	}
 `;
 
 const UserImage = styled.img`
@@ -62,9 +65,6 @@ const UserImage = styled.img`
 	cursor: pointer;
 	&:hover {
 		filter: brightness(80%);
-	}
-	@media ${(props) => props.theme.mobileS} {
-		margin-left: 25%;
 	}
 `;
 
