93a582e9fab240c124c0750b06bf6d1c0f31e589
9603100 <imgoing222@gmail.com>
2022-04-07 14:58:34 +0900

    [S06P22D105-359] style: 폰트 후보 import

--- frontend/src/styles/globalstyle.ts
+++ frontend/src/styles/globalstyle.ts
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  
   *, *::before, *::after {
     box-sizing: border-box;
   }
   html{
     font-size: 62.5%;
-    font-family: "Helvetica", "Arial", sans-serif;
     line-height: 1.5;
   }
   ul{
