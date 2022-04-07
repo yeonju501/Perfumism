5323c8dcace1938da17cd6af172dc5bbafa21e33
9603100 <imgoing222@gmail.com>
2022-04-07 15:04:05 +0900

    [S06P22D105-359] fix: 메인화면에서 유저 정보 저장하도록 수정

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 import { RecommendSection, PerfumesSection, ReviewSection, MonthOfPerfumes } from "components/main";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { profileApi } from "apis";
+import { SET_USER } from "store/user";
 
 function MainPage() {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		getUserInfo();
+	});
+
+	const getUserInfo = async () => {
+		const res = await profileApi.getUserInfo();
+		dispatch(SET_USER(res.data));
+	};
+
 	return (
 		<Main>
 			<RecommendSection />
